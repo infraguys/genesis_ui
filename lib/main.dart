c89b94860e7a8d1c5036976cbbf29349e451cd35
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-16 17:09:08 +0300

    resolve urlPathStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:genesis/src/l10n/generated/app_localizations.dart';
 import 'package:genesis/src/routing/app_router.dart';
+import 'package:genesis/src/routing/url_strategy/app_url_strategy.dart';
 import 'package:genesis/src/theming/app_theme.dart';
 
 void main() {
+  configureAppUrlStrategy();
   runApp(const MyApp());
 }
 
