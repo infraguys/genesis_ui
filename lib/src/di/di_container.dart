29115e2e54978bb2dd7e5c49a56abff3d32a6078
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-16 17:29:00 +0300

    add di_container

---
+++ lib/src/di/di_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/core/rest_client/rest_client.dart';
+import 'package:genesis/src/features/auth/data/iam_client_repository.dart';
+import 'package:genesis/src/features/auth/data/source/impl/remote_iam_client_api.dart';
+
+class DiContainer extends StatelessWidget {
+  const DiContainer({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) {
+            final iamApi = RemoteIamClientApi(RestClient());
+            return IamClientRepository(iamApi);
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+}
