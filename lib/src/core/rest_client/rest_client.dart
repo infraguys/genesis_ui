5e0828ae4e98a425bd01fd102f81f9765943d925
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 10:07:04 +0300

    add post method in RestClient

--- lib/src/core/rest_client/rest_client.dart
+++ lib/src/core/rest_client/rest_client.dart
@@ -32,11 +32,31 @@ class RestClient {
     );
   }
 
+  Future<Response<T>> post<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    void Function(int, int)? onReceiveProgress,
+    void Function(int, int)? onSendProgress,
+  }) async {
+    return await _dio.post<T>(
+      path,
+      data: data,
+      options: options,
+      cancelToken: cancelToken,
+      queryParameters: queryParameters,
+      onReceiveProgress: onReceiveProgress,
+      onSendProgress: onSendProgress,
+    );
+  }
+
   static Dio _createDio() {
     final dio = Dio()
       ..options = BaseOptions(
         baseUrl: 'http://10.130.4.45:11010',
-        connectTimeout: const Duration(seconds: 5),
+        connectTimeout: const Duration(seconds: 15),
         contentType: Headers.jsonContentType,
       )
       ..interceptors.add(
