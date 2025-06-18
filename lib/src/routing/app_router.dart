19e00a30c4d937868fda715a1b1e41e1c3f1ceb1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 18:43:06 +0300

    refactor app_router.dart

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/features/auth/presentation/auth_bloc/auth_bloc.dart';
 import 'package:genesis/src/features/auth/presentation/login_screen/login_screen.dart';
 import 'package:genesis/src/features/dashboard/presentation/dashboard_page.dart';
 import 'package:genesis/src/widgets/scaffold_with_navigation.dart';
@@ -6,77 +10,106 @@ import 'package:go_router/go_router.dart';
 
 part './routes.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/login',
-  routes: [
-    GoRoute(
-      path: '/login',
-      pageBuilder: (context, state) {
-        return NoTransitionPage(child: LoginScreen());
-      },
-    ),
-    StatefulShellRoute.indexedStack(
-      builder: (context, state, navigationShell) {
-        return ScaffoldWithNavigation(navigationShell: navigationShell);
-      },
-      branches: [
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              name: AppRoutes.dashboard.name,
-              path: '/',
-              pageBuilder: (context, state) {
-                return NoTransitionPage(child: DashboardPage());
-              },
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              name: AppRoutes.monitoring.name,
-              path: '/monitoring',
-              pageBuilder: (context, state) {
-                return NoTransitionPage(
-                  child: Scaffold(
-                    body: Center(child: Text('monitoring')),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              name: AppRoutes.users.name,
-              path: '/users',
-              pageBuilder: (context, state) {
-                return NoTransitionPage(
-                  child: Scaffold(
-                    body: Center(child: Text('user')),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              name: AppRoutes.vpn.name,
-              path: '/vpn',
-              pageBuilder: (context, state) {
-                return NoTransitionPage(
-                  child: Scaffold(
-                    body: Center(child: Text('user')),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ],
-    ),
-  ],
-);
+GoRouter createRouter(BuildContext context) {
+  print('router');
+  final authBloc = context.read<AuthBloc>();
+  return GoRouter(
+    initialLocation: '/login',
+    refreshListenable: _GoRouterRefreshStream(authBloc.stream),
+    routes: [
+      GoRoute(
+        path: '/login',
+        pageBuilder: (context, state) {
+          return NoTransitionPage(child: LoginScreen());
+        },
+        redirect: (context, state) {
+          final bloc = context.read<AuthBloc>();
+          if (bloc.state is Authenticated) {
+            return '/';
+          }
+          return null;
+        },
+      ),
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return ScaffoldWithNavigation(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: AppRoutes.dashboard.name,
+                path: '/',
+                pageBuilder: (context, state) {
+                  return NoTransitionPage(child: DashboardPage());
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: AppRoutes.monitoring.name,
+                path: '/monitoring',
+                pageBuilder: (context, state) {
+                  return NoTransitionPage(
+                    child: Scaffold(
+                      body: Center(child: Text('monitoring')),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: AppRoutes.users.name,
+                path: '/users',
+                pageBuilder: (context, state) {
+                  return NoTransitionPage(
+                    child: Scaffold(
+                      body: Center(child: Text('user')),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: AppRoutes.vpn.name,
+                path: '/vpn',
+                pageBuilder: (context, state) {
+                  return NoTransitionPage(
+                    child: Scaffold(
+                      body: Center(child: Text('user')),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+class _GoRouterRefreshStream extends ChangeNotifier {
+  _GoRouterRefreshStream(Stream<AuthState> stream) {
+    notifyListeners();
+    _subscription = stream.listen((state) {
+      notifyListeners();
+    });
+  }
+
+  late final StreamSubscription<AuthState> _subscription;
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
