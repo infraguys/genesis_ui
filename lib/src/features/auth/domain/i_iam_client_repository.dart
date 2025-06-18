e5c3520280446791c7d2d3a46df2c5e1d0a85a8f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 10:09:05 +0300

    create IamClientRepository interface

--- lib/src/features/auth/domain/i_iam_client_repository.dart
+++ lib/src/features/auth/domain/i_iam_client_repository.dart
@@ -1,7 +1,8 @@
 import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
+import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
 abstract interface class IIamClientRepository {
-  Future<IamClient> createTokenByPassword();
+  Future<IamClient> createTokenByPassword(CreateTokenParams params);
 
   Future<void> resetPasswordIamClient();
 }
