f7e996db8d4d065f0eb2cddd32625e85bdab3d7f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 10:16:45 +0300

    create addTokenUseCaseˆ

--- lib/src/features/auth/domain/use_case/create_token_by_password.dart
+++ lib/src/features/auth/domain/use_case/create_token_by_password.dart
@@ -1,4 +1,12 @@
-class CreateTokenByPassword {
+import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
+import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
+class CreateTokenUseCase {
+  CreateTokenUseCase(this._repository);
+
+  final IIamClientRepository _repository;
+
+  Future<void> call(CreateTokenParams params) async {
+    await _repository.createTokenByPassword(params);
+  }
 }
-
