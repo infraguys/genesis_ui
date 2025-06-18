d7562d6e3da9f1da9bb1bbca6974c9d1833455c2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 10:25:28 +0300

    add snackbar in ScaffolWithNavigation

--- lib/src/widgets/scaffold_with_navigation.dart
+++ lib/src/widgets/scaffold_with_navigation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/extensions/localized_build_context.dart';
 import 'package:genesis/src/extensions/string_extension.dart';
+import 'package:genesis/src/features/auth/presentation/auth_bloc/auth_bloc/auth_bloc/auth_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class ScaffoldWithNavigation extends StatelessWidget {
@@ -17,44 +19,60 @@ class ScaffoldWithNavigation extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          PopupMenuButton<void>(
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                child: const Text('Sign out'),
-                onTap: () {
-                  // Sign out logic
-                },
-              ),
-            ],
-            child: const Icon(Icons.notifications_none_outlined),
-          ),
-          const SizedBox(width: 24),
-          PopupMenuButton<void>(
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                child: const Text('Sign out'),
-                onTap: () {
-                  // Sign out logic
-                },
-              ),
-            ],
-            child: const Icon(Icons.account_circle_outlined),
-          ),
-        ],
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is SignInSuccess) {
+          final snackBar = SnackBar(
+            content: const Text('Yay! SignIn was Success!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            PopupMenuButton<void>(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: const Text('Sign out'),
+                  onTap: () {
+                    // Sign out logic
+                  },
+                ),
+              ],
+              child: const Icon(Icons.notifications_none_outlined),
+            ),
+            const SizedBox(width: 24),
+            PopupMenuButton<void>(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: const Text('Sign out'),
+                  onTap: () {
+                    // Sign out logic
+                  },
+                ),
+              ],
+              child: const Icon(Icons.account_circle_outlined),
+            ),
+          ],
+        ),
+        drawer: NavigationDrawer(
+          tilePadding: EdgeInsetsGeometry.zero,
+          selectedIndex: navigationShell.currentIndex,
+          onDestinationSelected: goBranch,
+          children: [
+            NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text(context.$.dashboard)),
+            NavigationDrawerDestination(icon: Icon(Icons.laptop), label: Text('Monitoring'.hardcoded)),
+          ],
+        ),
+        body: navigationShell,
       ),
-      drawer: NavigationDrawer(
-        tilePadding: EdgeInsetsGeometry.zero,
-        selectedIndex: navigationShell.currentIndex,
-        onDestinationSelected: goBranch,
-        children: [
-          NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text(context.$.dashboard)),
-          NavigationDrawerDestination(icon: Icon(Icons.laptop), label: Text('Monitoring'.hardcoded)),
-        ],
-      ),
-      body: navigationShell,
     );
   }
 }
