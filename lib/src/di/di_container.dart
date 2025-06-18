78dd19c47f23e945fb5b89f6d3c12c0adbb825f5
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 13:34:24 +0300

    fix imports DiContainer

--- lib/src/di/di_container.dart
+++ lib/src/di/di_container.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/rest_client/rest_client.dart';
 import 'package:genesis/src/features/auth/data/iam_client_repository.dart';
 import 'package:genesis/src/features/auth/data/source/impl/remote_iam_client_api.dart';
-import 'package:genesis/src/features/auth/presentation/auth_bloc/auth_bloc/auth_bloc/auth_bloc.dart';
+import 'package:genesis/src/features/auth/presentation/auth_bloc/auth_bloc.dart';
 
 class DiContainer extends StatelessWidget {
   const DiContainer({required this.child, super.key});
