cf5e4d7f575da7b96496eca047f21a18b1e973ca
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-18 10:24:39 +0300

    wrap MaterialApp in DiContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:genesis/src/di/di_container.dart';
 import 'package:genesis/src/l10n/generated/app_localizations.dart';
 import 'package:genesis/src/routing/app_router.dart';
 import 'package:genesis/src/routing/url_strategy/app_url_strategy.dart';
@@ -14,12 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: AppTheme().light,
+    return DiContainer(
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: AppTheme().light,
+      ),
     );
   }
 }
