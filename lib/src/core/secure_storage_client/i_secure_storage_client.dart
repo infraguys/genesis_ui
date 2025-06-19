acfaf1f80ccc461b1c786339529c952de23c15dd
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 17:28:40 +0300

    remove some filesˆ

--- lib/src/core/secure_storage_client/i_secure_storage_client.dart
+++
@@ -1,7 +0,0 @@
-abstract interface class ISecureStorageClient {
-  Future<void> write({required String key, required String? value});
-
-  Future<String?> read(String key);
-
-  Future<void> delete(String key);
-}
