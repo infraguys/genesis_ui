8658f6918103cfa736e2490930ac8533c106dd10
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 17:29:31 +0300

    change createTokenByPassword method to fetchCurrentClient

--- lib/src/features/auth/data/iam_client_repository.dart
+++ lib/src/features/auth/data/iam_client_repository.dart
@@ -1,4 +1,3 @@
-import 'package:genesis/src/features/auth/data/source/access_token_dao.dart';
 import 'package:genesis/src/features/auth/data/source/i_remote_iam_client_api.dart';
 import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
 import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
@@ -7,16 +6,17 @@ import 'package:genesis/src/features/auth/domain/params/create_token_params.dart
 class IamClientRepository implements IIamClientRepository {
   IamClientRepository({
     required this.iamApi,
-    required this.accessTokenDao,
   });
 
   final IRemoteIamClientApi iamApi;
-  final IAccessTokenDao accessTokenDao;
 
   @override
-  Future<IamClient?> createTokenByPassword(CreateTokenParams params) async {
-    final dto = await iamApi.createTokenByPassword(params);
-    await accessTokenDao.writeToken(dto.accessToken);
+  Future<IamClient?> fetchCurrentClient(CreateTokenParams params) async {
+    await iamApi.createTokenByPassword(params);
+    final dto = await iamApi.fetchCurrentClient();
+    if (dto == null) {
+      return null;
+    }
     return dto.toEntity();
   }
 
