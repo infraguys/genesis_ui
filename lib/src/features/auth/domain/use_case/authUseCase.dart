4f5830200e73f8a1be9043242cbfb5e7d7cdb5fa
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 17:31:31 +0300

    rename CreateTokenUseCase to AuthUseCase

--- lib/src/features/auth/domain/use_case/authUseCase.dart
+++ lib/src/features/auth/domain/use_case/authUseCase.dart
@@ -2,12 +2,12 @@ import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
 import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
 import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
-class CreateTokenUseCase {
-  CreateTokenUseCase(this._repository);
+class AuthUseCase {
+  AuthUseCase(this._repository);
 
   final IIamClientRepository _repository;
 
   Future<IamClient?> call(CreateTokenParams params) async {
-    return await _repository.createTokenByPassword(params);
+    return await _repository.fetchCurrentClient(params);
   }
 }
