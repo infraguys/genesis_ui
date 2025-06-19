785be0b09f0a6728d3ad3b6f50b249079cef3664
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 13:29:23 +0300

    create user dto

---
+++ lib/src/features/auth/data/dto/user_dto.dart
@@ -0,0 +1,59 @@
+import 'package:genesis/src/features/auth/domain/entity/user.dart';
+import 'package:genesis/src/interfaces/i_dto.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_dto.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, createToJson: false)
+class UserDto implements IDto<User> {
+  UserDto({
+    required this.uuid,
+    required this.name,
+    required this.description,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.status,
+    required this.firstName,
+    required this.lastName,
+    required this.surname,
+    required this.phone,
+    required this.email,
+    required this.emailVerified,
+    required this.otpEnabled,
+  });
+
+  factory UserDto.fromJson(Map<String, dynamic> json) => _$UserDtoFromJson(json);
+
+  final int uuid;
+  final String name;
+  final String description;
+  final String createdAt;
+  final String updatedAt;
+  final String status;
+  final String firstName;
+  final String lastName;
+  final String surname;
+  final String? phone;
+  final String email;
+  final bool emailVerified;
+  final bool otpEnabled;
+
+  @override
+  User toEntity() {
+    return User(
+      uuid: uuid,
+      name: name,
+      description: description,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      status: status,
+      firstName: firstName,
+      lastName: lastName,
+      surname: surname,
+      phone: phone,
+      email: email,
+      emailVerified: emailVerified,
+      otpEnabled: otpEnabled,
+    );
+  }
+}
