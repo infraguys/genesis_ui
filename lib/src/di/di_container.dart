2b8bd3a1e5e715ba8a5ec655fa09b2145e403e2b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 17:30:41 +0300

    fix diContainerˆˆ

--- lib/src/di/di_container.dart
+++ lib/src/di/di_container.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:genesis/src/core/rest_client/rest_client.dart';
-import 'package:genesis/src/core/secure_storage_client/i_secure_storage_client.dart';
-import 'package:genesis/src/core/secure_storage_client/secure_storage_client.dart';
+import 'package:genesis/src/core/secure_storage_client/i_simple_storage_client.dart';
+import 'package:genesis/src/core/secure_storage_client/shared_pref_storage.dart';
 import 'package:genesis/src/features/auth/data/iam_client_repository.dart';
-import 'package:genesis/src/features/auth/data/source/impl/access_token_dao.dart';
 import 'package:genesis/src/features/auth/data/source/impl/remote_iam_client_api.dart';
+import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
 import 'package:genesis/src/features/auth/presentation/auth_bloc/auth_bloc.dart';
 import 'package:genesis/src/routing/app_router.dart';
 import 'package:provider/provider.dart';
@@ -18,28 +17,25 @@ class DiContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('di');
     return MultiProvider(
       providers: [
-        Provider(
-          create: (context) {
-            return SecureStorageClient(FlutterSecureStorage());
-          },
+        Provider<ISimpleStorageClient>(
+          create: (_) => SharedPrefStorage(),
         ),
-        RepositoryProvider(
+        Provider<RestClient>(
+          create: (_) => RestClient(),
+        ),
+        RepositoryProvider<IIamClientRepository>(
           create: (context) {
-            final iamApi = RemoteIamClientApi(RestClient());
-            final storageClient = context.read<ISecureStorageClient>();
-            final tokenDao = AccessTokenDao(storageClient);
+            final iamApi = RemoteIamClientApi(context.read<RestClient>());
             return IamClientRepository(
               iamApi: iamApi,
-              accessTokenDao: tokenDao,
             );
           },
         ),
         BlocProvider(
           create: (context) {
-            final repository = context.read<IamClientRepository>();
+            final repository = context.read<IIamClientRepository>();
             return AuthBloc(repository);
           },
         ),
