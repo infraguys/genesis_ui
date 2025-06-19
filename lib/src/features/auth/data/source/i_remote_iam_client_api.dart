db0fa1f46ddc4e7a298ebea29a35685a913f77e0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-19 17:32:38 +0300

    fix returned type i_iam_client_repository.dart

--- lib/src/features/auth/data/source/i_remote_iam_client_api.dart
+++ lib/src/features/auth/data/source/i_remote_iam_client_api.dart
@@ -2,7 +2,9 @@ import 'package:genesis/src/features/auth/data/dto/iam_client_dto.dart';
 import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
 abstract interface class IRemoteIamClientApi {
-  Future<IamClientDto> createTokenByPassword(CreateTokenParams params);
+  Future<void> createTokenByPassword(CreateTokenParams params);
 
   Future<void> resetPasswordIamClient();
+
+  Future<IamClientDto?> fetchCurrentClient();
 }
--- lib/src/features/auth/domain/i_iam_client_repository.dart
+++ lib/src/features/auth/domain/i_iam_client_repository.dart
@@ -2,7 +2,7 @@ import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
 import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
 abstract interface class IIamClientRepository {
-  Future<IamClient?> createTokenByPassword(CreateTokenParams params);
-
   Future<void> resetPasswordIamClient();
+
+  Future<IamClient?> fetchCurrentClient(CreateTokenParams params);
 }
