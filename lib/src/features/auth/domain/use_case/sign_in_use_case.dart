bbaf2a546612438ccd07f85c678860901ae121f9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-20 13:01:00 +0300

    authUseCase renamed to sign_in_use_case

--- lib/src/features/auth/domain/use_case/sign_in_use_case.dart
+++ lib/src/features/auth/domain/use_case/sign_in_use_case.dart
@@ -2,12 +2,12 @@ import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
 import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
 import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
 
-class AuthUseCase {
-  AuthUseCase(this._repository);
+class SignInUseCase {
+  SignInUseCase(this._repo);
 
-  final IIamClientRepository _repository;
+  final IIamClientRepository _repo;
 
   Future<IamClient?> call(CreateTokenParams params) async {
-    return await _repository.fetchCurrentClient(params);
+    return await _repo.fetchCurrentClient(params);
   }
 }
