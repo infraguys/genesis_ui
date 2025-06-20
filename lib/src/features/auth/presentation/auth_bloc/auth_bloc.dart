b47c3bb1a70f352ec8ebf1f2777762e63c94ee00
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-20 13:46:31 +0300

    add _signOut method in AuthBloc

--- lib/src/features/auth/presentation/auth_bloc/auth_bloc.dart
+++ lib/src/features/auth/presentation/auth_bloc/auth_bloc.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/features/auth/domain/entity/iam_client.dart';
 import 'package:genesis/src/features/auth/domain/i_iam_client_repository.dart';
 import 'package:genesis/src/features/auth/domain/params/create_token_params.dart';
-import 'package:genesis/src/features/auth/domain/use_case/authUseCase.dart';
+import 'package:genesis/src/features/auth/domain/use_case/sign_in_use_case.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -10,20 +10,13 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(this._iamClientRepository) : super(AuthState.init()) {
     on<_SingIn>(_signIn);
+    on<_SingOut>(_signOut);
   }
 
   final IIamClientRepository _iamClientRepository;
 
   Future<void> _signIn(_SingIn event, Emitter<AuthState> emit) async {
-    final useCase = AuthUseCase(_iamClientRepository);
-    /**
-     * grantType: 'password',
-        clientId: 'GenesisCoreClientId',
-        clientSecret: 'GenesisCoreClientSecret',
-        username: 'admin',
-        password: 'admin',
-     *
-     */
+    final useCase = SignInUseCase(_iamClientRepository);
     final iamClient = await useCase(
       CreateTokenParams(
         iamClientUuid: '00000000-0000-0000-0000-000000000000',
@@ -39,5 +32,9 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     if (iamClient != null) {
       emit(Authenticated(iamClient));
     }
+  }
+
+  Future<void> _signOut(_, Emitter<AuthState> emit) async {
+    emit(Unauthenticated());
   }
 }
