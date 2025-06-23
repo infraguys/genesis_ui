5e580eb481d4d37be82d35b7566bf01c43d7390b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-23 20:33:52 +0300

    create SignUpParams

---
+++ lib/src/features/auth/domain/params/sign_up_params.dart
@@ -0,0 +1,11 @@
+class SignUpParams {
+  SignUpParams({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+  });
+
+  final String firstName;
+  final String lastName;
+  final String email;
+}
