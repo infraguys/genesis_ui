f2ee11511c2819af8cc0101f50411db119b1712a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-25 02:32:18 +0300

    add logic in network_exception.dart

--- lib/src/core/exceptions/network_exception.dart
+++ lib/src/core/exceptions/network_exception.dart
@@ -3,20 +3,28 @@ import 'package:dio/dio.dart';
 class NetworkException implements Exception {
   factory NetworkException(DioException exception) {
     final statusCode = exception.response!.statusCode!;
-    final message = switch (exception.type) {
-      DioExceptionType.cancel => 'Request to API server was cancelled',
-      DioExceptionType.connectionTimeout => 'Connection timeout with API server',
-      DioExceptionType.unknown => 'Connection to API server failed due to internet connection',
-      DioExceptionType.receiveTimeout => 'Receive timeout in connection with API server',
-      DioExceptionType.sendTimeout => 'Send timeout in connection with API server',
-      DioExceptionType.badResponse when statusCode == 400 => 'Bad request',
-      DioExceptionType.badResponse when statusCode == 404 => 'The requested resource was not found',
-      DioExceptionType.badResponse when statusCode == 500 => 'Internal server error',
+    final data = exception.response!.data!;
+
+    late final String? message;
+    if (data case {'type': String _, 'code': int _, 'message': String msg}) {
+      message = msg;
+    } else {
+      message = null;
+    }
+
+    final errorMessage = switch (exception.type) {
+      DioExceptionType.cancel => message ?? 'Request to API server was cancelled',
+      DioExceptionType.connectionTimeout => message ?? 'Connection timeout with API server',
+      DioExceptionType.unknown => message ?? 'Connection to API server failed due to internet connection',
+      DioExceptionType.receiveTimeout => message ?? 'Receive timeout in connection with API server',
+      DioExceptionType.sendTimeout => message ?? 'Send timeout in connection with API server',
+      DioExceptionType.badResponse when statusCode == 400 => message ?? 'Bad request',
+      DioExceptionType.badResponse when statusCode == 404 => message ?? 'The requested resource was not found',
+      DioExceptionType.badResponse when statusCode == 500 => message ?? 'Internal server error',
       _ => 'Something went wrong',
     };
-    message;
 
-    return NetworkException._(message);
+    return NetworkException._(errorMessage);
   }
 
   NetworkException._(this.message);
