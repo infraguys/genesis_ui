f4d83fba6e91267d7ce900cda705a3bbe4704a89
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-25 00:45:02 +0300

    remove CustomPainter from SignInScreen

--- lib/src/features/auth/presentation/sign_in_screen/sign_in_screen.dart
+++ lib/src/features/auth/presentation/sign_in_screen/sign_in_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:genesis/src/core/extensions/color_extension.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
@@ -53,11 +52,10 @@ class _SignInScreenState extends State<SignInScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 spacing: 20,
                 children: [
-                  Center(
-                    child: CustomPaint(
-                      size: const Size(200, 200),
-                      painter: LogoPainter(),
-                    ),
+                  SvgPicture.asset(
+                    'assets/images/logo.svg',
+                    width: 250,
+                    height: 250,
                   ),
                   Text(
                     '${context.$.genesis} ${context.$.core}',
@@ -124,61 +122,4 @@ class _SignInScreenState extends State<SignInScreen> {
       context.read<AuthBloc>().add(event);
     }
   }
-}
-
-class LogoPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final cx = size.width / 2;
-    final cy = size.height / 2;
-    final center = Offset(cx, cy);
-
-    // Рисуем шестиугольник "вписанный" в круг радиуса outerR
-    final outerR = min(size.width, size.height) * 0.45;
-    final strokeWidth = outerR * 0.5;
-    final innerR = outerR * 0.4;
-    const sides = 6;
-
-    // Вычисляем вершины правильного шестиугольника
-    final points = List<Offset>.generate(sides, (i) {
-      final angle = -pi / 2 + 2 * pi * i / sides;
-      return center + Offset(cos(angle), sin(angle)) * outerR;
-    });
-
-    // Путь для шестиугольника
-    final hexPath = Path()..addPolygon(points, true);
-
-    // SweepGradient для плавного обтекания всего круга
-    final shader = SweepGradient(
-      startAngle: -pi / 2,
-      endAngle: -pi / 2 + 2 * pi,
-      colors: const [
-        Color(0xFFFFD600), // жёлтый
-        Color(0xFF00E676), // зелёный
-        Color(0xFF2979FF), // синий
-        Color(0xFF651FFF), // пурпурный
-        Color(0xFFD50000), // красный
-        Color(0xFFFFD600), // снова жёлтый для плавного замыкания
-      ],
-      stops: const [0, 0.2, 0.4, 0.6, 0.8, 1],
-    ).createShader(Rect.fromCircle(center: center, radius: outerR));
-
-    // Настройки краски для обводки
-    final paintBorder = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth
-      ..shader = shader
-      ..strokeJoin = StrokeJoin
-          .round // скруглённые стыки
-      ..strokeCap = StrokeCap.round; // скруглённые концы линий
-
-    canvas.drawPath(hexPath, paintBorder);
-
-    // Центральный белый круг
-    final paintCircle = Paint()..color = Colors.white;
-    canvas.drawCircle(center, innerR, paintCircle);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
