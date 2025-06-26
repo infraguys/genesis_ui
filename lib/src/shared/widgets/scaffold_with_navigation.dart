a0cf0ec0305914ae1701b55ee660f36127d55d3e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-26 10:39:09 +0300

    Add me_appbar_widget

--- lib/src/shared/widgets/scaffold_with_navigation.dart
+++ lib/src/shared/widgets/scaffold_with_navigation.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
+import 'package:genesis/src/shared/widgets/me_appbar_widget.dart';
 import 'package:go_router/go_router.dart';
 
 class ScaffoldWithNavigation extends StatelessWidget {
@@ -11,6 +12,7 @@ class ScaffoldWithNavigation extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
   void goBranch(int index) {
+    if (index == 2) {}
     navigationShell.goBranch(
       index,
       initialLocation: index == navigationShell.currentIndex,
@@ -49,129 +51,41 @@ class ScaffoldWithNavigation extends StatelessWidget {
               child: const Icon(Icons.notifications_none_outlined),
             ),
             const SizedBox(width: 24),
-            PopupMenuButton<void>(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  onTap: () {
-                    context.read<AuthBloc>().add(AuthEvent.signOut());
-                  },
-                  child: Text('Sign Out'),
-                ),
-              ],
-              child: const Icon(Icons.account_circle_outlined),
-            ),
-            // AccountButtoon(),
-            // ShowFollowerButtontton(adjustmentLabel: 'ececec', adjustFollower: adjustFollower)
+            MeAppbarWidget(),
+            // PopupMenuButton<void>(
+            //   itemBuilder: (context) => [
+            //     PopupMenuItem(
+            //       onTap: () {
+            //         context.read<AuthBloc>().add(AuthEvent.signOut());
+            //       },
+            //       child: Text('Sign Out'),
+            //     ),
+            //   ],
+            //   child: const Icon(Icons.account_circle_outlined),
+            // ),
           ],
         ),
         drawer: NavigationDrawer(
           tilePadding: EdgeInsetsGeometry.zero,
           selectedIndex: navigationShell.currentIndex,
-          onDestinationSelected: goBranch,
+          onDestinationSelected: (index) {
+            if (index == 2) {
+              context.read<AuthBloc>().add(AuthEvent.signOut());
+            } else {
+              goBranch(index);
+            }
+          },
           children: [
             NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text(context.$.dashboard)),
             NavigationDrawerDestination(icon: Icon(Icons.laptop), label: Text('Monitoring'.hardcoded)),
+            Divider(height: 1, color: Colors.grey.shade200),
+            NavigationDrawerDestination(
+              icon: Icon(Icons.logout),
+              label: Text('Sign out'.hardcoded),
+            ),
           ],
         ),
         body: navigationShell,
-      ),
-    );
-  }
-}
-
-// class AccountButtoon extends StatefulWidget {
-//   const AccountButtoon({super.key});
-//
-//   @override
-//   State<AccountButtoon> createState() => _AccountButtoonState();
-// }
-//
-// class _AccountButtoonState extends State<AccountButtoon> {
-//   late Offset g;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       child: IconButton(
-//         onPressed: () {
-//           final overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
-//           final position = RelativeRect.fromLTRB(
-//             g.dx,
-//             g.dy,
-//             overlay.size.width - g.dx,
-//             overlay.size.height - g.dy,
-//           );
-//           showMenu<Widget>(
-//             context: context,
-//             position: position,
-//             items: [
-//               PopupMenuItem(
-//                 child: BlocBuilder<AuthBloc, AuthState>(
-//                   builder: (context, state) {
-//                     final user = (state as Authenticated).iamClient.user;
-//                     return Container(
-//                       width: 200,
-//                       child: Padding(
-//                         padding: const EdgeInsets.all(8.0),
-//                         child: Column(
-//                           children: [Text('Привет, ${user.name}')],
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                 ),
-//               ),
-//               PopupMenuItem(
-//                 onTap: () {
-//                   context.read<AuthBloc>().add(AuthEvent.signOut());
-//                 },
-//                 child: Text('Sign Out'),
-//               ),
-//             ],
-//           );
-//         },
-//         icon: Icon(Icons.account_circle_outlined),
-//       ),
-//
-//       onTapDown: (details) {
-//         g = details.globalPosition;
-//       },
-//     );
-//   }
-// }
-
-// todo(E.Koretsky): Временное решение.
-class ShowFollowerButton extends StatelessWidget {
-  final LayerLink layerLink = LayerLink();
-  final Widget Function(CompositedTransformFollower follower) adjustFollower;
-  final String adjustmentLabel;
-
-  ShowFollowerButton({required this.adjustmentLabel, required this.adjustFollower});
-
-  @override
-  Widget build(BuildContext context) {
-    return CompositedTransformTarget(
-      link: layerLink,
-      child: ElevatedButton(
-        onPressed: () async {
-          var follower = CompositedTransformFollower(
-            link: layerLink,
-            targetAnchor: Alignment.bottomLeft,
-            child: Container(
-              width: 200,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [Text('Привет, Joe')],
-                ),
-              ),
-            ),
-          );
-          var entry = OverlayEntry(builder: (context) => adjustFollower(follower));
-          Overlay.of(context).insert(entry);
-          Future.delayed(Duration(seconds: 1), () => entry.remove());
-        },
-        child: Text('Show $adjustmentLabel Follower'),
       ),
     );
   }
