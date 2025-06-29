2330be6951095f74e32352f147ca2c85b42b0e15
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-29 23:28:00 +0300

    add IamClientConfig in Env

--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -1,3 +1,8 @@
 abstract class Env {
   static const apiUrl = String.fromEnvironment('api_url');
+
+  // Iam client config
+  static const iamClientUuid = String.fromEnvironment('iam_client_uuid');
+  static const clientId = String.fromEnvironment('client_id');
+  static const clientSecret = String.fromEnvironment('client_secret');
 }
