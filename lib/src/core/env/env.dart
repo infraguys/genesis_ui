826a3f68752d410e458d6100df8785de223cf62f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-29 23:57:58 +0300

    add newField in Env

--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -5,4 +5,8 @@ abstract class Env {
   static const iamClientUuid = String.fromEnvironment('iam_client_uuid');
   static const clientId = String.fromEnvironment('client_id');
   static const clientSecret = String.fromEnvironment('client_secret');
+  static const grantType = String.fromEnvironment('grant_type');
+  static const ttl = int.fromEnvironment('ttl');
+  static const refreshTtl = int.fromEnvironment('refresh_ttl');
+  static const scope = String.fromEnvironment('scope');
 }
