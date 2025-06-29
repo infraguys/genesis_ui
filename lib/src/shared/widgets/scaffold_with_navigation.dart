222efca39f4c1ce0d827cc5e100749e29bb00fb5
Eugene Koretsky <E.Koretsky@outlook.com>
2025-06-30 02:57:26 +0300

    remove goBranch function from scaffold_with_navigation.dart

--- lib/src/shared/widgets/scaffold_with_navigation.dart
+++ lib/src/shared/widgets/scaffold_with_navigation.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
+import 'package:genesis/src/routing/app_router.dart';
 import 'package:genesis/src/shared/widgets/me_appbar_widget.dart';
 import 'package:go_router/go_router.dart';
 
@@ -10,14 +11,6 @@ class ScaffoldWithNavigation extends StatelessWidget {
   const ScaffoldWithNavigation({required this.navigationShell, super.key});
 
   final StatefulNavigationShell navigationShell;
-
-  void goBranch(int index) {
-    if (index == 2) {}
-    navigationShell.goBranch(
-      index,
-      initialLocation: index == navigationShell.currentIndex,
-    );
-  }
 
   @override
   Widget build(context) {
@@ -37,55 +30,126 @@ class ScaffoldWithNavigation extends StatelessWidget {
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            PopupMenuButton<void>(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  child: const Text('Notification'),
-                  onTap: () {
-                    // Sign out logic
-                  },
-                ),
-              ],
-              child: const Icon(Icons.notifications_none_outlined),
-            ),
-            const SizedBox(width: 24),
-            MeAppbarWidget(),
-            // PopupMenuButton<void>(
-            //   itemBuilder: (context) => [
-            //     PopupMenuItem(
-            //       onTap: () {
-            //         context.read<AuthBloc>().add(AuthEvent.signOut());
-            //       },
-            //       child: Text('Sign Out'),
-            //     ),
-            //   ],
-            //   child: const Icon(Icons.account_circle_outlined),
-            // ),
-          ],
-        ),
-        drawer: NavigationDrawer(
-          tilePadding: EdgeInsetsGeometry.zero,
-          selectedIndex: navigationShell.currentIndex,
-          onDestinationSelected: (index) {
-            if (index == 2) {
-              context.read<AuthBloc>().add(AuthEvent.signOut());
-            } else {
-              goBranch(index);
-            }
-          },
+        // appBar: AppBar(
+        //
+        //   backgroundColor: Colors.green,
+        //   actions: [
+        //     PopupMenuButton<void>(
+        //       itemBuilder: (context) => [
+        //         PopupMenuItem(
+        //           child: const Text('Notification'),
+        //           onTap: () {
+        //             // Sign out logic
+        //           },
+        //         ),
+        //       ],
+        //       child: const Icon(Icons.notifications_none_outlined),
+        //     ),
+        //     const SizedBox(width: 24),
+        //     MeAppbarWidget(),
+        //     // PopupMenuButton<void>(
+        //     //   itemBuilder: (context) => [
+        //     //     PopupMenuItem(
+        //     //       onTap: () {
+        //     //         context.read<AuthBloc>().add(AuthEvent.signOut());
+        //     //       },
+        //     //       child: Text('Sign Out'),
+        //     //     ),
+        //     //   ],
+        //     //   child: const Icon(Icons.account_circle_outlined),
+        //     // ),
+        //   ],
+        // ),
+        // drawer: NavigationDrawer(
+        //   tilePadding: EdgeInsetsGeometry.zero,
+        //   selectedIndex: navigationShell.currentIndex,
+        //   onDestinationSelected: (index) {
+        //     // if (index == 2) {
+        //     //   context.read<AuthBloc>().add(AuthEvent.signOut());
+        //     // } else {
+        //       goBranch(index);
+        //     // }
+        //   },
+        //   children: [
+        //     NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text(context.$.dashboard)),
+        //     NavigationDrawerDestination(icon: Icon(Icons.supervised_user_circle_sharp), label: Text('Iam')),
+        //     // NavigationDrawerDestination(icon: Icon(Icons.laptop), label: Text('Monitoring'.hardcoded)),
+        //     // Divider(height: 1, color: Colors.grey.shade200),
+        //     // NavigationDrawerDestination(
+        //     //   icon: Icon(Icons.logout),
+        //     //   label: Text('Sign out'.hardcoded),
+        //     // ),
+        //   ],
+        // ),
+        body: Row(
           children: [
-            NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text(context.$.dashboard)),
-            NavigationDrawerDestination(icon: Icon(Icons.laptop), label: Text('Monitoring'.hardcoded)),
-            Divider(height: 1, color: Colors.grey.shade200),
-            NavigationDrawerDestination(
-              icon: Icon(Icons.logout),
-              label: Text('Sign out'.hardcoded),
+            Drawer(
+              backgroundColor: Colors.grey.shade200,
+              width: 220,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Container(
+                      decoration: BoxDecoration(border: BoxBorder.fromBorderSide(BorderSide.none)),
+                      child: SvgPicture.asset(
+                        'assets/images/logo.svg',
+                        width: 50,
+                        height: 50,
+                      ),
+                      margin: EdgeInsets.zero,
+                    ),
+                  ),
+                  ListTile(title: Text('dashboard'.hardcoded)),
+                  ExpansionTile(
+                    title: Text('iam'.hardcoded),
+                    children: [
+                      ListTile(
+                        title: Text('Пользователи'.hardcoded),
+                        onTap: () => context.goNamed(AppRoutes.users.name),
+                      ),
+                      ListTile(
+                        title: Text('Проекты'.hardcoded),
+                        onTap: () => context.goNamed(AppRoutes.projects.name),
+                      ),
+                      ListTile(
+                        title: Text('Роли'.hardcoded),
+                        onTap: () => context.goNamed(AppRoutes.roles.name),
+                      ),
+                    ],
+                  ),
+                  ListTile(
+                    title: Text('dashboard'.hardcoded),
+                  ),
+                ],
+              ),
+            ),
+            VerticalDivider(color: Colors.white, endIndent: 50, indent: 50, width: 1),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        spacing: 20,
+                        children: [const Icon(Icons.notifications_none_outlined), MeAppbarWidget()],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: navigationShell,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-        body: navigationShell,
       ),
     );
   }
