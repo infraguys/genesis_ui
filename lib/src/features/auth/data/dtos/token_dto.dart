698d2c46f319b44e72083d2238d62b29605e1509
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-02 03:15:58 +0300

    fix new fields in TokenDto

--- lib/src/features/auth/data/dtos/token_dto.dart
+++ lib/src/features/auth/data/dtos/token_dto.dart
@@ -6,11 +6,13 @@ part 'token_dto.g.dart';
 class TokenDto {
   TokenDto({
     required this.accessToken,
+    required this.tokenType,
     required this.expiresAt,
     required this.idToken,
     required this.refreshToken,
     required this.scope,
-    required this.tokenType,
+    required this.expiresIn,
+    required this.refreshExpiresIn,
   });
 
   factory TokenDto.fromJson(Map<String, dynamic> json) => _$TokenDtoFromJson(json);
@@ -18,10 +20,12 @@ class TokenDto {
   static DateTime _fromTimestamp(int timestamp) => DateTime.fromMillisecondsSinceEpoch(timestamp);
 
   final String accessToken;
+  final String tokenType;
   @JsonKey(fromJson: _fromTimestamp)
   final DateTime expiresAt;
+  final int expiresIn;
   final String idToken;
   final String refreshToken;
+  final int refreshExpiresIn;
   final String scope;
-  final String tokenType;
 }
