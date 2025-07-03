eb8b14d248d0ef7336ef4e5a897e0bcd765bc773
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-03 08:47:00 +0300

    add users_page.dart widget

--- lib/src/features/users/presentation/users_page.dart
+++ lib/src/features/users/presentation/users_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/features/users/presentation/blocs/users_bloc/users_bloc.dart';
+import 'package:genesis/src/features/users/presentation/widgets/users_list_details.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({super.key});
@@ -30,55 +30,7 @@ class _UsersPageState extends State<UsersPage> {
             itemCount: usersState.users.length,
             itemBuilder: (context, index) {
               final currentUser = usersState.users[index];
-              return ExpansionTile(
-                title: Row(
-                  spacing: 100,
-                  children: [
-                    Text(currentUser.firstName),
-                    Text(currentUser.status.name),
-                  ],
-                ),
-                leading: Icon(Icons.play_arrow, color: Colors.green),
-                trailing: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-                expandedAlignment: Alignment.centerLeft,
-                children: [
-                  Row(
-                    spacing: 100,
-                    children: [
-                      Text('Description'.hardcoded),
-                      Text(currentUser.description),
-                    ],
-                  ),
-                  Row(
-                    spacing: 100,
-                    children: [
-                      Text('First name'.hardcoded),
-                      Text(currentUser.firstName),
-                    ],
-                  ),
-                  Row(
-                    spacing: 100,
-                    children: [
-                      Text('Last name'.hardcoded),
-                      Text(currentUser.lastName),
-                    ],
-                  ),
-                  Row(
-                    spacing: 100,
-                    children: [
-                      Text('email'.hardcoded),
-                      Text(currentUser.email),
-                    ],
-                  ),
-                  Row(
-                    spacing: 100,
-                    children: [
-                      Text('Created at'.hardcoded),
-                      Text(currentUser.createdAt.toString()),
-                    ],
-                  ),
-                ],
-              );
+              return UsersListDetails(user: currentUser);
             },
             separatorBuilder: (context, index) {
               return Divider(color: Colors.white, indent: 100, endIndent: 100);
