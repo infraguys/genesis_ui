594fa32a5f38eac3bafa64763dbc3a126bf2663b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-04 03:31:29 +0300

    add forms in UserPageˆ

--- lib/src/features/users/presentation/widgets/user_page.dart
+++ lib/src/features/users/presentation/widgets/user_page.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:genesis/src/features/auth/domain/entity/user.dart';
 
-class UserPage extends StatelessWidget {
-  const UserPage({required this.userId, super.key});
+class UserPage extends StatefulWidget {
+  const UserPage({required this.user, super.key});
 
-  final String userId;
+  final User user;
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  late final TextEditingController _firstNameController;
+  late final TextEditingController _lastNameController;
+
+  @override
+  void initState() {
+    _firstNameController = TextEditingController(text: widget.user.firstName);
+    _lastNameController = TextEditingController(text: widget.user.firstName);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Петя - $userId'),
-        // Text('${user.firstName} ${user.lastName}')
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 24,
+          children: [
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                enabled: false,
+                decoration: InputDecoration(filled: true, fillColor: Colors.white),
+                controller: _firstNameController,
+              ),
+            ),
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                enabled: false,
+                controller: _lastNameController,
+                decoration: InputDecoration(filled: true, fillColor: Colors.white),
+              ),
+            ),
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                decoration: InputDecoration(filled: true, fillColor: Colors.white),
+              ),
+            ),
+            SizedBox(
+              width: 400,
+              child: TextFormField(
+                decoration: InputDecoration(filled: true, fillColor: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
