931e09674c6f1ec8ebfbc2e85e69315fb719aeab
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-08 16:25:01 +0300

    remove old methods in UsersEvent

--- lib/src/presentation/features/users/blocs/users_bloc/users_event.dart
+++ lib/src/presentation/features/users/blocs/users_bloc/users_event.dart
@@ -3,13 +3,7 @@ part of 'users_bloc.dart';
 sealed class UsersEvent {
   factory UsersEvent.getUsers() = _GetUsers;
 
-  factory UsersEvent.blockUser() = _BlockUser;
 
-  factory UsersEvent.listenDependencies() = _ListenDependencies;
 }
 
 final class _GetUsers implements UsersEvent {}
-
-final class _BlockUser implements UsersEvent {}
-
-final class _ListenDependencies implements UsersEvent {}
