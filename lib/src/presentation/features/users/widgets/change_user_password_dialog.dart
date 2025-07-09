f7bb2176d1bb5b235e51842e86ba7ea964e6f4a4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-09 04:47:04 +0300

    refactor ChangeUserPasswordDialog to use FormControllers for improved form management

--- lib/src/presentation/features/users/widgets/change_user_password_dialog.dart
+++ lib/src/presentation/features/users/widgets/change_user_password_dialog.dart
@@ -2,38 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
+import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/domain/entities/user.dart';
 import 'package:genesis/src/presentation/features/users/blocs/user_bloc/user_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class ChangeUserPasswordDialog extends StatefulWidget {
-  const ChangeUserPasswordDialog({super.key, required this.user});
-
-  final User user;
+  const ChangeUserPasswordDialog({super.key});
 
   @override
   State<ChangeUserPasswordDialog> createState() => _ChangeUserPasswordDialogState();
 }
 
 class _ChangeUserPasswordDialogState extends State<ChangeUserPasswordDialog> {
-  final _oldPasswordController = TextEditingController();
-  final _newPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _controllers = _ChangePasswordFormControllers();
 
-  bool get isOkBtnEnabled {
-    return _oldPasswordController.text.isNotEmpty && _newPasswordController.text.isNotEmpty;
+  @override
+  void dispose() {
+    _controllers.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       content: Form(
+        key: _formKey,
         child: Column(
           spacing: 12,
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(context.$.changeUserPassword),
             TextFormField(
-              controller: _oldPasswordController,
+              controller: _controllers.oldPassword,
               autovalidateMode: AutovalidateMode.onUnfocus,
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(hintText: context.$.oldPassword),
@@ -46,7 +48,7 @@ class _ChangeUserPasswordDialogState extends State<ChangeUserPasswordDialog> {
               },
             ),
             TextFormField(
-              controller: _newPasswordController,
+              controller: _controllers.newPassword,
               autovalidateMode: AutovalidateMode.onUnfocus,
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(hintText: context.$.newPassword),
@@ -64,10 +66,10 @@ class _ChangeUserPasswordDialogState extends State<ChangeUserPasswordDialog> {
       actions: [
         TextButton(onPressed: context.pop, child: Text(context.$.cancel)),
         ListenableBuilder(
-          listenable: Listenable.merge([_oldPasswordController, _newPasswordController]),
+          listenable: Listenable.merge(_controllers.all),
           builder: (context, _) {
             return TextButton(
-              onPressed: null,
+              onPressed: _controllers.allFilled ? () => changePassword(context) : null,
               child: Text(context.$.ok),
             );
           },
@@ -80,7 +82,22 @@ class _ChangeUserPasswordDialogState extends State<ChangeUserPasswordDialog> {
     final userBloc = context.read<UserBloc>();
     final user = context.read<User>();
     userBloc.add(
-      UserEvent.changePassword(uuid: user.uuid, oldPassword: 'cce', newPassword: 'cwcw'),
+      UserEvent.changePassword(
+        uuid: user.uuid,
+        oldPassword: _controllers.oldPassword.text,
+        newPassword: _controllers.newPassword.text,
+      ),
     );
   }
+}
+
+class _ChangePasswordFormControllers extends FormControllers {
+  final oldPassword = TextEditingController();
+  final newPassword = TextEditingController();
+
+  @override
+  List<TextEditingController> get all => [oldPassword, newPassword];
+
+  @override
+  bool get allFilled => all.every((controller) => controller.text.isNotEmpty);
 }
