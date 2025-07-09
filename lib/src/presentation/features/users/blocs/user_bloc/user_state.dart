62beab48aca1f7f5216c2f76f708cdda6c440c04
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-10 01:20:47 +0300

    remove unused user creation states from UserState

--- lib/src/presentation/features/users/blocs/user_bloc/user_state.dart
+++ lib/src/presentation/features/users/blocs/user_bloc/user_state.dart
@@ -5,10 +5,6 @@ sealed class UserState {
 
   factory UserState.loading() = UserStateLoading;
 
-  factory UserState.userCreated() = UserStateCreatedSuccess;
-
-  factory UserState.createdFailure(String message) = UserStateCreatedFailure;
-
   factory UserState.deleteSuccess() = UserStateDeleteSuccess;
 
   factory UserState.changePasswordSuccess() = UserStateChangePasswordSuccess;
@@ -18,14 +14,6 @@ final class UserStateInit implements UserState {}
 
 final class UserStateLoading implements UserState {}
 
-final class UserStateCreatedSuccess implements UserState {}
-
 final class UserStateDeleteSuccess implements UserState {}
 
 final class UserStateChangePasswordSuccess implements UserState {}
-
-final class UserStateCreatedFailure implements UserState {
-  UserStateCreatedFailure(this.message);
-
-  final String message;
-}
