b76888273ca6f42a7f53ffbe60ae64e3a4922801
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-09 03:29:06 +0300

    add abstract FormControllers class for centralized form management

---
+++ lib/src/core/interfaces/form_controllers.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class FormControllers {
+  /// Returns a list of all controllers.
+  List<TextEditingController> get all;
+
+  /// Returns true if all controllers have text.
+  bool get allFilled;
+
+  /// Disposes all controllers.
+  void dispose() {
+    for (final controller in all) {
+      controller.dispose();
+    }
+  }
+}
