f0cef87ebfe21ef2ab48f75d1667440111477028
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-11 10:15:24 +0300

    add Project class and ProjectStatus enum for project

---
+++ lib/src/features/projects/domain/entities/project.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+class Project extends Equatable {
+  const Project({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.status,
+    required this.organization,
+  });
+
+  final String id;
+  final String name;
+  final String description;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final ProjectStatus status;
+  final String organization;
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    description,
+    createdAt,
+    updatedAt,
+    status,
+    organization,
+  ];
+}
+
+enum ProjectStatus {
+  newProject,
+}
