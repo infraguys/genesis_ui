74743e90949a32939f913ef5aba23147c39a5c99
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-11 10:43:30 +0300

    implement IProjectsRepository interface for project management operations

---
+++ lib/src/features/projects/domain/repositories/i_projects_repository.dart
@@ -0,0 +1,11 @@
+import 'package:genesis/src/features/projects/domain/entities/project.dart';
+import 'package:genesis/src/features/projects/domain/params/create_project_params.dart';
+import 'package:genesis/src/features/projects/domain/params/update_project_paramas.dart';
+
+abstract interface class IProjectsRepository {
+  Future<Project> createProject(CreateProjectParams params);
+
+  Future<void> deleteProject(String uuid);
+
+  Future<Project> updateProject(UpdateProjectParams params);
+}
