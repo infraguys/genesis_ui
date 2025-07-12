5f6e2498d7d6ce5ea689ca6f751c5125c7e17250
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-13 02:06:32 +0300

    remove iam_client.dart

--- lib/src/features/auth/domain/auth_entities/iam_client.dart
+++
@@ -1,9 +0,0 @@
-import 'package:genesis/src/features/common/shared_entities/organization.dart';
-import 'package:genesis/src/features/common/shared_entities/user.dart';
-
-class IamClient {
-  IamClient({required this.user, required this.organizations});
-
-  final User user;
-  final List<Organization> organizations;
-}
