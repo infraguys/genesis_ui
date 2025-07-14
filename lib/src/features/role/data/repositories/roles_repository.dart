e59c821a6eac6556e4e6afb31f9c3a29dcf72ae8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-14 05:15:06 +0300

    feat: implement getRolesByUserUuid method in RolesRepository to retrieve user roles

--- lib/src/features/role/data/repositories/roles_repository.dart
+++ lib/src/features/role/data/repositories/roles_repository.dart
@@ -1,10 +1,15 @@
 import 'package:genesis/src/features/common/shared_entities/role.dart';
+import 'package:genesis/src/features/role/data/source/remote/i_roles_api.dart';
 import 'package:genesis/src/features/role/domain/repositories/i_roles_repositories.dart';
 
 final class RolesRepository implements IRolesRepository {
+  RolesRepository(this._rolesApi);
+
+  final IRolesApi _rolesApi;
+
   @override
-  Future<List<Role>> getRolesByUserUuid(String userUuid) {
-    // TODO: implement getUserRoles
-    throw UnimplementedError();
+  Future<List<Role>> getRolesByUserUuid(String userUuid) async {
+    final rolesDto = await _rolesApi.getRolesByUserUuid(userUuid);
+    return rolesDto.map((dto) => dto.toEntity()).toList();
   }
 }
