a84dde6002725e1ce5877186db4f6adab6566927
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-14 17:43:31 +0300

    chore: remove commented-out code in roles_bindings.dart

--- lib/src/features/projects/data/dtos/roles_bindings.dart
+++ lib/src/features/projects/data/dtos/roles_bindings.dart
@@ -34,10 +34,3 @@ class RolesBindingsDto implements IDto<dynamic> {
     throw UnimplementedError();
   }
 }
-
-//"uuid": "e82011d4-5ab2-41bf-9df0-13c8c3bfe987",
-//     "created_at": "2025-07-08T23:02:24.965314Z",
-//     "updated_at": "2025-07-08T23:02:24.965316Z",
-//     "status": "ACTIVE",
-//     "user": "/v1/iam/users/c35e94de-fe43-4a3a-9ea1-c50f4289b7ab",
-//     "role": "/v1/iam/roles/726f6c65-0000-0000-0000-000000000001"
