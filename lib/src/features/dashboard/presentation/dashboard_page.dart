a90425e16e7b89f3dc3a79ebb070bfc769a41d7b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-15 10:39:56 +0300

    feat: convert DashboardPage to StatefulWidget and initialize UsersBloc in initState

--- lib/src/features/dashboard/presentation/dashboard_page.dart
+++ lib/src/features/dashboard/presentation/dashboard_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
-import 'package:genesis/src/features/common/shared_widgets/active_users_card.dart';
 import 'package:genesis/src/features/common/shared_widgets/page_header.dart';
 import 'package:genesis/src/features/common/shared_widgets/summary_card.dart';
 import 'package:genesis/src/features/dashboard/presentation/widgets/table_view/table_view.dart';
+import 'package:genesis/src/features/users/presentation/blocs/users_bloc/users_bloc.dart';
+import 'package:genesis/src/features/users/presentation/widgets/active_users_card.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  void initState() {
+    context.read<UsersBloc>().add(UsersEvent.getUsers());
+    super.initState();
+  }
 
   @override
   Widget build(context) {
