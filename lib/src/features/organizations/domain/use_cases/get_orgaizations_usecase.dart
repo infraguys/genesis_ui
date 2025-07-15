6b6c1dae511907a64eb8ea84f33ced401838fefb
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-15 16:21:23 +0300

    add getOrganizationsUsecase

---
+++ lib/src/features/organizations/domain/use_cases/get_orgaizations_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:genesis/src/features/common/shared_entities/organization.dart';
+
+final class GetOrganizationsUseCase {
+  const GetOrganizationsUseCase();
+
+  Future<List<Organization>> call() async {
+    throw UnimplementedError();
+  }
+}
