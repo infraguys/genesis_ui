88917096bca05eac46685ff3763e327db286d5c1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-15 16:40:41 +0300

    feat: add IOrganizationsRepository interface for organization management

---
+++ lib/src/features/organizations/domain/repositories/i_organizations_repository.dart
@@ -0,0 +1,18 @@
+import 'package:genesis/src/features/common/shared_entities/organization.dart';
+import 'package:genesis/src/features/organizations/domain/params/create_organization_params.dart';
+import 'package:genesis/src/features/organizations/domain/params/get_organizations_params.dart';
+import 'package:genesis/src/features/organizations/domain/params/update_organiztion_params.dart';
+
+abstract interface class IOrganizationsRepository {
+  const IOrganizationsRepository();
+
+  Future<List<Organization>> getOrganizations(GetOrganizationsParams params);
+
+  Future<Organization> createOrganization(CreateOrganizationParams params);
+
+  Future<Organization> updateOrganization(UpdateOrganizationParams params);
+
+  Future<void> deleteOrganization(String uuid);
+
+  Future<Organization> getOrganizationByUuid(String uuid);
+}
