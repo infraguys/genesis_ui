dfff50984c1f6615478cb6f1619403f71a5ebbf0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-16 10:44:36 +0300

    feat: add delete project functionality to ProjectBloc

--- lib/src/features/projects/presentation/blocs/project_bloc/project_bloc.dart
+++ lib/src/features/projects/presentation/blocs/project_bloc/project_bloc.dart
@@ -3,6 +3,7 @@ import 'package:genesis/src/features/projects/domain/entities/project.dart';
 import 'package:genesis/src/features/projects/domain/params/create_project_params.dart';
 import 'package:genesis/src/features/projects/domain/repositories/i_projects_repository.dart';
 import 'package:genesis/src/features/projects/domain/use_cases/create_project_usecase.dart';
+import 'package:genesis/src/features/projects/domain/use_cases/delete_project_usecase.dart';
 
 part 'project_event.dart';
 part 'project_state.dart';
@@ -10,6 +11,7 @@ part 'project_state.dart';
 class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
   ProjectBloc(this._projectsRepository) : super(ProjectInitialState()) {
     on(_onCreateProject);
+    on(_onDeleteProject);
   }
 
   final IProjectsRepository _projectsRepository;
@@ -26,5 +28,12 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
       ),
     );
     emit(ProjectCreatedState(createdProject));
+  }
+
+  Future<void> _onDeleteProject(_DeleteProjectEvent event, Emitter<ProjectState> emit) async {
+    final useCase = DeleteProjectUseCase(_projectsRepository);
+    emit(ProjectLoadingState());
+    await useCase(event.projectUuid);
+    emit(ProjectDeletedState());
   }
 }
