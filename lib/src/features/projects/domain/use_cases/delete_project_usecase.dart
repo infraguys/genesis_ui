4d04d7806ab7af9a31ca16a123ec2d92791041b9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-16 10:38:45 +0300

    feat: update DeleteProjectUseCase to use IProjectsRepository interface

--- lib/src/features/projects/domain/use_cases/delete_project_usecase.dart
+++ lib/src/features/projects/domain/use_cases/delete_project_usecase.dart
@@ -1,9 +1,9 @@
-import 'package:genesis/src/features/projects/data/repositories/projects_repository.dart';
+import 'package:genesis/src/features/projects/domain/repositories/i_projects_repository.dart';
 
 final class DeleteProjectUseCase {
   DeleteProjectUseCase(this._repository);
 
-  final ProjectsRepository _repository;
+  final IProjectsRepository _repository;
 
   Future<void> call(String projectUuid) async {
     await _repository.deleteProject(projectUuid);
