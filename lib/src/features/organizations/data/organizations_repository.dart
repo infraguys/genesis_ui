4a1950f45414eb4d14aa7a05281c5848b3cabe9e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-16 10:03:19 +0300

    feat: implement OrganizationsRepository with unimplemented CRUD methods for organization management

---
+++ lib/src/features/organizations/data/organizations_repository.dart
@@ -0,0 +1,37 @@
+import 'package:genesis/src/features/common/shared_entities/organization.dart';
+import 'package:genesis/src/features/organizations/domain/params/create_organization_params.dart';
+import 'package:genesis/src/features/organizations/domain/params/get_organizations_params.dart';
+import 'package:genesis/src/features/organizations/domain/params/update_organiztion_params.dart';
+import 'package:genesis/src/features/organizations/domain/repositories/i_organizations_repository.dart';
+
+final class OrganizationsRepository implements IOrganizationsRepository {
+  @override
+  Future<Organization> createOrganization(CreateOrganizationParams params) {
+    // TODO: implement createOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteOrganization(String uuid) {
+    // TODO: implement deleteOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Organization> getOrganizationByUuid(String uuid) {
+    // TODO: implement getOrganizationByUuid
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Organization>> getOrganizations(GetOrganizationsParams params) {
+    // TODO: implement getOrganizations
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Organization> updateOrganization(UpdateOrganizationParams params) {
+    // TODO: implement updateOrganization
+    throw UnimplementedError();
+  }
+}
