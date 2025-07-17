a2444b9d8b6a762bfe2b6e97e41d3b4d937c285a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-17 05:38:34 +0300

    feat: add getOrganizationByUser method to organizations repository

---
+++ lib/src/features/organizations/data/source/remote/organizations_api.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:genesis/src/core/exceptions/network_exception.dart';
+import 'package:genesis/src/core/rest_client/rest_client.dart';
+import 'package:genesis/src/features/common/shared_entities/organization.dart';
+import 'package:genesis/src/features/organizations/data/source/remote/i_organizations_api.dart';
+
+final class OrganizationsApi implements IOrganizationsApi {
+  OrganizationsApi(this._client);
+
+  final RestClient _client;
+
+  static const _organizationMembersUrl = '/v1/iam/organization_member/';
+
+  @override
+  Future<Organization> createOrganization() {
+    // TODO: implement createOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteOrganization(String uuid) {
+    // TODO: implement deleteOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Organization>> getOrganizationByUser(String userUuid) async {
+    final url = '$_organizationMembersUrl?user=$userUuid';
+    try {
+      final Response(:data) = await _client.get<List<dynamic>>(url);
+      if (data != null) {
+        final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
+        // return castedData.map((it) => OrganizationD.fromJson(it)).toList();
+      }
+      return [];
+    } on DioException catch (e) {
+      throw NetworkException(e);
+    }
+  }
+
+  @override
+  Future<Organization> getOrganizationByUuid(String uuid) {
+    // TODO: implement getOrganizationByUuid
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Organization>> getOrganizations() {
+    // TODO: implement getOrganizations
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Organization> updateOrganization() {
+    // TODO: implement updateOrganization
+    throw UnimplementedError();
+  }
+}
