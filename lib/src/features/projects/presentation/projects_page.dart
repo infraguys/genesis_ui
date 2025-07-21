2ae022cf1ecf5d206c0042e484e48f90fb83c262
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-21 17:59:34 +0300

    feat: add BlocListener for ProjectBloc to refresh projects on deletion

--- lib/src/features/projects/presentation/projects_page.dart
+++ lib/src/features/projects/presentation/projects_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:genesis/src/features/projects/presentation/blocs/auth_user_projects_bloc/auth_user_projects_bloc.dart';
+import 'package:genesis/src/features/projects/presentation/blocs/project_bloc/project_bloc.dart';
 import 'package:genesis/src/features/projects/presentation/widgets/create_project_dialog.dart';
 import 'package:genesis/src/features/projects/presentation/widgets/project_action_popup_menu_button.dart';
 import 'package:provider/provider.dart';
@@ -18,99 +19,105 @@ class ProjectsPage extends StatefulWidget {
 class _ProjectsPageState extends State<ProjectsPage> {
   @override
   void initState() {
-    final authState = context.read<AuthBloc>().state;
-    if (authState is AuthenticatedAuthState) {
-      context.read<AuthUserProjectsBloc>().add(AuthUserProjectsEvent.getProjects(authState.user.uuid));
-    }
+    final authState = context.read<AuthBloc>().state as AuthenticatedAuthState;
+    context.read<AuthUserProjectsBloc>().add(AuthUserProjectsEvent.getProjects(authState.user.uuid));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Column(
-      spacing: 24,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Проекты',
-          style: textTheme.headlineMedium,
-        ),
-        BlocBuilder<AuthUserProjectsBloc, AuthUserProjectsState>(
-          builder: (context, state) {
-            if (state is! AuthUserProjectsLoadedState) {
-              return Center(child: CupertinoActivityIndicator());
-            }
-            return GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 320,
-                mainAxisExtent: 250,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemCount: state.projects.length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return Material(
-                    color: Colors.transparent,
-                    child: Card(
-                      clipBehavior: Clip.hardEdge,
-                      child: InkWell(
-                        onTap: () {
-                          showDialog<void>(
-                            context: context,
-                            builder: (context) {
-                              return CreateProjectDialog();
-                            },
-                          );
-                        },
-                        child: Center(
-                          child: Icon(Icons.add),
+    return BlocListener<ProjectBloc, ProjectState>(
+      listener: (context, state) {
+        if (state is ProjectDeletedState) {
+          final authState = context.read<AuthBloc>().state as AuthenticatedAuthState;
+          context.read<AuthUserProjectsBloc>().add(AuthUserProjectsEvent.getProjects(authState.user.uuid));
+        }
+      },
+      child: Column(
+        spacing: 24,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Проекты',
+            style: textTheme.headlineMedium,
+          ),
+          BlocBuilder<AuthUserProjectsBloc, AuthUserProjectsState>(
+            builder: (context, state) {
+              if (state is! AuthUserProjectsLoadedState) {
+                return Center(child: CupertinoActivityIndicator());
+              }
+              return GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 320,
+                  mainAxisExtent: 250,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: state.projects.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Material(
+                      color: Colors.transparent,
+                      child: Card(
+                        clipBehavior: Clip.hardEdge,
+                        child: InkWell(
+                          onTap: () {
+                            showDialog<void>(
+                              context: context,
+                              builder: (context) {
+                                return CreateProjectDialog();
+                              },
+                            );
+                          },
+                          child: Center(
+                            child: Icon(Icons.add),
+                          ),
                         ),
+                      ),
+                    );
+                  }
+                  final project = state.projects[index - 1];
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        spacing: 4,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                project.name,
+                                style: textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+                              ),
+                              Provider.value(
+                                value: project,
+                                child: ProjectActionPopupMenuButton(),
+                              ),
+                            ],
+                          ),
+                          Text(project.createdAt.toString(), style: textTheme.bodySmall),
+                          Text(project.description, style: textTheme.bodySmall),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            context.$.roles,
+                            style: textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
                     ),
                   );
-                }
-                final project = state.projects[index - 1];
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      spacing: 4,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              project.name,
-                              style: textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                            Provider.value(
-                              value: project,
-                              child: ProjectActionPopupMenuButton(),
-                            ),
-                          ],
-                        ),
-                        Text(project.createdAt.toString(), style: textTheme.bodySmall),
-                        Text(project.description, style: textTheme.bodySmall),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          context.$.roles,
-                          style: textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      ],
+                },
+              );
+            },
+          ),
+        ],
+      ),
     );
     // return BlocListener<ProjectBloc, ProjectState>(
     //   listener: (context, state) {
