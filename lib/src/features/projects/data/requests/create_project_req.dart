baa5fc72522d09f9656c3d2e878514dad68372e1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-22 04:25:59 +0300

    feat: simplify CreateProjectReq constructor and remove unused enum

--- lib/src/features/projects/data/requests/create_project_req.dart
+++ lib/src/features/projects/data/requests/create_project_req.dart
@@ -1,27 +1,17 @@
 import 'package:genesis/src/core/interfaces/i_req.dart';
-import 'package:genesis/src/features/projects/domain/entities/project.dart';
+import 'package:genesis/src/features/projects/data/requests/project_status_req.dart';
 import 'package:genesis/src/features/projects/domain/params/create_project_params.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'create_project_req.g.dart';
 
-@JsonSerializable(createFactory: false, constructor: '_')
+@JsonSerializable(createFactory: false)
 final class CreateProjectReq implements IReq {
-  factory CreateProjectReq(CreateProjectParams params) {
-    return CreateProjectReq._(
-      name: params.name,
-      description: params.description,
-      organizationID: params.organizationID,
-      status: ProjectStatusReq.fromProjectStatus(params.status),
-    );
-  }
-
-  CreateProjectReq._({
-    required this.name,
-    required this.description,
-    required this.organizationID,
-    this.status,
-  });
+  CreateProjectReq(CreateProjectParams params)
+    : name = params.name,
+      description = params.description,
+      organizationID = params.organizationID,
+      status = ProjectStatusReq.fromProjectStatus(params.status);
 
   @override
   Map<String, dynamic> toJson() => _$CreateProjectReqToJson(this);
@@ -32,18 +22,4 @@ final class CreateProjectReq implements IReq {
   final String organizationID;
   @JsonKey(defaultValue: 'NEW')
   final ProjectStatusReq? status;
-}
-
-@JsonEnum()
-enum ProjectStatusReq {
-  // todo: remove this  status
-  @JsonValue('NEW')
-  newProject;
-
-  factory ProjectStatusReq.fromProjectStatus(ProjectStatus status) {
-    return switch (status) {
-      ProjectStatus.newProject => ProjectStatusReq.newProject,
-      // Add other cases as needed
-    };
-  }
 }
