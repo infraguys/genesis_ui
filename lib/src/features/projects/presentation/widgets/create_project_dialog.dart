91e875e7b7e7d80cac8f55cf8cb4e918175911c6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-23 11:45:20 +0300

    feat: add user selection to project creation dialog and update user handling

--- lib/src/features/projects/presentation/widgets/create_project_dialog.dart
+++ lib/src/features/projects/presentation/widgets/create_project_dialog.dart
@@ -6,10 +6,12 @@ import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:genesis/src/features/common/shared_entities/organization.dart';
+import 'package:genesis/src/features/common/shared_entities/user.dart';
 import 'package:genesis/src/features/common/shared_widgets/custom_options_view.dart';
 import 'package:genesis/src/features/organizations/presentation/blocs/organizations_bloc/organizations_bloc.dart';
 import 'package:genesis/src/features/projects/presentation/blocs/auth_user_projects_bloc/auth_user_projects_bloc.dart';
 import 'package:genesis/src/features/projects/presentation/blocs/project_bloc/project_bloc.dart';
+import 'package:genesis/src/features/users/presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class CreateProjectDialog extends StatefulWidget {
@@ -23,12 +25,14 @@ class _CreateProjectDialogState extends State<CreateProjectDialog> {
   final _formKey = GlobalKey<FormState>();
 
   late final FocusNode _organizationFocusNode;
+  late final FocusNode _userFocusNode;
   late final _ControllersManager _controllersManager;
 
   @override
   void initState() {
     super.initState();
     _organizationFocusNode = FocusNode();
+    _userFocusNode = FocusNode();
     _controllersManager = _ControllersManager();
     final authState = context.read<AuthBloc>().state as AuthenticatedAuthState;
     context.read<OrganizationsBloc>().add(
@@ -37,6 +41,7 @@ class _CreateProjectDialogState extends State<CreateProjectDialog> {
   }
 
   Organization? _selectedOrganization;
+  User? _selectedUser;
 
   @override
   Widget build(BuildContext context) {
@@ -63,62 +68,61 @@ class _CreateProjectDialogState extends State<CreateProjectDialog> {
                   spacing: 24,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // BlocBuilder<UsersBloc, UsersState>(
-                    //   builder: (context, state) {
-                    //     return RawAutocomplete<User>(
-                    //       textEditingController: _userController,
-                    //       focusNode: FocusNode(),
-                    //       optionsBuilder: (textEditingValue) {
-                    //         if (state is! UsersLoadedState) {
-                    //           return [];
-                    //         }
-                    //         return state.users.where(
-                    //           (user) => user.username.toLowerCase().contains(textEditingValue.text.toLowerCase()),
-                    //         );
-                    //       },
-                    //       fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
-                    //         return TextFormField(
-                    //           controller: controller,
-                    //           focusNode: focusNode,
-                    //           style: const TextStyle(color: Colors.white),
-                    //           decoration: InputDecoration(
-                    //             hintText: 'Выберите пользователя',
-                    //           ),
-                    //         );
-                    //       },
-                    //       onSelected: (option) {
-                    //         _selectedUser = option;
-                    //         _userController.text = option.username;
-                    //       },
-                    //       optionsViewBuilder: (context, onSelected, options) {
-                    //         return Align(
-                    //           alignment: Alignment.topLeft,
-                    //           child: Material(
-                    //             color: Colors.grey[850],
-                    //             elevation: 4,
-                    //             borderRadius: BorderRadius.circular(8),
-                    //             child: ListView.separated(
-                    //               shrinkWrap: true,
-                    //               padding: EdgeInsets.zero,
-                    //               itemCount: options.length,
-                    //               separatorBuilder: (_, __) => Divider(height: 1, color: Colors.grey[700]),
-                    //               itemBuilder: (context, index) {
-                    //                 final option = options.elementAt(index);
-                    //                 return InkWell(
-                    //                   onTap: () => onSelected(option),
-                    //                   child: Padding(
-                    //                     padding: const EdgeInsets.all(16),
-                    //                     child: Text(option.username, style: const TextStyle(color: Colors.white)),
-                    //                   ),
-                    //                 );
-                    //               },
-                    //             ),
-                    //           ),
-                    //         );
-                    //       },
-                    //     );
-                    //   },
-                    // ),
+                    BlocBuilder<UsersBloc, UsersState>(
+                      builder: (context, state) {
+                        return RawAutocomplete<User>(
+                          textEditingController: _controllersManager.userController,
+                          focusNode: _userFocusNode,
+                          optionsBuilder: (textEditingValue) {
+                            if (state is! UsersLoadedState) {
+                              return [];
+                            }
+                            return state.users.where(
+                              (user) => user.username.toLowerCase().contains(textEditingValue.text.toLowerCase()),
+                            );
+                          },
+                          fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
+                            return TextFormField(
+                              controller: controller,
+                              focusNode: focusNode,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: InputDecoration(hintText: 'Выберите пользователя'),
+                            );
+                          },
+                          displayStringForOption: (option) => option.username,
+                          onSelected: (option) {
+                            _selectedUser = option;
+                            _userFocusNode.unfocus();
+                          },
+                          optionsViewBuilder: (context, onSelected, options) {
+                            return Align(
+                              alignment: Alignment.topLeft,
+                              child: Material(
+                                color: Colors.grey[850],
+                                elevation: 4,
+                                borderRadius: BorderRadius.circular(8),
+                                child: ListView.separated(
+                                  shrinkWrap: true,
+                                  padding: EdgeInsets.zero,
+                                  itemCount: options.length,
+                                  separatorBuilder: (_, __) => Divider(height: 1, color: Colors.grey[700]),
+                                  itemBuilder: (context, index) {
+                                    final option = options.elementAt(index);
+                                    return InkWell(
+                                      onTap: () => onSelected(option),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(16),
+                                        child: Text(option.username, style: const TextStyle(color: Colors.white)),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
                     BlocBuilder<OrganizationsBloc, OrganizationsState>(
                       builder: (context, state) {
                         return RawAutocomplete<Organization>(
@@ -199,6 +203,7 @@ class _CreateProjectDialogState extends State<CreateProjectDialog> {
                     ? () {
                         context.read<ProjectBloc>().add(
                           ProjectEvent.create(
+                            userUuid: _selectedUser?.uuid ?? authState.user.uuid,
                             name: _controllersManager.projectNameController.text,
                             description: _controllersManager.projectDescriptionController.text,
                             organization: _selectedOrganization!.uuid,
@@ -217,6 +222,7 @@ class _CreateProjectDialogState extends State<CreateProjectDialog> {
 }
 
 final class _ControllersManager extends FormControllersManager {
+  final userController = TextEditingController();
   final projectNameController = TextEditingController();
   final projectDescriptionController = TextEditingController();
   final organizationSourceController = TextEditingController();
