2e053d8b1be7652ff7484c46b450c9a28c1f438c
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-25 18:12:39 +0300

    refactor: improve layout and spacing in user list details

--- lib/src/features/users/presentation/widgets/users_list_details.dart
+++ lib/src/features/users/presentation/widgets/users_list_details.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/features/common/shared_entities/user.dart';
 import 'package:genesis/src/features/common/shared_widgets/status_label.dart';
 import 'package:genesis/src/features/users/presentation/widgets/users_actions_popup_menu_button.dart';
@@ -23,66 +22,73 @@ class UsersListDetails extends StatelessWidget {
           tileColor: Colors.transparent,
         ),
       ),
-      child: ExpansionTile(
-        visualDensity: VisualDensity(vertical: -4),
-        title: Row(
-          children: [
-            SizedBox(width: 250, child: Text(user.username)),
-            StatusLabel(status: user.status),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                context.goNamed(AppRoutes.user.name, pathParameters: {'uuid': user.uuid}, extra: user);
-              },
-              child: Icon(Icons.remove_red_eye),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final totalFlex = 6;
+          return ExpansionTile(
+            title: Row(
+              spacing: 48,
+              children: [
+                // SizedBox(width: 250, child: Text(user.username)),
+                Expanded(flex: 2, child: Text(user.username)),
+                Flexible(child: StatusLabel(status: user.status)),
+                Expanded(flex: 4, child: Text(user.createdAt.toString())),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    context.goNamed(AppRoutes.user.name, pathParameters: {'uuid': user.uuid}, extra: user);
+                  },
+                  child: Icon(Icons.remove_red_eye),
+                ),
+              ],
             ),
-          ],
-        ),
-        leading: Checkbox(value: true, onChanged: (_) {}),
-        trailing: UsersActionsPopupMenuButton(),
-        expandedAlignment: Alignment.centerLeft,
-        childrenPadding: EdgeInsets.only(left: 50),
-        children: [
-          Table(
-            columnWidths: const {
-              0: FixedColumnWidth(250),
-              1: FlexColumnWidth(),
-            },
-            children: [
-              TableRow(
-                decoration: BoxDecoration(),
-                children: [
-                  Text(context.$.description, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                  Text(user.description, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Text(context.$.firstName, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                  SelectableText(user.firstName, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Text(context.$.lastName, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                  SelectableText(user.lastName, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Text(context.$.email, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                  SelectableText(user.email, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Text(context.$.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                  SelectableText(user.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8)),
-                ],
-              ),
-            ],
-          ),
-        ],
+            leading: Checkbox(value: true, onChanged: (_) {}),
+            trailing: UsersActionsPopupMenuButton(),
+            expandedAlignment: Alignment.centerLeft,
+            childrenPadding: EdgeInsets.only(left: 50),
+            // children: [
+            //   Table(
+            //     columnWidths: const {
+            //       0: FixedColumnWidth(250),
+            //       1: FlexColumnWidth(),
+            //     },
+            //     children: [
+            //       TableRow(
+            //         decoration: BoxDecoration(),
+            //         children: [
+            //           Text(context.$.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //           Text(user.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //         ],
+            //       ),
+            //       TableRow(
+            //         children: [
+            //           Text(context.$.firstName, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //           SelectableText(user.firstName, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //         ],
+            //       ),
+            //       TableRow(
+            //         children: [
+            //           Text(context.$.lastName, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //           SelectableText(user.lastName, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //         ],
+            //       ),
+            //       TableRow(
+            //         children: [
+            //           Text(context.$.email, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //           SelectableText(user.email, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //         ],
+            //       ),
+            //       TableRow(
+            //         children: [
+            //           Text(context.$.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //           SelectableText(user.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+            //         ],
+            //       ),
+            //     ],
+            //   ),
+            // ],
+          );
+        },
       ),
     );
   }
