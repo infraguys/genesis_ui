afa8e6cc451e2363dc8a2253e95a7d91d85910a3
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-25 15:46:09 +0300

    feat: add base text theme with light and dark variations

---
+++ lib/src/theming/text_theme.dart
@@ -0,0 +1,9 @@
+part of 'app_theme.dart';
+
+final TextTheme _baseTextTheme = TextTheme(
+  labelMedium: TextStyle(fontSize: 14, height: 24 / 14),
+);
+
+final _lightTextTheme = _baseTextTheme.copyWith();
+
+final _darkTextTheme = _baseTextTheme.copyWith();
