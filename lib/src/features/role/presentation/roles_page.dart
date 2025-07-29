f56a0b1739779ef8b3b91a17b777453fa8a779e9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-29 12:21:44 +0300

    feat: add button to navigate to role creation in roles page

--- lib/src/features/role/presentation/roles_page.dart
+++ lib/src/features/role/presentation/roles_page.dart
@@ -5,6 +5,9 @@ import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:genesis/src/features/role/presentation/blocs/user_roles_bloc/user_roles_bloc.dart';
 import 'package:genesis/src/features/role/presentation/widgets/roles_list_item.dart';
+import 'package:genesis/src/routing/app_router.dart';
+import 'package:genesis/src/theming/palette.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class RolesPage extends StatefulWidget {
@@ -26,109 +29,66 @@ class _ProjectsPageState extends State<RolesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<UserRolesBloc, UserRolesState>(
-      builder: (context, state) {
-        if (state is! UserRolesLoaded) {
-          return Center(child: CupertinoActivityIndicator());
-        }
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    final textTheme = TextTheme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(context.$.users, style: TextStyle(color: Colors.white54, fontSize: 12)),
+        const SizedBox(height: 24),
+        Row(
           children: [
-            Text(context.$.users, style: TextStyle(color: Colors.white54, fontSize: 12)),
-            const SizedBox(height: 24),
-            ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+            Spacer(),
+            ElevatedButton.icon(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Palette.colorFF8900),
+                padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 20, vertical: 18)),
               ),
-              contentPadding: EdgeInsets.zero,
-              leading: Checkbox(value: true, onChanged: (_) {}),
-              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-              title: Row(
-                spacing: 48,
-                children: [
-                  Expanded(flex: 2, child: Text(context.$.role(1))),
-                  Expanded(child: Text(context.$.status)),
-                  Expanded(flex: 4, child: Text('Created At')),
-                  Spacer(flex: 2),
-                ],
-              ),
+              onPressed: () {
+                context.goNamed(AppRoutes.createRole.name);
+              },
+              label: Text(context.$.create, style: textTheme.headlineSmall!.copyWith(height: 20 / 14)),
+              icon: Icon(Icons.add, color: Palette.color1B1B1D),
             ),
-            Expanded(
-              child: ListView.separated(
+          ],
+        ),
+        const SizedBox(height: 24),
+        ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+          ),
+          contentPadding: EdgeInsets.zero,
+          leading: Checkbox(value: true, onChanged: (_) {}),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          title: Row(
+            spacing: 48,
+            children: [
+              Expanded(flex: 2, child: Text(context.$.role(1))),
+              Expanded(child: Text(context.$.status)),
+              Expanded(flex: 4, child: Text('Created At')),
+              Spacer(flex: 2),
+            ],
+          ),
+        ),
+        Expanded(
+          child: BlocBuilder<UserRolesBloc, UserRolesState>(
+            builder: (context, state) {
+              if (state is! UserRolesLoaded) {
+                return Center(child: CupertinoActivityIndicator());
+              }
+              return ListView.separated(
                 itemCount: state.roles.length,
+                separatorBuilder: (_, _) => Divider(height: 0),
                 itemBuilder: (_, index) {
                   return Provider.value(
                     value: state.roles[index],
                     child: RolesListItem(),
                   );
                 },
-                separatorBuilder: (_, _) => Divider(height: 0),
-              ),
-            ),
-          ],
-        );
-      },
+              );
+            },
+          ),
+        ),
+      ],
     );
-    // return Column(
-    //   spacing: 24,
-    //   crossAxisAlignment: CrossAxisAlignment.start,
-    //   children: [
-    //     Text('Роли', style: textTheme.headlineMedium),
-    //     BlocBuilder<UserRolesBloc, UserRolesState>(
-    //       builder: (context, state) {
-    //         if (state is! UserRolesLoaded) {
-    //           return Expanded(child: Center(child: CupertinoActivityIndicator()));
-    //         }
-    //         return GridView.builder(
-    //           shrinkWrap: true,
-    //           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-    //             maxCrossAxisExtent: 320,
-    //             mainAxisExtent: 250,
-    //             crossAxisSpacing: 16,
-    //             mainAxisSpacing: 16,
-    //           ),
-    //           itemCount: state.roles.toSet().toList().length + 1,
-    //           itemBuilder: (context, index) {
-    //             if (index == 0) {
-    //               return AddRoleCardButton();
-    //             }
-    //             final role = state.roles[index - 1];
-    //             return Card(
-    //               child: Padding(
-    //                 padding: const EdgeInsets.all(12.0),
-    //                 child: Column(
-    //                   spacing: 4,
-    //                   crossAxisAlignment: CrossAxisAlignment.start,
-    //                   children: [
-    //                     Row(
-    //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                       children: [
-    //                         Text(
-    //                           role.name,
-    //                           style: textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
-    //                         ),
-    //                         Provider.value(
-    //                           value: role,
-    //                           child: RoleActionPopupMenuButton(),
-    //                         ),
-    //                       ],
-    //                     ),
-    //                     Text(role.createdAt.toString(), style: textTheme.bodySmall),
-    //                     Text(role.description, style: textTheme.bodySmall),
-    //                     SizedBox(height: 16),
-    //                     // Text(
-    //                     //   context.$.roles,
-    //                     //   style: textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
-    //                     // ),
-    //                   ],
-    //                 ),
-    //               ),
-    //             );
-    //           },
-    //         );
-    //       },
-    //     ),
-    //   ],
-    // );
   }
 }
