b7b80c11e7bfbc247cc57d72ec888debd74ffff9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-30 11:15:35 +0300

    feat: implement PermissionsApi class for permission management operations

---
+++ lib/src/features/permissions/data/source/permissions_api.dart
@@ -0,0 +1,51 @@
+import 'package:dio/dio.dart';
+import 'package:genesis/src/core/exceptions/network_exception.dart';
+import 'package:genesis/src/core/rest_client/rest_client.dart';
+import 'package:genesis/src/features/permissions/data/dtos/permission_dto.dart';
+import 'package:genesis/src/features/permissions/data/requests/get_permission_req.dart';
+import 'package:genesis/src/features/permissions/data/source/i_permissions_api.dart';
+
+final class PermissionsApi implements IPermissionsApi {
+  PermissionsApi(this._client);
+
+  final RestClient _client;
+
+  static const String _baseUrl = '/permissions';
+
+  @override
+  Future<PermissionDto> createPermission() {
+    // TODO: implement createPermission
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<PermissionDto> getPermission() {
+    // TODO: implement getPermission
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<PermissionDto>> getPermissions(GetPermissionsReq req) async {
+    const url = _baseUrl;
+    final queryParams = {
+      'name': req.name,
+      'description': req.description,
+      'createdAt': req.createdAt,
+      'updatedAt': req.updatedAt,
+      'status': req.status,
+    };
+
+    try {
+      final Response(:data) = await _client.get<List<Map<String, dynamic>>>(
+        url,
+        queryParameters: queryParams,
+      );
+      if (data != null) {
+        return data.map((it) => PermissionDto.fromJson(it)).toList();
+      }
+      return [];
+    } on DioException catch (e) {
+      throw NetworkException(e);
+    }
+  }
+}
