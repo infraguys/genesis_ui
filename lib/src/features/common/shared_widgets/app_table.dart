5e3ae553583ee8057b3f6129905f0a8cfd32606f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-30 19:31:22 +0300

    feat: add AppTable widget for displaying a list of entities with a customizable header

---
+++ lib/src/features/common/shared_widgets/app_table.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppTable<T> extends StatelessWidget {
+  const AppTable({
+    required this.entities,
+    required this.item,
+    required this.title,
+    super.key,
+  });
+
+  final List<T> entities;
+  final Widget item;
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPersistentHeader(
+          delegate: _AppTableHeaderDelegate(title),
+          pinned: true,
+        ),
+        SliverList.separated(
+          itemCount: entities.length,
+          separatorBuilder: (_, _) => Divider(height: 0),
+          itemBuilder: (context, index) {
+            return Provider.value(
+              value: entities[index],
+              child: item,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _AppTableHeaderDelegate extends SliverPersistentHeaderDelegate {
+  _AppTableHeaderDelegate(this.title);
+
+  final Widget title;
+
+  @override
+  double get minExtent => 50.0;
+
+  @override
+  double get maxExtent => 50.0;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Material(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+          ),
+        ),
+        minTileHeight: maxExtent,
+        contentPadding: EdgeInsets.zero,
+        leading: Checkbox(value: true, onChanged: (_) {}),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.more_vert),
+        ),
+        title: title,
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => false;
+}
