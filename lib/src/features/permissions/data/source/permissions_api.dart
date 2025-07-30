55b8357abc8668bcde0d07610a75c41ce5996c56
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-30 11:55:55 +0300

    fix: simplify permissions API query parameters handling

--- lib/src/features/permissions/data/source/permissions_api.dart
+++ lib/src/features/permissions/data/source/permissions_api.dart
@@ -27,21 +27,19 @@ final class PermissionsApi implements IPermissionsApi {
   @override
   Future<List<PermissionDto>> getPermissions(GetPermissionsReq req) async {
     const url = _baseUrl;
-    final queryParams = {
-      'name': req.name,
-      'description': req.description,
-      'created_at': req.createdAt,
-      'updated_at': req.updatedAt,
-      'status': req.status,
-    };
+    // final queryParams = {
+    //   'name': req.name,
+    //   'description': req.description,
+    //   'created_at': req.createdAt,
+    //   'updated_at': req.updatedAt,
+    //   'status': req.status,
+    // };
 
     try {
-      final Response(:data) = await _client.get<List<Map<String, dynamic>>>(
-        url,
-        queryParameters: queryParams,
-      );
+      final Response(:data) = await _client.get<List<dynamic>>(url);
       if (data != null) {
-        return data.map((it) => PermissionDto.fromJson(it)).toList();
+        final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
+        return castedData.map((it) => PermissionDto.fromJson(it)).toList();
       }
       return [];
     } on DioException catch (e) {
