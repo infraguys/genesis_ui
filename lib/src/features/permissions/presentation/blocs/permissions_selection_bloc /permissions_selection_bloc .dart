924491effde4bae233c4adc91194439b2801269a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 11:15:04 +0300

    feat: implement toggle functionality for permission selection in PermissionsSelectionBloc

--- lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_bloc .dart
+++ lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_bloc .dart
@@ -4,5 +4,17 @@ import 'package:genesis/src/features/common/shared_entities/permission.dart';
 part 'permissions_selection_event.dart';
 
 class PermissionsSelectionBloc extends Bloc<PermissionsSelectionEvent, List<Permission>> {
-  PermissionsSelectionBloc() : super([]);
+  PermissionsSelectionBloc() : super([]) {
+    on(_onTogglePermission);
+  }
+
+  void _onTogglePermission(_TogglePermission event, Emitter<List<Permission>> emit) {
+    final updatedPermissions = List.of(state);
+    if (updatedPermissions.contains(event.permission)) {
+      updatedPermissions.remove(event.permission);
+    } else {
+      updatedPermissions.add(event.permission);
+    }
+    emit(updatedPermissions);
+  }
 }
--- lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
+++ lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
@@ -1,19 +1,11 @@
 part of 'permissions_selection_bloc .dart';
 
 sealed class PermissionsSelectionEvent {
-  factory PermissionsSelectionEvent.addPermission(Permission permission) = _AddPermission;
-
-  factory PermissionsSelectionEvent.removePermission(Permission permission) = _RemovePermission;
+  factory PermissionsSelectionEvent.togglePermission(Permission permission) => _TogglePermission(permission);
 }
 
-final class _AddPermission implements PermissionsSelectionEvent {
-  _AddPermission(this.permission);
-
-  final Permission permission;
-}
-
-final class _RemovePermission implements PermissionsSelectionEvent {
-  _RemovePermission(this.permission);
+final class _TogglePermission implements PermissionsSelectionEvent {
+  _TogglePermission(this.permission);
 
   final Permission permission;
 }
