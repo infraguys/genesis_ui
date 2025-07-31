1f2f709346f03abf26adc183a389a665752637b4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 13:07:48 +0300

    feat: add selectAll event to PermissionsSelectionEvent for bulk permission selection

--- lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
+++ lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
@@ -1,11 +1,19 @@
 part of 'permissions_selection_bloc .dart';
 
 sealed class PermissionsSelectionEvent {
-  factory PermissionsSelectionEvent.togglePermission(Permission permission) => _TogglePermission(permission);
+  factory PermissionsSelectionEvent.togglePermission(Permission permission) = _TogglePermission;
+
+  factory PermissionsSelectionEvent.selectAll(List<Permission> permissions) = _SelectAllPermissions;
 }
 
 final class _TogglePermission implements PermissionsSelectionEvent {
   _TogglePermission(this.permission);
 
   final Permission permission;
+}
+
+final class _SelectAllPermissions implements PermissionsSelectionEvent {
+  _SelectAllPermissions(this.permissions);
+
+  final List<Permission> permissions;
 }
