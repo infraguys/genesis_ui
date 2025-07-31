a0994e5a0b6d338f3dbf71d20af2d87c89f4a4d7
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 13:08:05 +0300

    feat: add selectAll functionality to PermissionsSelectionBloc for bulk permission management

--- lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_bloc .dart
+++ lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_bloc .dart
@@ -3,9 +3,11 @@ import 'package:genesis/src/features/common/shared_entities/permission.dart';
 
 part 'permissions_selection_event.dart';
 
+// TODO: Поменять стейт на Мар
 class PermissionsSelectionBloc extends Bloc<PermissionsSelectionEvent, List<Permission>> {
   PermissionsSelectionBloc() : super([]) {
     on(_onTogglePermission);
+    on(_onSelectAllPermissions);
   }
 
   void _onTogglePermission(_TogglePermission event, Emitter<List<Permission>> emit) {
@@ -16,5 +18,15 @@ class PermissionsSelectionBloc extends Bloc<PermissionsSelectionEvent, List<Perm
       updatedPermissions.add(event.permission);
     }
     emit(updatedPermissions);
+  }
+
+  void _onSelectAllPermissions(_SelectAllPermissions event, Emitter<List<Permission>> emit) {
+    if (state.length == event.permissions.length) {
+      emit([]);
+      return;
+    } else {
+      emit(event.permissions);
+    }
+    emit(event.permissions);
   }
 }
