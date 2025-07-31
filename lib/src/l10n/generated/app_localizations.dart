7613624e8be4f068fde9e22ce21a1a9c99db77d3
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 04:27:15 +0300

    feat: add name localization to English and Russian language files

--- lib/src/l10n/generated/app_localizations.dart
+++ lib/src/l10n/generated/app_localizations.dart
@@ -331,6 +331,12 @@ abstract class AppLocalizations {
   /// In en, this message translates to:
   /// **'Permissions'**
   String get permissions;
+
+  /// No description provided for @name.
+  ///
+  /// In en, this message translates to:
+  /// **'Name'**
+  String get name;
 }
 
 class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
