54fb31dab81fd8cd3b1e56fd228406053df34e04
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 10:48:06 +0300

    feat: implement add and remove permission events in PermissionsSelectionEvent

--- lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
+++ lib/src/features/permissions/presentation/blocs/permissions_selection_bloc /permissions_selection_event.dart
@@ -1,3 +1,19 @@
 part of 'permissions_selection_bloc .dart';
 
-sealed class PermissionsSelectionEvent {}
+sealed class PermissionsSelectionEvent {
+  factory PermissionsSelectionEvent.addPermission(Permission permission) = _AddPermission;
+
+  factory PermissionsSelectionEvent.removePermission(Permission permission) = _RemovePermission;
+}
+
+final class _AddPermission implements PermissionsSelectionEvent {
+  _AddPermission(this.permission);
+
+  final Permission permission;
+}
+
+final class _RemovePermission implements PermissionsSelectionEvent {
+  _RemovePermission(this.permission);
+
+  final Permission permission;
+}
