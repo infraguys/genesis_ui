a2b6997b7f4ba5c36b5666bfd34007ee2b7e0ee9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-07-31 04:29:31 +0300

    feat: enhance permission list item with UUID display and copy functionality

--- lib/src/features/permissions/presentation/widgets/permission_list_item.dart
+++ lib/src/features/permissions/presentation/widgets/permission_list_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:genesis/src/core/extensions/text_style_extension.dart';
 import 'package:genesis/src/features/common/shared_entities/permission.dart';
 import 'package:genesis/src/features/common/shared_entities/status.dart';
 import 'package:genesis/src/features/common/shared_widgets/status_label.dart';
@@ -10,10 +13,12 @@ class PermissionListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final permission = context.read<Permission>();
+    final textTheme = TextTheme.of(context);
+    final theme = Theme.of(context);
 
     return Theme(
-      data: Theme.of(context).copyWith(
-        listTileTheme: ListTileThemeData(
+      data: theme.copyWith(
+        listTileTheme: theme.listTileTheme.copyWith(
           minVerticalPadding: 0,
           contentPadding: EdgeInsets.zero,
           tileColor: Colors.transparent,
@@ -27,14 +32,74 @@ class PermissionListItem extends StatelessWidget {
             Expanded(flex: 2, child: Text(permission.name)),
             // TODO:  проверить статус
             Flexible(child: StatusLabel(status: Status.active)), // Assuming all permissions are active
-            Expanded(flex: 4, child: Text(permission.createdAt.toString())),
-            Spacer(flex: 2),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('...${permission.uuid.split('-').last}'),
+                  IconButton(
+                    icon: Icon(Icons.copy, color: Colors.white, size: 18),
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: permission.uuid));
+                      final snack = SnackBar(
+                        backgroundColor: Colors.green,
+                        content: Text('Скопировано в буфер обмена: ${permission.uuid}'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snack);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Spacer(flex: 4),
           ],
         ),
         leading: Checkbox(value: true, onChanged: (_) {}),
         // trailing: UsersActionsPopupMenuButton(),
         expandedAlignment: Alignment.centerLeft,
         childrenPadding: EdgeInsets.only(left: 50),
+        children: [
+          Table(
+            columnWidths: const {
+              0: FixedColumnWidth(250),
+              1: FlexColumnWidth(),
+            },
+            children: [
+              TableRow(
+                decoration: BoxDecoration(),
+                children: [
+                  Text(context.$.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                  Text(permission.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                ],
+              ),
+              TableRow(
+                children: [
+                  Text(context.$.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                  SelectableText(permission.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                ],
+              ),
+              TableRow(
+                children: [
+                  Text(context.$.createdAt, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                  SelectableText(
+                    permission.createdAt.toIso8601String(),
+                    style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white,
+                  ),
+                ],
+              ),
+              TableRow(
+                children: [
+                  Text(context.$.updatedAt, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
+                  SelectableText(
+                    permission.updatedAt.toIso8601String(),
+                    style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
