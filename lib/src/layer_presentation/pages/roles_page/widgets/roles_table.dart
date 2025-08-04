6e71374deda97a55aa1e6ccd47a43ddfdeeaaee9
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 23:46:47 +0300

    refactor: update roles_table.dart to replace hardcoded 'Created At' with localized string

--- lib/src/layer_presentation/pages/roles_page/widgets/roles_table.dart
+++ lib/src/layer_presentation/pages/roles_page/widgets/roles_table.dart
@@ -16,6 +16,15 @@ class RolesTable extends StatelessWidget {
     return AppTable(
       entities: roles,
       item: RolesListItem(),
+      title: Row(
+        spacing: 48,
+        children: [
+          Expanded(flex: 2, child: Text(context.$.role(1))),
+          Expanded(child: Text(context.$.status)),
+          Expanded(flex: 4, child: Text(context.$.uuid)),
+          Spacer(flex: 2),
+        ],
+      ),
       headerLeading: BlocBuilder<RolesSelectionBloc, List<Role>>(
         builder: (context, state) {
           return Checkbox(
@@ -30,16 +39,6 @@ class RolesTable extends StatelessWidget {
             },
           );
         },
-      ),
-
-      title: Row(
-        spacing: 48,
-        children: [
-          Expanded(flex: 2, child: Text(context.$.role(1))),
-          Expanded(child: Text(context.$.status)),
-          Expanded(flex: 4, child: Text('Created At')),
-          Spacer(flex: 2),
-        ],
       ),
     );
   }
