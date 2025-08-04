1ba228490f6fa227fd26d0e3d4a5bf3e86032ddf
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 05:30:18 +0300

    feat: add CreateRoleReq class for role creation request handling

---
+++ lib/src/features/role/data/requests/create_role_req.dart
@@ -0,0 +1,8 @@
+import 'package:genesis/src/features/role/domain/params/create_role_params.dart';
+
+final class CreateRoleReq {
+  CreateRoleReq(CreateRoleParams params) : _name = params.name, _description = params.description;
+
+  final String _name;
+  final String? _description;
+}
