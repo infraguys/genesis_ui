09eedfc9d342e5448e31f1352dcac46ccb826258
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 05:31:43 +0300

    feat: add createRole method to IRolesRepository for role creation

--- lib/src/features/role/domain/repositories/i_roles_repositories.dart
+++ lib/src/features/role/domain/repositories/i_roles_repositories.dart
@@ -1,5 +1,8 @@
 import 'package:genesis/src/features/common/shared_entities/role.dart';
+import 'package:genesis/src/features/role/domain/params/create_role_params.dart';
 
 abstract interface class IRolesRepository {
   Future<List<Role>> getRolesByUserUuid(String userUuid);
+
+  Future<Role> createRole(CreateRoleParams params);
 }
