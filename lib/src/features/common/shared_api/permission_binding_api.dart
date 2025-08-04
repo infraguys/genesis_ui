7f97a746e0f7aeb5844179a5aceedabee3ecda6a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 10:50:16 +0300

    feat: implement PermissionBindingApi for creating permission bindings

---
+++ lib/src/features/common/shared_api/permission_binding_api.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:genesis/src/core/exceptions/network_exception.dart';
+import 'package:genesis/src/core/rest_client/rest_client.dart';
+import 'package:genesis/src/features/common/shared_api/i_permission_bindings_api.dart';
+
+final class PermissionBindingApi implements IPermissionBindingsApi {
+  PermissionBindingApi(this._client);
+
+  final RestClient _client;
+
+  static const _permissionBindingsUrl = '/v1/iam/permission_bindings/';
+
+  @override
+  Future<void> createPermissionBinding(String role, String permissionUuid) async {
+    const url = _permissionBindingsUrl;
+    try {
+      final Response(:data) = await _client.post<Map<String, dynamic>>(
+        url,
+        data: {
+          'role': '/v1/iam/roles/$role',
+          'permission': '/v1/iam/permissions/$permissionUuid',
+        },
+      );
+    } on DioException catch (e) {
+      throw NetworkException(e);
+    }
+  }
+}
