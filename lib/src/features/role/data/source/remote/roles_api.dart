287e37e4e936ecfed1626e826ea24ca841c309ec
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 10:49:28 +0300

    feat: add createRole method to RolesApi for role creation

--- lib/src/features/role/data/source/remote/roles_api.dart
+++ lib/src/features/role/data/source/remote/roles_api.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:genesis/src/core/exceptions/network_exception.dart';
 import 'package:genesis/src/core/rest_client/rest_client.dart';
 import 'package:genesis/src/features/role/data/dtos/role_dto.dart';
+import 'package:genesis/src/features/role/data/requests/create_role_req.dart';
 import 'package:genesis/src/features/role/data/source/remote/i_roles_api.dart';
 
 final class RolesApi implements IRolesApi {
@@ -10,25 +11,37 @@ final class RolesApi implements IRolesApi {
   final RestClient _client;
 
   static const _userUrl = '/v1/iam/users/';
+  static const _rolesUrl = '/v1/iam/roles/';
 
   @override
   Future<List<RoleDto>> getRolesByUserUuid(String userUuid) async {
     final url = '$_userUrl/$userUuid/actions/get_my_roles';
 
     try {
-      final Response(:data, :requestOptions) = await _client.get<List<dynamic>>(
-        url,
-        options: Options(
-          headers: {
-            Headers.contentTypeHeader: Headers.formUrlEncodedContentType,
-          },
-        ),
-      );
+      final Response(:data, :requestOptions) = await _client.get<List<dynamic>>(url);
       if (data != null) {
         final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
         return castedData.map((it) => RoleDto.fromJson(it)).toList();
       }
       return [];
+    } on DioException catch (e) {
+      throw NetworkException(e);
+    }
+  }
+
+  @override
+  Future<RoleDto?> createRole(CreateRoleReq req) async {
+    const url = _rolesUrl;
+    try {
+      final Response(:data, :requestOptions) = await _client.post<Map<String, dynamic>>(
+        url,
+        data: req.toJson(),
+      );
+      if (data != null) {
+        return RoleDto.fromJson(data);
+      }
+
+      return null;
     } on DioException catch (e) {
       throw NetworkException(e);
     }
