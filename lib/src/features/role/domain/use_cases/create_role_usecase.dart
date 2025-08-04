d25d2bf24f3ccb91a5f487f68819db74776c1cfc
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 05:32:08 +0300

    feat: add CreateRoleUseCase for handling role creation logic

---
+++ lib/src/features/role/domain/use_cases/create_role_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:genesis/src/features/common/shared_entities/role.dart';
+import 'package:genesis/src/features/role/domain/params/create_role_params.dart';
+import 'package:genesis/src/features/role/domain/repositories/i_roles_repositories.dart';
+
+final class CreateRoleUseCase {
+  const CreateRoleUseCase(this._repository);
+
+  final IRolesRepository _repository;
+
+  Future<Role> call(CreateRoleParams params) async {
+    return await _repository.createRole(params);
+  }
+}
