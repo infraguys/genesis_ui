0f82719395fb8fa8c7a10ee11c4179494a68ee7b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 05:32:38 +0300

    feat: add createRole method to IRolesApi for role creation

--- lib/src/features/role/data/source/remote/i_roles_api.dart
+++ lib/src/features/role/data/source/remote/i_roles_api.dart
@@ -1,5 +1,8 @@
 import 'package:genesis/src/features/role/data/dtos/role_dto.dart';
+import 'package:genesis/src/features/role/data/requests/create_role_req.dart';
 
 abstract interface class IRolesApi {
   Future<List<RoleDto>> getRolesByUserUuid(String userUuid);
+
+  Future<RoleDto> createRole(CreateRoleReq req);
 }
