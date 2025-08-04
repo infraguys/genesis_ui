74e299999bb10144f1e8b6bf9654676a4dd8ac7d
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-04 19:42:32 +0300

    feat: add organizations localization to English and Russian language files

--- lib/src/l10n/generated/app_localizations.dart
+++ lib/src/l10n/generated/app_localizations.dart
@@ -337,6 +337,12 @@ abstract class AppLocalizations {
   /// In en, this message translates to:
   /// **'Name'**
   String get name;
+
+  /// No description provided for @organizations.
+  ///
+  /// In en, this message translates to:
+  /// **'Organizations'**
+  String get organizations;
 }
 
 class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
