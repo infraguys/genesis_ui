74f28d542e99e1d3d9135756818f7b6dad2309a8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 10:58:52 +0300

    feat: update createPermissionBinding method to accept CreatePermissionBindingReq

--- lib/src/layer_data/source/remote/i_permission_bindings_api.dart
+++ lib/src/layer_data/source/remote/i_permission_bindings_api.dart
@@ -1,3 +1,5 @@
+import 'package:genesis/src/layer_data/requests/create_permission_binding_req.dart';
+
 abstract interface class IPermissionBindingsApi {
-  Future<void> createPermissionBinding(String role, String permissionUuid);
+  Future<void> createPermissionBinding(CreatePermissionBindingReq req);
 }
