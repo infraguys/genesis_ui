2ee4edcecb0d5f21102f6a8baf0ec56f3c0225ab
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 10:14:28 +0300

    feat: add OrganizationsSelectionBloc and events for organization selection management

--- lib/src/layer_presentation/pages/organizations_page/organizations_page.dart
+++ lib/src/layer_presentation/pages/organizations_page/organizations_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart';
-import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_list_item.dart';
+import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_table.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
-import 'package:provider/provider.dart';
 
 class OrganizationsPage extends StatefulWidget {
   const OrganizationsPage({super.key});
@@ -37,34 +36,14 @@ class _OrganizationsPageState extends State<OrganizationsPage> {
               ],
             ),
             const SizedBox(height: 24),
-            ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
-              ),
-              contentPadding: EdgeInsets.zero,
-              leading: Checkbox(value: true, onChanged: (_) {}),
-              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-              title: Row(
-                spacing: 48,
-                children: [
-                  Expanded(flex: 2, child: Text('Имя')),
-                  Expanded(child: Text(context.$.status)),
-                  Expanded(flex: 4, child: Text('Created At')),
-                  Spacer(flex: 2),
-                ],
-              ),
-            ),
             Expanded(
-              child: ListView.separated(
-                itemCount: organizationsState.organizations.length,
-                itemBuilder: (context, index) {
-                  final currentOrganization = organizationsState.organizations[index];
-                  return Provider.value(
-                    value: currentOrganization,
-                    child: OrganizationsListItem(),
-                  );
+              child: BlocBuilder<OrganizationsBloc, OrganizationsState>(
+                builder: (_, state) {
+                  if (state is! OrganizationsLoadedState) {
+                    return AppProgressIndicator();
+                  }
+                  return OrganizationsTable(organizations: state.organizations);
                 },
-                separatorBuilder: (_, _) => Divider(height: 0),
               ),
             ),
           ],
