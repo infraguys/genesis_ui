67eea0e7dc230b162fa25f5419b3aed77017b72c
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 10:58:41 +0300

    feat: update CreateRoleParams to support multiple permissions

--- lib/src/layer_domain/params/create_role_params.dart
+++ lib/src/layer_domain/params/create_role_params.dart
@@ -3,11 +3,11 @@ import 'package:genesis/src/layer_domain/entities/permission.dart';
 final class CreateRoleParams {
   const CreateRoleParams({
     required this.name,
-    required this.permission,
+    required this.permissions,
     this.description,
   });
 
   final String name;
   final String? description;
-  final Permission permission;
+  final List<Permission> permissions;
 }
