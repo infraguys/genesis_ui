3ea05eeb6d8f758b5888544f7047b452933f2db0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 18:10:31 +0300

    feat: add CreateOrganizationUseCase to handle organization creation logic

---
+++ lib/src/layer_domain/use_cases/organizations/create_organization_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:genesis/src/layer_domain/entities/organization.dart';
+import 'package:genesis/src/layer_domain/params/create_organization_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_organizations_repository.dart';
+
+final class CreateOrganizationUseCase {
+  CreateOrganizationUseCase(this._repository);
+
+  final IOrganizationsRepository _repository;
+
+  Future<Organization> call(CreateOrganizationParams params) async {
+    return await _repository.createOrganization(params);
+  }
+}
