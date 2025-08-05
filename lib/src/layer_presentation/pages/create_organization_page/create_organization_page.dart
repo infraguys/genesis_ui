d510b5f00c1550eb9ce4f301f287c991675add7b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 18:23:30 +0300

    feat: add CreateOrganizationPage for organization creation UI

---
+++ lib/src/layer_presentation/pages/create_organization_page/create_organization_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:genesis/src/core/extensions/string_extension.dart';
+import 'package:genesis/src/core/interfaces/form_controllers.dart';
+import 'package:genesis/src/layer_domain/params/create_organization_params.dart';
+import 'package:genesis/src/layer_presentation/pages/create_organization_page/blocs/organization_editor_bloc/organization_editor_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/role_page/blocs/role_editor_bloc/role_editor_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
+import 'package:genesis/src/theming/palette.dart';
+
+class CreateOrganizationPage extends StatefulWidget {
+  const CreateOrganizationPage({super.key});
+
+  @override
+  State<CreateOrganizationPage> createState() => _CreateOrganizationPageState();
+}
+
+class _CreateOrganizationPageState extends State<CreateOrganizationPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  late _ControllersManager _controllersManager;
+
+  @override
+  void initState() {
+    // context.read<PermissionsBloc>().add(PermissionsEvent.getPermissions());
+    // context.read<UserRolesBloc>().add(UserRolesEvent.getRoles(widget.user.uuid));
+    _controllersManager = _ControllersManager();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<RoleEditorBloc, RoleEditorState>(
+        listener: (context, state) {
+          // if (state is RoleEditorStateSuccess) {
+          //   _controllersManager.clear();
+          //   context.read<OrganizationsSelectionBloc>().add(OrganizationsSelectionEvent.clearSelection());
+          //   final snack = SnackBar(
+          //     backgroundColor: Palette.color6DCF91,
+          //     content: Text(context.$.success),
+          //   );
+          //   ScaffoldMessenger.of(context).showSnackBar(snack);
+          // }
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 24,
+          children: [
+            Breadcrumbs(
+              items: [
+                BreadcrumbItem(text: context.$.organizations),
+                BreadcrumbItem(text: context.$.create),
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 24,
+                children: [
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUnfocus,
+                      controller: _controllersManager.nameController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: context.$.name,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'required'.hardcoded;
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      controller: _controllersManager.descriptionController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: context.$.description,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Palette.color6DCF91),
+                    ),
+                    onPressed: () => save(context),
+                    child: Text(context.$.create),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void save(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      context.read<OrganizationEditorBloc>().add(
+        OrganizationEditorEvent.createOrganization(
+          CreateOrganizationParams(
+            name: _controllersManager.nameController.text,
+            description: _controllersManager.descriptionController.text,
+          ),
+        ),
+      );
+    }
+  }
+}
+
+class _ControllersManager extends FormControllersManager {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  @override
+  List<TextEditingController> get all => [nameController, descriptionController];
+
+  @override
+  bool get allFilled => all.every((it) => it.text.isNotEmpty);
+}
