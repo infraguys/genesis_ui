0b9f20e01ba8d2c8c17c28cfe566b77a31106515
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-05 11:05:34 +0300

    feat: update createPermissionBinding method to return PermissionDto

--- lib/src/layer_data/source/remote/i_permission_bindings_api.dart
+++ lib/src/layer_data/source/remote/i_permission_bindings_api.dart
@@ -1,5 +1,6 @@
+import 'package:genesis/src/layer_data/dtos/permission_dto.dart';
 import 'package:genesis/src/layer_data/requests/create_permission_binding_req.dart';
 
 abstract interface class IPermissionBindingsApi {
-  Future<void> createPermissionBinding(CreatePermissionBindingReq req);
+  Future<PermissionDto> createPermissionBinding(CreatePermissionBindingReq req);
 }
