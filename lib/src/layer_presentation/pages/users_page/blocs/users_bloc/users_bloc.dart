6052a26d05bf1470d4788464210ec9bdb1557ac4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 15:18:22 +0300

    feat: add deleteUsers event to UsersBloc for batch user deletion

--- lib/src/layer_presentation/pages/users_page/blocs/users_bloc/users_bloc.dart
+++ lib/src/layer_presentation/pages/users_page/blocs/users_bloc/users_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/get_users_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/users/delete_users_usecase.dart';
 
 part 'users_event.dart';
 part 'users_state.dart';
@@ -9,6 +10,7 @@ part 'users_state.dart';
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
   UsersBloc(this._usersRepository) : super(UsersInitState()) {
     on(_getUsers);
+    on(_onDeleteUsers);
   }
 
   final IUsersRepository _usersRepository;
@@ -18,5 +20,12 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
     emit(UsersLoadingState());
     final users = await useCase();
     emit(UsersLoadedState(users));
+  }
+
+  Future<void> _onDeleteUsers(_DeleteUsers event, Emitter<UsersState> emit) async {
+    final useCase = DeleteUsersUseCase(_usersRepository);
+    emit(UsersLoadingState());
+    await useCase(event.userUuids);
+    add(UsersEvent.getUsers());
   }
 }
