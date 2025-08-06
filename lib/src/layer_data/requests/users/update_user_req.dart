547f3af543c09e3424eb9c591ce4311dbc5a0788
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 20:24:36 +0300

    refactor: implement UpdateUserReq class with JsonEncodable and PathEncodable

---
+++ lib/src/layer_data/requests/users/update_user_req.dart
@@ -0,0 +1,25 @@
+import 'package:genesis/src/core/interfaces/json_encodable.dart';
+import 'package:genesis/src/core/interfaces/path_encodable.dart';
+import 'package:genesis/src/layer_domain/params/update_user_params.dart';
+
+class UpdateUserReq implements JsonEncodable, PathEncodable {
+  const UpdateUserReq(this._params);
+
+  final UpdateUserParams _params;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'username': ?_params.username,
+      'description': ?_params.description,
+      'first_name': ?_params.firstName,
+      'last_name': ?_params.lastName,
+      'surname': ?_params.surname,
+      'phone': ?_params.phone,
+      'email': ?_params.email,
+    };
+  }
+
+  @override
+  String toPath(String prefix) => '$prefix/${_params.uuid}';
+}
