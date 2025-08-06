5fd56eaaf3cca2ff3f176187ab0af953c02d9413
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 19:03:15 +0300

    feat: update CreateRoleReq to implement JsonEncodable interface

--- lib/src/layer_data/requests/create_role_req.dart
+++ lib/src/layer_data/requests/create_role_req.dart
@@ -1,7 +1,7 @@
-import 'package:genesis/src/core/interfaces/i_req.dart';
+import 'package:genesis/src/core/interfaces/json_encodable.dart';
 import 'package:genesis/src/layer_domain/params/create_role_params.dart';
 
-final class CreateRoleReq implements IReq {
+final class CreateRoleReq implements JsonEncodable {
   CreateRoleReq(this._params);
 
   final CreateRoleParams _params;
