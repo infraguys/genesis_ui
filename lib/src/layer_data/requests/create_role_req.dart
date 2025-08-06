19ba1b3dd2cc40201e772d546d6d263255082b89
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 18:22:35 +0300

    feat: implement IReq interface in CreateRoleReq for JSON serialization

--- lib/src/layer_data/requests/create_role_req.dart
+++ lib/src/layer_data/requests/create_role_req.dart
@@ -1,10 +1,12 @@
+import 'package:genesis/src/core/interfaces/i_req.dart';
 import 'package:genesis/src/layer_domain/params/create_role_params.dart';
 
-final class CreateRoleReq {
+final class CreateRoleReq implements IReq {
   CreateRoleReq(this._params);
 
   final CreateRoleParams _params;
 
+  @override
   Map<String, dynamic> toJson() {
     return {
       'name': _params.name,
