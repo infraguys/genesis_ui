1f3ce52104f25c8c1caaae7056bd6d54f23db1f2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 15:17:34 +0300

    feat: add deleteUsers event for batch user deletion

--- lib/src/layer_presentation/pages/users_page/blocs/users_bloc/users_event.dart
+++ lib/src/layer_presentation/pages/users_page/blocs/users_bloc/users_event.dart
@@ -2,6 +2,14 @@ part of 'users_bloc.dart';
 
 sealed class UsersEvent {
   factory UsersEvent.getUsers() = _GetUsers;
+
+  factory UsersEvent.deleteUsers(List<String> userUuids) = _DeleteUsers;
 }
 
 final class _GetUsers implements UsersEvent {}
+
+final class _DeleteUsers implements UsersEvent {
+  _DeleteUsers(this.userUuids);
+
+  final List<String> userUuids;
+}
