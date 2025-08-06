b2598dd5750327780c31248fb8681813ab0ca6b2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 19:08:40 +0300

    feat: add UsersCreateIconButton widget for user creation

---
+++ lib/src/layer_presentation/pages/users_page/widgets/users_create_icon_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/create_icon_button.dart';
+
+class UsersCreateIconButton extends StatelessWidget {
+  const UsersCreateIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // todo: реализовать создание пользователя
+    return CreateIconButton(onPressed: () {});
+  }
+}
