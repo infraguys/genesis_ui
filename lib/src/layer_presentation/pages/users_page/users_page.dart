3e28c10999c22b7dfb6509486c5f84e424d26b8d
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 15:18:49 +0300

    feat: refactor users_page to use MultiBlocListener and add UsersDeleteIconButton

--- lib/src/layer_presentation/pages/users_page/users_page.dart
+++ lib/src/layer_presentation/pages/users_page/users_page.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
-import 'package:genesis/src/core/extensions/text_style_extension.dart';
-import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/pages/user_page/blocs/user_bloc/user_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/users_page/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/users_page/blocs/users_selection_bloc/users_selection_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/users_page/widgets/users_delete_icon_button.dart';
 import 'package:genesis/src/layer_presentation/pages/users_page/widgets/users_table.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
@@ -23,19 +21,29 @@ class _UsersPageState extends State<UsersPage> {
   @override
   Widget build(BuildContext context) {
     final textTheme = TextTheme.of(context);
-    return BlocListener<UserBloc, UserState>(
-      listener: (context, state) {
-        switch (state) {
-          case UserStateDeleteSuccess():
-          case UserStateUpdateSuccess():
-            context.read<UsersBloc>().add(UsersEvent.getUsers());
-          default:
-        }
-      },
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<UserBloc, UserState>(
+          listener: (context, state) {
+            switch (state) {
+              case UserStateDeleteSuccess():
+              case UserStateUpdateSuccess():
+                context.read<UsersBloc>().add(UsersEvent.getUsers());
+              default:
+            }
+          },
+        ),
+        BlocListener<UsersBloc, UsersState>(
+          listener: (context, state) {
+            if (state is UsersLoadedState) {
+              context.read<UsersSelectionBloc>().add(UsersSelectionEvent.clearSelection());
+            }
+          },
+        ),
+      ],
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // TODO: Добавить название страницы
           Breadcrumbs(
             items: [
               BreadcrumbItem(text: context.$.users),
@@ -47,26 +55,7 @@ class _UsersPageState extends State<UsersPage> {
             spacing: 4.0,
             children: [
               Spacer(),
-              BlocBuilder<UsersSelectionBloc, List<User>>(
-                builder: (context, state) {
-                  if (state.isNotEmpty) {
-                    return ElevatedButton.icon(
-                      icon: Icon(CupertinoIcons.delete, color: Palette.colorF04C4C),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(Palette.color333333),
-                        padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
-                      ),
-                      // todo: добавить обработку нажатия
-                      label: Text(
-                        context.$.delete,
-                        style: textTheme.headlineSmall!.copyWith(height: 20 / 14) + Colors.white,
-                      ),
-                      onPressed: () {},
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-              ),
+              UsersDeleteIconButton(),
               // todo: вынести в отдельный виджет или стиль
               ElevatedButton.icon(
                 style: ButtonStyle(
