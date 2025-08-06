df92ec464a3e230e98e9e4c8b696a4426d63fab7
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-06 19:33:12 +0300

    refactor: change return type of toQuery method in QueryEncodable interface to Map<String, dynamic>

--- lib/src/core/interfaces/query_encodable.dart
+++ lib/src/core/interfaces/query_encodable.dart
@@ -1,4 +1,4 @@
 abstract interface class QueryEncodable {
   /// Converts the object to a map that can be used in a query string.
-  Map<String, String> toQuery();
+  Map<String, dynamic> toQuery();
 }
