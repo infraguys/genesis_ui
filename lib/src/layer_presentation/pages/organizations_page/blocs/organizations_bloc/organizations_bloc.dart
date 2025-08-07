d8f2eba716b3de5c0556cd33ded4b6920a141f45
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 16:37:03 +0300

    feat: add _onDeleteOrganizations event handler to manage organization deletions

--- lib/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart
+++ lib/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/organization.dart';
 import 'package:genesis/src/layer_domain/params/get_organizations_params.dart';
+import 'package:genesis/src/layer_domain/params/organizations/delete_organization_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_organizations_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/get_organizations_by_user_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/organizations/delete_organizations_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/organizations/get_orgaizations_usecase.dart';
 
 part 'organizations_event.dart';
@@ -12,6 +14,7 @@ class OrganizationsBloc extends Bloc<OrganizationsEvent, OrganizationsState> {
   OrganizationsBloc(this._repository) : super(OrganizationsInitState()) {
     on(_getOrganizationsByUser);
     on(_getOrganizations);
+    on(_onDeleteOrganizations);
   }
 
   final IOrganizationsRepository _repository;
@@ -28,5 +31,12 @@ class OrganizationsBloc extends Bloc<OrganizationsEvent, OrganizationsState> {
     emit(OrganizationsLoadingState());
     final organizations = await useCase(event.userUuid);
     emit(OrganizationsLoadedState(organizations));
+  }
+
+  Future<void> _onDeleteOrganizations(_DeleteOrganizations event, Emitter<OrganizationsState> emit) async {
+    final useCase = DeleteOrganizationsUseCase(_repository);
+    emit(OrganizationsLoadingState());
+    await useCase(event.listOfParams);
+    add(OrganizationsEvent.getOrganizations());
   }
 }
