2249298d1fed1784ae0db9249d729b95961b4d1a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 19:21:08 +0300

    feat: update projects table to display project name instead of username

--- lib/src/layer_presentation/pages/projects_page/widgets/projects_table.dart
+++ lib/src/layer_presentation/pages/projects_page/widgets/projects_table.dart
@@ -17,7 +17,7 @@ class ProjectsTable extends StatelessWidget {
       title: Row(
         spacing: 48,
         children: [
-          Expanded(flex: 2, child: Text(context.$.username)),
+          Expanded(flex: 2, child: Text(context.$.project)),
           Expanded(child: Text(context.$.status)),
           Expanded(flex: 4, child: Text(context.$.uuid)),
           Spacer(flex: 2),
