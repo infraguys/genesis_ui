9aab9f1359618b2552cfa8cf608a40cd1443af2d
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 19:16:16 +0300

    feat: add ProjectsCreateIconButton for creating new projects

---
+++ lib/src/layer_presentation/pages/projects_page/widgets/projects_create_icon_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/create_icon_button.dart';
+
+class ProjectsCreateIconButton extends StatelessWidget {
+  const ProjectsCreateIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CreateIconButton(onPressed: null);
+  }
+}
