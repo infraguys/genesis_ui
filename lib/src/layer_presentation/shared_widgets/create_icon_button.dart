8b3a2063cf924d960d9d7556b27d247cf0a7d251
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 01:34:43 +0300

    feat: update CreateIconButton label style to use labelLarge and apply color

--- lib/src/layer_presentation/shared_widgets/create_icon_button.dart
+++ lib/src/layer_presentation/shared_widgets/create_icon_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:genesis/src/core/extensions/text_style_extension.dart';
 import 'package:genesis/src/theming/palette.dart';
 
 class CreateIconButton extends StatelessWidget {
@@ -15,7 +16,7 @@ class CreateIconButton extends StatelessWidget {
         backgroundColor: WidgetStatePropertyAll(Palette.colorFF8900),
         padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
       ),
-      label: Text(context.$.create, style: textTheme.headlineSmall!.copyWith(height: 20 / 14)),
+      label: Text(context.$.create, style: textTheme.labelLarge!.w700 + Palette.color1B1B1D),
       icon: Icon(Icons.add, color: Palette.color1B1B1D),
       onPressed: onPressed,
     );
