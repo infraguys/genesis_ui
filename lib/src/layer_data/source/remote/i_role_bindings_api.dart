964934885d33fc9e9850714efe79e25a572af2b0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 18:07:41 +0300

    feat: update createRoleBinding method to accept CreateRoleBindingReq parameters

--- lib/src/layer_data/source/remote/i_role_bindings_api.dart
+++ lib/src/layer_data/source/remote/i_role_bindings_api.dart
@@ -1,3 +1,5 @@
+import 'package:genesis/src/layer_data/requests/role_bindings/create_role_binding_req.dart';
+
 abstract interface class IRoleBindingsApi {
-  Future<void> createRoleBinding(String roleUuid, String userUuid);
+  Future<void> createRoleBinding(CreateRoleBindingReq params);
 }
