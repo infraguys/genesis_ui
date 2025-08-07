7592376b61e7e8391252a586cfb1ecf706424a15
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 10:33:07 +0300

    feat: enhance text theme with headlineLarge style and dark mode adjustments

--- lib/src/theming/text_theme.dart
+++ lib/src/theming/text_theme.dart
@@ -1,10 +1,15 @@
 part of 'app_theme.dart';
 
 final TextTheme _baseTextTheme = TextTheme(
+  headlineLarge: TextStyle(fontSize: 32, height: 20 / 14, fontWeight: FontWeight.bold),
   headlineSmall: TextStyle(fontSize: 14, height: 20 / 14, fontWeight: FontWeight.bold),
   labelMedium: TextStyle(fontSize: 14, height: 24 / 14),
 );
 
 final _lightTextTheme = _baseTextTheme.copyWith();
 
-final _darkTextTheme = _baseTextTheme.copyWith();
+final _darkTextTheme = _baseTextTheme.copyWith(
+  headlineLarge: _baseTextTheme.headlineLarge! + Colors.white,
+  headlineSmall: _baseTextTheme.headlineSmall! + Colors.white,
+  labelMedium: _baseTextTheme.labelMedium! + Colors.white,
+);
