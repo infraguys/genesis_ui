10c04b89d4db832d10874f6519fd1fd7074b2632
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 11:25:33 +0300

    feat: add ProjectCard widget for displaying project details

---
+++ lib/src/layer_presentation/shared_widgets/project_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:genesis/src/layer_domain/entities/project.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/hexagon_icon_button.dart';
+import 'package:genesis/src/theming/palette.dart';
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({required this.project, super.key});
+
+  final Project project;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Palette.color333333,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(project.name),
+                  Text(project.description),
+                  // Text('Роль в проекте: администратор'),
+                ],
+              ),
+            ),
+            HexagonIconButton(
+              iconData: Icons.add,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
