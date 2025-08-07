108f0d9368f904fe0d44c830bf5804f07742f30b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 16:32:25 +0300

    feat: add deleteOrganizations event to handle batch organization deletions

--- lib/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_event.dart
+++ lib/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_event.dart
@@ -4,6 +4,9 @@ sealed class OrganizationsEvent {
   factory OrganizationsEvent.getOrganizationsByUser(String userUuid) = _GetOrganizationsByUser;
 
   factory OrganizationsEvent.getOrganizations() = _GetOrganizations;
+
+  factory OrganizationsEvent.deleteOrganizations(List<DeleteOrganizationParams> organizationUuids) =
+      _DeleteOrganizations;
 }
 
 final class _GetOrganizations implements OrganizationsEvent {}
@@ -12,4 +15,10 @@ final class _GetOrganizationsByUser implements OrganizationsEvent {
   _GetOrganizationsByUser(this.userUuid);
 
   final String userUuid;
+}
+
+final class _DeleteOrganizations implements OrganizationsEvent {
+  _DeleteOrganizations(this.listOfParams);
+
+  final List<DeleteOrganizationParams> listOfParams;
 }
