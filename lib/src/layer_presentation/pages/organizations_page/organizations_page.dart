c13b80e81a7579c9cb0dcddab624669aac250e5f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 03:55:35 +0300

    feat: replace button with OrganizationsCreateIconButton and OrganizationsDeleteIconButton in OrganizationsPage

--- lib/src/layer_presentation/pages/organizations_page/organizations_page.dart
+++ lib/src/layer_presentation/pages/organizations_page/organizations_page.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_create_icon_button.dart';
+import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_delete_icon_button.dart';
 import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_table.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
-import 'package:genesis/src/routing/app_router.dart';
-import 'package:genesis/src/theming/palette.dart';
-import 'package:go_router/go_router.dart';
 
 class OrganizationsPage extends StatefulWidget {
   const OrganizationsPage({super.key});
@@ -25,7 +24,6 @@ class _OrganizationsPageState extends State<OrganizationsPage> {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = TextTheme.of(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -36,21 +34,9 @@ class _OrganizationsPageState extends State<OrganizationsPage> {
         ),
         const SizedBox(height: 24),
         Row(
+          spacing: 4.0,
           children: [
-            Spacer(),
-            // todo: вынести в отдельный виджет или стиль
-            ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Palette.colorFF8900),
-                padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
-              ),
-              onPressed: () {
-                context.goNamed(AppRoutes.createOrganization.name);
-              },
-              label: Text(context.$.create, style: textTheme.headlineSmall!.copyWith(height: 20 / 14)),
-              icon: Icon(Icons.add, color: Palette.color1B1B1D),
-            ),
-          ],
+            Spacer(), OrganizationsDeleteIconButton(), OrganizationsCreateIconButton()],
         ),
         const SizedBox(height: 24),
         Expanded(
