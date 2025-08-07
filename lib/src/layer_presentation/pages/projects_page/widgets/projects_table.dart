4d95ad98162196f5d6c27030a9809975fc923fc2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 18:33:22 +0300

    feat: add ProjectsTable widget for displaying a table of project details

---
+++ lib/src/layer_presentation/pages/projects_page/widgets/projects_table.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:genesis/src/layer_domain/entities/project.dart';
+import 'package:genesis/src/layer_presentation/pages/projects_page/widgets/projects_list_item.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/app_table.dart';
+
+class ProjectsTable extends StatelessWidget {
+  const ProjectsTable({required this.projects, super.key});
+
+  final List<Project> projects;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTable<Project>(
+      entities: projects,
+      item: ProjectsListItem(),
+      title: Row(
+        spacing: 48,
+        children: [
+          Expanded(flex: 2, child: Text(context.$.username)),
+          Expanded(child: Text(context.$.status)),
+          Expanded(flex: 4, child: Text(context.$.uuid)),
+          Spacer(flex: 2),
+        ],
+      ),
+      headerLeading: Checkbox(
+        value: switch (10) {
+          0 => false,
+          final len when len == projects.length => true,
+          _ => null,
+        },
+        tristate: true,
+        onChanged: (val) {},
+      ),
+    );
+  }
+}
