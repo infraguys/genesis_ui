682fec3c1db4189b144c1dddf434039c2802ba40
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 03:54:27 +0300

    refactor: clean up unused imports in routes.dart

--- lib/src/routing/routes.dart
+++ lib/src/routing/routes.dart
@@ -1,10 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
-//
-// part 'routes.g.dart';
-// part 'routes/test.dart';
-// part 'routes/dashboard_route.dart';
-
 part of 'app_router.dart';
 
 enum AppRoutes {
