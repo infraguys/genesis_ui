e1fa383c37995446c8dd183dcc7649ec869a2b2e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-07 18:09:36 +0300

    feat: enhance createProject method to create role binding after project creation

--- lib/src/layer_data/repositories/projects_repository.dart
+++ lib/src/layer_data/repositories/projects_repository.dart
@@ -1,22 +1,36 @@
 import 'package:genesis/src/layer_data/requests/create_project_req.dart';
 import 'package:genesis/src/layer_data/requests/get_projects_req.dart';
+import 'package:genesis/src/layer_data/requests/role_bindings/create_role_binding_req.dart';
 import 'package:genesis/src/layer_data/requests/update_project_req.dart';
 import 'package:genesis/src/layer_data/source/remote/i_projects_api.dart';
+import 'package:genesis/src/layer_data/source/remote/i_role_bindings_api.dart';
 import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_domain/params/create_project_params.dart';
+import 'package:genesis/src/layer_domain/params/role_bindings/create_role_binding_params.dart';
 import 'package:genesis/src/layer_domain/params/update_project_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_projects_repository.dart';
 
 final class ProjectsRepository implements IProjectsRepository {
-  ProjectsRepository(this._projectsApi);
+  ProjectsRepository(this._projectsApi, this._roleBindingsApi);
 
   final IProjectsApi _projectsApi;
+  final IRoleBindingsApi _roleBindingsApi;
 
   @override
   Future<Project> createProject(CreateProjectParams params) async {
     final req = CreateProjectReq(params);
-    final dto = await _projectsApi.createProject(req);
-    return dto.toEntity();
+    final projectDto = await _projectsApi.createProject(req);
+
+    final createRoleBindingReq = CreateRoleBindingReq(
+      CreateRoleBindingParams(
+        userUuid: params.userUuid,
+        roleUuid: '726f6c65-0000-0000-0000-000000000002',
+        projectUuid: projectDto.uuid,
+      ),
+    );
+    await _roleBindingsApi.createRoleBinding(createRoleBindingReq);
+
+    return projectDto.toEntity();
   }
 
   @override
