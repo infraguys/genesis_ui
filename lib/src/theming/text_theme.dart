09c427f051cb87f457906d9ae643381daff680a2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 01:31:50 +0300

    feat: update text theme to include labelLarge and adjust labelMedium styles

--- lib/src/theming/text_theme.dart
+++ lib/src/theming/text_theme.dart
@@ -3,7 +3,8 @@ part of 'app_theme.dart';
 final TextTheme _baseTextTheme = TextTheme(
   headlineLarge: TextStyle(fontSize: 32, height: 20 / 14, fontWeight: FontWeight.bold),
   headlineSmall: TextStyle(fontSize: 14, height: 20 / 14, fontWeight: FontWeight.bold),
-  labelMedium: TextStyle(fontSize: 14, height: 24 / 14),
+  labelLarge: TextStyle(fontSize: 14, height: 20 / 14),
+  labelMedium: TextStyle(fontSize: 14, height: 20 / 14),
 );
 
 final _lightTextTheme = _baseTextTheme.copyWith();
@@ -11,5 +12,6 @@ final _lightTextTheme = _baseTextTheme.copyWith();
 final _darkTextTheme = _baseTextTheme.copyWith(
   headlineLarge: _baseTextTheme.headlineLarge! + Colors.white,
   headlineSmall: _baseTextTheme.headlineSmall! + Colors.white,
+  labelLarge: _baseTextTheme.labelLarge! + Colors.white,
   labelMedium: _baseTextTheme.labelMedium! + Colors.white,
 );
