2b51a2241b287c8b286d25ea2a2c0c083f85ef8e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 11:16:43 +0300

    feat: implement edit organization functionality in OrganizationEditorBloc

--- lib/src/layer_presentation/shared_blocs/organization_editor_bloc/organization_editor_bloc.dart
+++ lib/src/layer_presentation/shared_blocs/organization_editor_bloc/organization_editor_bloc.dart
@@ -3,6 +3,7 @@ import 'package:genesis/src/layer_domain/params/organizations/create_organizatio
 import 'package:genesis/src/layer_domain/params/organizations/edit_organiztion_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_organizations_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/organizations/create_organization_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/organizations/edit_organization_usecase.dart';
 
 part 'organization_editor_event.dart';
 part 'organization_editor_state.dart';
@@ -10,13 +11,20 @@ part 'organization_editor_state.dart';
 class OrganizationEditorBloc extends Bloc<OrganizationEditorEvent, OrganizationEditorState> {
   OrganizationEditorBloc(this._repository) : super(OrganizationEditorState.initial()) {
     on(_onCreateOrganization);
-    on(_onUpdateOrganization);
+    on(_onEditOrganization);
   }
 
   final IOrganizationsRepository _repository;
 
   Future<void> _onCreateOrganization(_CreateOrganization event, Emitter<OrganizationEditorState> emit) async {
     final useCase = CreateOrganizationUseCase(_repository);
+    emit(OrganizationEditorState.loading());
+    await useCase(event.params);
+    emit(OrganizationEditorState.success());
+  }
+
+  Future<void> _onEditOrganization(_EditOrganization event, Emitter<OrganizationEditorState> emit) async {
+    final useCase = EditOrganizationUseCase(_repository);
     emit(OrganizationEditorState.loading());
     await useCase(event.params);
     emit(OrganizationEditorState.success());
