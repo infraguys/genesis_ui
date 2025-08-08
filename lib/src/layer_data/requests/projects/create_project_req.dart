28486bbd521c632c6b24cabe5194296ba83b604a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-09 02:10:42 +0300

    feat: update create project request to use endpoints and simplify path method

--- lib/src/layer_data/requests/projects/create_project_req.dart
+++ lib/src/layer_data/requests/projects/create_project_req.dart
@@ -1,3 +1,4 @@
+import 'package:genesis/src/core/env/endpoints.dart';
 import 'package:genesis/src/core/interfaces/json_encodable.dart';
 import 'package:genesis/src/core/interfaces/path_encodable.dart';
 import 'package:genesis/src/layer_domain/params/projects/create_project_params.dart';
@@ -11,13 +12,13 @@ final class CreateProjectReq implements JsonEncodable, PathEncodable {
   Map<String, dynamic> toJson() {
     return {
       'name': _params.name,
-      'description': _params.description,
-      'organization': '/v1/iam/organizations/${_params.organizationUuid}',
+      'description': ?_params.description,
+      'organization': OrganizationsEndpoints.getOrganizations.replaceFirst(':uuid', _params.organizationUuid),
     };
   }
 
   @override
-  String toPath(String prefix) {
-    return prefix;
+  String toPath() {
+    return ProjectsEndpoints.createProject;
   }
 }
