72e90b72984b65c0485fa1666e8d70accdf461c4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 12:41:01 +0300

    refactor: remove status field from EditOrganizationParams class

--- lib/src/layer_domain/params/organizations/edit_organiztion_params.dart
+++ lib/src/layer_domain/params/organizations/edit_organiztion_params.dart
@@ -1,17 +1,13 @@
-import 'package:genesis/src/layer_domain/entities/status.dart';
-
 final class EditOrganizationParams {
   const EditOrganizationParams({
     required this.uuid,
     required this.name,
     this.description,
-    this.status,
     this.info,
   });
 
   final String uuid;
   final String name;
   final String? description;
-  final Status? status;
   final Map<String, dynamic>? info;
 }
