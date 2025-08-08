e85e2b322b5373c38fcd0b768a4208c9ff96d3aa
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 12:40:42 +0300

    feat: add EditOrganizationReq class for handling edit organization requests

---
+++ lib/src/layer_data/requests/organizations/edit_organization_req.dart
@@ -0,0 +1,22 @@
+import 'package:genesis/src/core/interfaces/json_encodable.dart';
+import 'package:genesis/src/core/interfaces/path_encodable.dart';
+import 'package:genesis/src/layer_domain/params/organizations/edit_organiztion_params.dart';
+
+final class EditOrganizationReq implements JsonEncodable, PathEncodable {
+  EditOrganizationReq(this._params);
+
+  final EditOrganizationParams _params;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _params.name,
+      'description': ?_params.description,
+    };
+  }
+
+  @override
+  String toPath(String prefix) {
+    return '$prefix/${_params.uuid}';
+  }
+}
