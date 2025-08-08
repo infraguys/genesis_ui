e21709e4b54e8649592c294b096166fb5eeceb3a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-08 04:27:01 +0300

    refactor: rename update_organiztion_params to edit_organiztion_params and update imports

--- lib/src/layer_domain/repositories/i_organizations_repository.dart
+++ lib/src/layer_domain/repositories/i_organizations_repository.dart
@@ -2,7 +2,7 @@ import 'package:genesis/src/layer_domain/entities/organization.dart';
 import 'package:genesis/src/layer_domain/params/get_organizations_params.dart';
 import 'package:genesis/src/layer_domain/params/organizations/create_organization_params.dart';
 import 'package:genesis/src/layer_domain/params/organizations/delete_organization_params.dart';
-import 'package:genesis/src/layer_domain/params/update_organiztion_params.dart';
+import 'package:genesis/src/layer_domain/params/organizations/edit_organiztion_params.dart';
 
 abstract interface class IOrganizationsRepository {
   Future<List<Organization>> getOrganizations(GetOrganizationsParams params);
