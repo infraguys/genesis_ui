149617bf97e5f7281d1dba180cd054279b9170df
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-09 02:39:07 +0300

    feat: update DeleteProjectUseCase to accept DeleteProjectParams for improved type safety

--- lib/src/layer_domain/use_cases/delete_project_usecase.dart
+++ lib/src/layer_domain/use_cases/delete_project_usecase.dart
@@ -1,3 +1,4 @@
+import 'package:genesis/src/layer_domain/params/projects/delete_project_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_projects_repository.dart';
 
 final class DeleteProjectUseCase {
@@ -5,7 +6,7 @@ final class DeleteProjectUseCase {
 
   final IProjectsRepository _repository;
 
-  Future<void> call(String projectUuid) async {
-    await _repository.deleteProject(projectUuid);
+  Future<void> call(DeleteProjectParams params) async {
+    await _repository.deleteProject(params);
   }
 }
