83be133a1ff5dd40ce6b0b51052cbbaa57b675de
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-09 02:01:21 +0300

    feat: add endpoints for getting, editing, and deleting organizations

--- lib/src/core/env/endpoints.dart
+++ lib/src/core/env/endpoints.dart
@@ -1,4 +1,7 @@
 abstract class OrganizationsEndpoints {
   static const String getOrganizations = '/iam/organizations/';
   static const String createOrganization = '/iam/organizations/';
+  static const String getOrganization = '/iam/organizations/:uuid';
+  static const String editOrganization = '/iam/organizations/:uuid';
+  static const String deleteOrganization = '/iam/organizations/:uuid';
 }
