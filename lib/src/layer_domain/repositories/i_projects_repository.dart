c2530f278169c88023b396b239dfeb8fededac95
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-09 02:21:37 +0300

    feat: rename updateProject method to editProject for consistency

--- lib/src/layer_domain/repositories/i_projects_repository.dart
+++ lib/src/layer_domain/repositories/i_projects_repository.dart
@@ -1,14 +1,14 @@
 import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_domain/params/projects/create_project_params.dart';
+import 'package:genesis/src/layer_domain/params/projects/edit_project_params.dart';
 import 'package:genesis/src/layer_domain/params/projects/get_projects_params.dart';
-import 'package:genesis/src/layer_domain/params/projects/update_project_params.dart';
 
 abstract interface class IProjectsRepository {
   Future<Project> createProject(CreateProjectParams params);
 
   Future<void> deleteProject(String projectUuid);
 
-  Future<Project> updateProject(UpdateProjectParams params);
+  Future<Project> editProject(EditProjectParams params);
 
   Future<List<Project>> getProjects(GetProjectsParams params);
 }
