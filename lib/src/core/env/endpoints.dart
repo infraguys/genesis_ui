9854fc491fada12bf56f76d6b147cf2f10610ada
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-09 02:10:34 +0300

    feat: add endpoints for getting, creating, editing, and deleting projects

--- lib/src/core/env/endpoints.dart
+++ lib/src/core/env/endpoints.dart
@@ -5,3 +5,11 @@ abstract class OrganizationsEndpoints {
   static const String editOrganization = '/iam/organizations/:uuid';
   static const String deleteOrganization = '/iam/organizations/:uuid';
 }
+
+abstract class ProjectsEndpoints {
+  static const String getProjects = '/iam/projects/';
+  static const String createProject = '/iam/projects/';
+  static const String getProject = '/iam/projects/:uuid';
+  static const String editProject = '/iam/projects/:uuid';
+  static const String deleteProject = '/iam/projects/:uuid';
+}
