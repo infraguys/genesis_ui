7740f521e170c52dff2bbac808913f8fb53a5347
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 11:56:35 +0300

    feat: add getRoleBindings method to IRoleBindingsApi interface

--- lib/src/layer_data/source/remote/interfaces/i_role_bindings_api.dart
+++ lib/src/layer_data/source/remote/interfaces/i_role_bindings_api.dart
@@ -1,5 +1,9 @@
+import 'package:genesis/src/layer_data/dtos/roles_bindings.dart';
 import 'package:genesis/src/layer_data/requests/role_bindings/create_role_binding_req.dart';
+import 'package:genesis/src/layer_data/requests/role_bindings/get_role_bindings_req.dart';
 
 abstract interface class IRoleBindingsApi {
-  Future<void> createRoleBinding(CreateRoleBindingReq params);
+  Future<void> createRoleBinding(CreateRoleBindingReq req);
+
+  Future<List<RolesBindingDto>> getRoleBindings(GetRoleBindingsReq req);
 }
