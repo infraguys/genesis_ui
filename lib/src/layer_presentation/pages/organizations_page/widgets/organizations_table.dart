fb667a81f6daba99232e4a68521d33402b09f975
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 16:32:58 +0300

    refactor: remove unnecessary BlocListener for organizations state management

--- lib/src/layer_presentation/pages/organizations_page/widgets/organizations_table.dart
+++ lib/src/layer_presentation/pages/organizations_page/widgets/organizations_table.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_domain/entities/organization.dart';
-import 'package:genesis/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/organizations_page/blocs/organizations_selection_bloc/organizations_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/organizations_page/widgets/organizations_list_item.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_table.dart';
@@ -14,39 +13,32 @@ class OrganizationsTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<OrganizationsBloc, OrganizationsState>(
-      listener: (context, state) {
-        if (state is OrganizationsLoadedState) {
-          context.read<OrganizationsSelectionBloc>().add(OrganizationsSelectionEvent.clearSelection());
-        }
-      },
-      child: AppTable<Organization>(
-        entities: organizations,
-        item: OrganizationsListItem(),
-        title: Row(
-          spacing: 48,
-          children: [
-            Expanded(flex: 2, child: Text(context.$.organization)),
-            Expanded(child: Text(context.$.status)),
-            Expanded(flex: 4, child: Text(context.$.uuid)),
-            Spacer(flex: 2),
-          ],
-        ),
-        headerLeading: BlocBuilder<OrganizationsSelectionBloc, List<Organization>>(
-          builder: (context, state) {
-            return Checkbox(
-              value: switch (state.length) {
-                0 => false,
-                final len when len == organizations.length => true,
-                _ => null,
-              },
-              tristate: true,
-              onChanged: (val) {
-                context.read<OrganizationsSelectionBloc>().add(OrganizationsSelectionEvent.selectAll(organizations));
-              },
-            );
-          },
-        ),
+    return AppTable<Organization>(
+      entities: organizations,
+      item: OrganizationsListItem(),
+      title: Row(
+        spacing: 48,
+        children: [
+          Expanded(flex: 2, child: Text(context.$.organization)),
+          Expanded(child: Text(context.$.status)),
+          Expanded(flex: 4, child: Text(context.$.uuid)),
+          Spacer(flex: 2),
+        ],
+      ),
+      headerLeading: BlocBuilder<OrganizationsSelectionBloc, List<Organization>>(
+        builder: (context, state) {
+          return Checkbox(
+            value: switch (state.length) {
+              0 => false,
+              final len when len == organizations.length => true,
+              _ => null,
+            },
+            tristate: true,
+            onChanged: (val) {
+              context.read<OrganizationsSelectionBloc>().add(OrganizationsSelectionEvent.selectAll(organizations));
+            },
+          );
+        },
       ),
     );
   }
