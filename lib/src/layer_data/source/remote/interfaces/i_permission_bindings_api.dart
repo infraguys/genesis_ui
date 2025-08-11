3a8c96e6c560b9450ded1ccad929790dae40b242
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 14:07:16 +0300

    refactor: rename create_permission_binding_req.dart for consistency

--- lib/src/layer_data/source/remote/interfaces/i_permission_bindings_api.dart
+++ lib/src/layer_data/source/remote/interfaces/i_permission_bindings_api.dart
@@ -1,5 +1,5 @@
 import 'package:genesis/src/layer_data/dtos/permission_dto.dart';
-import 'package:genesis/src/layer_data/requests/create_permission_binding_req.dart';
+import 'package:genesis/src/layer_data/requests/permission_bindings/create_permission_binding_req.dart';
 
 abstract interface class IPermissionBindingsApi {
   Future<PermissionDto> createPermissionBinding(CreatePermissionBindingReq req);
