e54f799866566484d7f0291920d56b92575bad96
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 17:06:41 +0300

    feat: add getRoles method to IRolesRepository and implement in RolesRepository

--- lib/src/layer_domain/repositories/i_roles_repositories.dart
+++ lib/src/layer_domain/repositories/i_roles_repositories.dart
@@ -1,8 +1,11 @@
 import 'package:genesis/src/layer_domain/entities/role.dart';
 import 'package:genesis/src/layer_domain/params/roles/create_role_params.dart';
+import 'package:genesis/src/layer_domain/params/roles/get_roles_params.dart';
 
 abstract interface class IRolesRepository {
   Future<List<Role>> getRolesByUserUuid(String userUuid);
+
+  Future<List<Role>> getRoles(GetRolesParams params);
 
   Future<Role> createRole(CreateRoleParams params);
 }
