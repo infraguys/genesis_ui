4f44d23500aa20f568eebca36e82402c2035aa44
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 19:16:52 +0300

    feat: replace OrganizationEditorBloc with OrganizationBloc in OrganizationPage and update save functionality

--- lib/src/layer_presentation/pages/organization_page/organization_page.dart
+++ lib/src/layer_presentation/pages/organization_page/organization_page.dart
@@ -5,9 +5,11 @@ import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/layer_domain/entities/organization.dart';
 import 'package:genesis/src/layer_domain/params/organizations/update_organization_params.dart';
-import 'package:genesis/src/layer_presentation/shared_blocs/organization_editor_bloc/organization_editor_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/organizations_page/blocs/organizations_bloc/organizations_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_blocs/organization_editor_bloc/organization_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
 import 'package:genesis/src/theming/palette.dart';
+import 'package:go_router/go_router.dart';
 
 class OrganizationPage extends StatefulWidget {
   const OrganizationPage({required this.organization, super.key});
@@ -37,81 +39,97 @@ class _OrganizationPageState extends State<OrganizationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 24,
-        children: [
-          Breadcrumbs(
-            items: [
-              BreadcrumbItem(text: context.$.organizations),
-              BreadcrumbItem(text: widget.organization.name),
-            ],
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 24,
+    return BlocListener<OrganizationBloc, OrganizationState>(
+      listener: (context, state) {
+        if (state is OrganizationStateSuccess) {
+          context.read<OrganizationsBloc>().add(OrganizationsEvent.getOrganizations());
+          _controllersManager.clear();
+          final navigator = GoRouter.of(context);
+
+          final snack = SnackBar(
+            duration: const Duration(milliseconds: 500),
+            backgroundColor: Palette.color6DCF91,
+            content: Text(context.$.success),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snack).closed.then((_) => navigator.pop());
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 24,
+          children: [
+            Breadcrumbs(
+              items: [
+                BreadcrumbItem(text: context.$.organizations),
+                BreadcrumbItem(text: widget.organization.name),
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 24,
+                children: [
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUnfocus,
+                      controller: _controllersManager.nameController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: context.$.name,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'required'.hardcoded;
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      controller: _controllersManager.descriptionController,
+                      minLines: 2,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: context.$.description,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 SizedBox(
-                  width: 400,
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUnfocus,
-                    controller: _controllersManager.nameController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: context.$.name,
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Palette.color6DCF91),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'required'.hardcoded;
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  width: 400,
-                  child: TextFormField(
-                    controller: _controllersManager.descriptionController,
-                    minLines: 2,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: context.$.description,
-                    ),
+                    onPressed: () => save(context),
+                    child: Text(context.$.update),
                   ),
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Palette.color6DCF91),
-                  ),
-                  onPressed: () => save(context),
-                  child: Text(context.$.update),
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   void save(BuildContext context) {
     if (_formKey.currentState!.validate()) {
-      context.read<OrganizationEditorBloc>().add(
-        OrganizationEditorEvent.updateOrganization(
+      context.read<OrganizationBloc>().add(
+        OrganizationEvent.updateOrganization(
           UpdateOrganizationParams(
             uuid: widget.organization.uuid,
             name: _controllersManager.nameController.text,
