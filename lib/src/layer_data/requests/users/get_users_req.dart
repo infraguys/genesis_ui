38c5656e7b31df0dc58656106b9e8e3ce819b3f2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-11 09:54:38 +0300

    feat: implement GetUsersReq class with query and path encoding for user retrieval

--- lib/src/layer_data/requests/users/get_users_req.dart
+++ lib/src/layer_data/requests/users/get_users_req.dart
@@ -1,3 +1,34 @@
-final class GetUsersReq {
-  GetUsersParams _params;
+import 'package:genesis/src/core/env/endpoints.dart';
+import 'package:genesis/src/core/interfaces/path_encodable.dart';
+import 'package:genesis/src/core/interfaces/query_encodable.dart';
+import 'package:genesis/src/layer_domain/params/users/get_users_params.dart';
+
+final class GetUsersReq implements PathEncodable, QueryEncodable {
+  const GetUsersReq(this._params);
+
+  final GetUsersParams _params;
+
+  @override
+  Map<String, dynamic> toQuery() {
+    return {
+      'name': ?_params.name,
+      'description': ?_params.description,
+      'createdAt': ?_params.createdAt?.toIso8601String(),
+      'updatedAt': ?_params.updatedAt?.toIso8601String(),
+      'salt': ?_params.salt,
+      'secretHash': ?_params.secretHash,
+      'firstName': ?_params.firstName,
+      'lastName': ?_params.lastName,
+      'surname': ?_params.surname,
+      'email': ?_params.email,
+      'phone': ?_params.phone,
+      'emailVerified': ?_params.emailVerified,
+      'password': ?_params.password,
+    };
+  }
+
+  @override
+  String toPath() {
+    return UsersEndpoints.getUsers;
+  }
 }
