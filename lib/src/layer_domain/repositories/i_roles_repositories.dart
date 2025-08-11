304fe74bf8bc7da74a87d8161502af7b7944cc53
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-12 01:33:22 +0300

    feat: add deleteRole method to IRolesRepository for role deletion functionality

--- lib/src/layer_domain/repositories/i_roles_repositories.dart
+++ lib/src/layer_domain/repositories/i_roles_repositories.dart
@@ -1,5 +1,6 @@
 import 'package:genesis/src/layer_domain/entities/role.dart';
 import 'package:genesis/src/layer_domain/params/roles/create_role_params.dart';
+import 'package:genesis/src/layer_domain/params/roles/delete_role_params.dart';
 import 'package:genesis/src/layer_domain/params/roles/get_roles_params.dart';
 
 abstract interface class IRolesRepository {
@@ -8,4 +9,6 @@ abstract interface class IRolesRepository {
   Future<List<Role>> getRoles(GetRolesParams params);
 
   Future<Role> createRole(CreateRoleParams params);
+
+  Future<Role> deleteRole(DeleteRoleParams params);
 }
