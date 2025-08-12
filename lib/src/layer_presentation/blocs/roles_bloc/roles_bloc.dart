cdf8baab49b0e6002d0908ef6ce7839b8d4130fc
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-12 03:34:35 +0300

    feat: add delete roles functionality to RolesBloc

--- lib/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart
+++ lib/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
+import 'package:genesis/src/layer_domain/params/roles/delete_role_params.dart';
 import 'package:genesis/src/layer_domain/params/roles/get_roles_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_roles_repositories.dart';
+import 'package:genesis/src/layer_domain/use_cases/roles/delete_roles_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/roles/get_roles.dart';
 
 part 'roles_event.dart';
@@ -10,6 +12,7 @@ part 'roles_state.dart';
 class RolesBloc extends Bloc<RolesEvent, RolesState> {
   RolesBloc(this._repository) : super(RolesState.init()) {
     on(_onGetRoles);
+    on(_onDeleteRoles);
   }
 
   final IRolesRepository _repository;
@@ -20,5 +23,12 @@ class RolesBloc extends Bloc<RolesEvent, RolesState> {
     emit(RolesState.loading());
     final roles = await usesCase(params);
     emit(RolesState.loaded(roles));
+  }
+
+  Future<void> _onDeleteRoles(_DeleteRoles event, Emitter<RolesState> emit) async {
+    final useCase = DeleteRolesUseCase(_repository);
+    emit(RolesState.loading());
+    await useCase(event.listOfParams);
+    add(RolesEvent.getRoles());
   }
 }
