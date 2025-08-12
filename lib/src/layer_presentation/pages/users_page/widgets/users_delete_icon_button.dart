02a13db2a3e0cedb967da16a79b0d3ba1172286d
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-12 17:56:05 +0300

    feat: integrate DeleteUserDialog into UsersDeleteIconButton for improved user deletion flow

--- lib/src/layer_presentation/pages/users_page/widgets/users_delete_icon_button.dart
+++ lib/src/layer_presentation/pages/users_page/widgets/users_delete_icon_button.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/users_page/widgets/delete_user_dialog.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
 
 class UsersDeleteIconButton extends StatelessWidget {
@@ -16,9 +17,17 @@ class UsersDeleteIconButton extends StatelessWidget {
           return const SizedBox.shrink();
         }
         return DeleteIconButton(
-          onPressed: () {
-            final userUuids = state.map((user) => user.uuid).toList();
-            context.read<UsersBloc>().add(UsersEvent.deleteUsers(userUuids));
+          onPressed: () async {
+            await showDialog<void>(
+              context: context,
+              builder: (context) => DeleteUserDialog(
+                users: state,
+                onDelete: () {
+                  final userUuids = state.map((user) => user.uuid).toList();
+                  context.read<UsersBloc>().add(UsersEvent.deleteUsers(userUuids));
+                },
+              ),
+            );
           },
         );
       },
