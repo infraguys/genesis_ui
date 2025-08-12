27a9a7cefb7608c12230b4c90bcd8827818efe9f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-12 03:34:53 +0300

    feat: add deleteRoles event to RolesEvent for role deletion

--- lib/src/layer_presentation/blocs/roles_bloc/roles_event.dart
+++ lib/src/layer_presentation/blocs/roles_bloc/roles_event.dart
@@ -2,6 +2,14 @@ part of 'roles_bloc.dart';
 
 sealed class RolesEvent {
   factory RolesEvent.getRoles() = _GetRoles;
+
+  factory RolesEvent.deleteRoles(List<DeleteRoleParams> listOfParams) = _DeleteRoles;
 }
 
 final class _GetRoles implements RolesEvent {}
+
+final class _DeleteRoles implements RolesEvent {
+  _DeleteRoles(this.listOfParams);
+
+  final List<DeleteRoleParams> listOfParams;
+}
