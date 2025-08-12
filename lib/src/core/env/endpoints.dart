76a9f19e1c1413c0de6d710429b0d66833ec7b46
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-12 16:26:21 +0300

    feat: update API endpoints to include versioning in Users, Roles, Permissions, Organizations, Projects, and RoleBindings

--- lib/src/core/env/endpoints.dart
+++ lib/src/core/env/endpoints.dart
@@ -1,49 +1,69 @@
+import 'package:genesis/src/core/env/env.dart';
+
 abstract class UsersEndpoints {
-  static const String getUsers = '/iam/users/';
-  static const String createUser = '/iam/users/';
-  static const String getUser = '/iam/users/:uuid';
-  static const String editUser = '/iam/users/:uuid';
-  static const String deleteUser = '/iam/users/:uuid';
-  static const String changeUserPassword = '/iam/users/:uuid/actions/change_password/invoke';
-  static const String confirmUserEmail = '/iam/users/:uuid/actions/confirm_email/invoke';
+  static const String _users = '/${Env.versionApi}/iam/users/';
+  static const String _user = '/${Env.versionApi}/iam/users/:uuid';
+
+  static const String getUsers = _users;
+  static const String createUser = _users;
+  static const String getUser = _user;
+  static const String updateUser = _user;
+  static const String deleteUser = _user;
+  static const String changeUserPassword = '$_user/actions/change_password/invoke';
+  static const String confirmUserEmail = '$_user/actions/confirm_email/invoke';
 }
 
 abstract class RolesEndpoints {
-  static const String getRoles = '/iam/roles/';
-  static const String createRole = '/iam/roles/';
-  static const String getRole = '/iam/roles/:uuid';
-  static const String editRole = '/iam/roles/:uuid';
-  static const String deleteRole = '/iam/roles/:uuid';
+  static const _roles = '/${Env.versionApi}/iam/roles/';
+  static const _role = '/${Env.versionApi}/iam/roles/:uuid';
+
+  static const String getRoles = _roles;
+  static const String createRole = _roles;
+  static const String getRole = _role;
+  static const String updateRole = _role;
+  static const String deleteRole = _role;
 }
 
 abstract class PermissionsEndpoints {
-  static const String getPermissions = '/iam/permissions/';
-  static const String createPermission = '/iam/permissions/';
-  static const String getPermission = '/iam/permissions/:uuid';
-  static const String editPermission = '/iam/permissions/:uuid';
-  static const String deletePermission = '/iam/permissions/:uuid';
+  static const _permissions = '/${Env.versionApi}/iam/permissions/';
+  static const _permission = '/${Env.versionApi}/iam/permissions/:uuid';
+
+  static const String getPermissions = _permissions;
+  static const String createPermission = _permissions;
+  static const String getPermission = _permission;
+  static const String updatePermission = _permission;
+  static const String deletePermission = _permission;
 }
 
 abstract class OrganizationsEndpoints {
-  static const String getOrganizations = '/iam/organizations/';
-  static const String createOrganization = '/iam/organizations/';
-  static const String getOrganization = '/iam/organizations/:uuid';
-  static const String updateOrganization = '/iam/organizations/:uuid';
-  static const String deleteOrganization = '/iam/organizations/:uuid';
+  static const String _organizations = '/${Env.versionApi}/iam/organizations/';
+  static const String _organization = '/${Env.versionApi}/iam/organizations/:uuid';
+
+  static const String getOrganizations = _organizations;
+  static const String createOrganization = _organizations;
+  static const String getOrganization = _organization;
+  static const String updateOrganization = _organization;
+  static const String deleteOrganization = _organization;
 }
 
 abstract class ProjectsEndpoints {
-  static const String getProjects = '/iam/projects/';
-  static const String createProject = '/iam/projects/';
-  static const String getProject = '/iam/projects/:uuid';
-  static const String editProject = '/iam/projects/:uuid';
-  static const String deleteProject = '/iam/projects/:uuid';
+  static const String _projects = '/${Env.versionApi}/iam/projects/';
+  static const String _project = '/${Env.versionApi}/iam/projects/:uuid';
+
+  static const String getProjects = _projects;
+  static const String createProject = _projects;
+  static const String getProject = _project;
+  static const String updateProject = _project;
+  static const String deleteProject = _project;
 }
 
 abstract class RoleBindingsEndpoints {
-  static const String getRoleBindings = '/iam/role_bindings/';
-  static const String createRoleBinding = '/iam/role_bindings/';
-  static const String getRoleBinding = '/iam/role_bindings/:uuid';
-  static const String editRoleBinding = '/iam/role_bindings/:uuid';
-  static const String deleteRoleBinding = '/iam/role_bindings/:uuid';
+  static const String _roleBindings = '/${Env.versionApi}/iam/role_bindings/';
+  static const String _roleBinding = '/${Env.versionApi}/iam/role_bindings/:uuid';
+
+  static const String getRoleBindings = _roleBindings;
+  static const String createRoleBinding = _roleBindings;
+  static const String getRoleBinding = _roleBinding;
+  static const String updateRoleBinding = _roleBinding;
+  static const String deleteRoleBinding = _roleBinding;
 }
