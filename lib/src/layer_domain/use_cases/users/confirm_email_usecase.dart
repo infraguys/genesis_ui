3421a340d2eaf1662338b06b7775b4cce5a6ebde
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-14 15:45:47 +0300

    feat: implement ConfirmEmailUseCase for handling email confirmation logic

---
+++ lib/src/layer_domain/use_cases/users/confirm_email_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:genesis/src/layer_domain/entities/user.dart';
+import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
+
+final class ConfirmEmailUseCase {
+  ConfirmEmailUseCase(this._repository);
+
+  final IUsersRepository _repository;
+
+  Future<User> call(ConfirmEmailParams params) async {
+    return await _repository.confirmEmail(params);
+  }
+}
