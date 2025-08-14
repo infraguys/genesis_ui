2332dc99031c9d32030059327f09a572794c2520
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-14 17:07:15 +0300

    feat: add VerifiedLabel to users list item for email verification status

--- lib/src/layer_presentation/pages/users_page/widgets/users_list_item.dart
+++ lib/src/layer_presentation/pages/users_page/widgets/users_list_item.dart
@@ -5,6 +5,7 @@ import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/users_page/widgets/users_actions_popup_menu_button.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/status_label.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/verified_label.dart';
 import 'package:genesis/src/routing/app_router.dart';
 import 'package:go_router/go_router.dart';
 
@@ -28,7 +29,7 @@ class UsersListItem extends StatelessWidget {
           spacing: 48,
           children: [
             Expanded(flex: 2, child: SelectableText(user.username)),
-            Flexible(child: StatusLabel(status: user.status)),
+            StatusLabel(status: user.status),
             Expanded(
               flex: 4,
               child: Row(
@@ -49,7 +50,7 @@ class UsersListItem extends StatelessWidget {
                 ],
               ),
             ),
-            Spacer(flex: 2),
+            VerifiedLabel(isVerified: user.emailVerified),
           ],
         ),
         leading: BlocBuilder<UsersSelectionBloc, List<User>>(
