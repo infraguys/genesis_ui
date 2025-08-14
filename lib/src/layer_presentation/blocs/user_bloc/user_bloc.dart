68908358e602877d0e80e04eca4c6ac40048d529
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-14 15:48:18 +0300

    feat: add confirmEmail event handler to UserBloc for email confirmation processing

--- lib/src/layer_presentation/blocs/user_bloc/user_bloc.dart
+++ lib/src/layer_presentation/blocs/user_bloc/user_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/params/users/change_user_password_params.dart';
+import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
 import 'package:genesis/src/layer_domain/params/users/delete_user_params.dart';
 import 'package:genesis/src/layer_domain/params/users/update_user_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/users/change_user_password_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/users/confirm_email_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/users/delete_user_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/users/update_user_usecase.dart';
 
@@ -12,28 +14,36 @@ part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc(this._repository) : super(UserState.init()) {
-    on(_deleteUser);
-    on(_changeUserPassword);
-    on(_updateUser);
+    on(_onDeleteUser);
+    on(_onChangeUserPassword);
+    on(_onUpdateUser);
+    on(_onConfirmEmail);
   }
 
   final IUsersRepository _repository;
 
-  Future<void> _deleteUser(_DeleteUser event, Emitter<UserState> emit) async {
+  Future<void> _onDeleteUser(_DeleteUser event, Emitter<UserState> emit) async {
     final useCase = DeleteUserUseCase(_repository);
     emit(UserStateLoading());
     useCase(event.params);
     emit(UserState.deleteSuccess());
   }
 
-  Future<void> _changeUserPassword(_ChangeUserPassword event, Emitter<UserState> emit) async {
+  Future<void> _onChangeUserPassword(_ChangeUserPassword event, Emitter<UserState> emit) async {
     final useCase = ChangeUserPasswordUseCase(_repository);
     emit(UserStateLoading());
     useCase(event.params);
   }
 
-  Future<void> _updateUser(_UpdateUser event, Emitter<UserState> emit) async {
+  Future<void> _onUpdateUser(_UpdateUser event, Emitter<UserState> emit) async {
     final useCase = UpdateUserUseCase(_repository);
+    emit(UserState.loading());
+    await useCase(event.params);
+    emit(UserState.updateSuccess());
+  }
+
+  Future<void> _onConfirmEmail(_ConfirmEmail event, Emitter<UserState> emit) async {
+    final useCase = ConfirmEmailUseCase(_repository);
     emit(UserState.loading());
     await useCase(event.params);
     emit(UserState.updateSuccess());
