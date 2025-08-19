25d8fcc3b5e5d1f0970d6c04bb58ed79d53b31ff
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-19 18:59:49 +0300

    feat: update InputDecorationTheme for improved visual consistency

--- lib/src/theming/input_decoration_theme.dart
+++ lib/src/theming/input_decoration_theme.dart
@@ -2,18 +2,18 @@ part of 'app_theme.dart';
 
 final _inputDecorationThemeData = InputDecorationTheme(
   filled: true,
-  fillColor: Color(0xFF1C1C1E).hardcoded,
+  fillColor: Colors.white.withValues(alpha: 0.05),
   hintStyle: TextStyle(color: Color(0xFF7A7B7D).hardcoded),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFF2E2E2E).hardcoded),
+    borderSide: BorderSide.none,
     borderRadius: BorderRadius.all(Radius.circular(8)),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFF2E2E2E).hardcoded),
+    borderSide: BorderSide.none,
     borderRadius: BorderRadius.all(Radius.circular(8)),
   ),
   border: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFF2E2E2E).hardcoded),
+    borderSide: BorderSide.none,
     borderRadius: BorderRadius.all(Radius.circular(8)),
   ),
 );
