865522080bff28d2c383300deaac413af5457b51
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-20 23:47:42 +0300

    refactor: simplify permission list item layout by replacing ExpansionTile with ListTile

--- lib/src/layer_presentation/shared_widgets/permission_list_item.dart
+++ lib/src/layer_presentation/shared_widgets/permission_list_item.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:genesis/src/core/extensions/localized_build_context.dart';
-import 'package:genesis/src/core/extensions/text_style_extension.dart';
 import 'package:genesis/src/layer_domain/entities/permission.dart';
 import 'package:genesis/src/layer_domain/entities/status.dart';
 import 'package:genesis/src/layer_presentation/blocs/permissions_selection_bloc/permissions_selection_bloc.dart';
@@ -14,15 +12,13 @@ class PermissionListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final permission = context.read<Permission>();
-    final textTheme = TextTheme.of(context);
-
-    return ExpansionTile(
+    return ListTile(
       title: Row(
         spacing: 48,
         children: [
           Expanded(flex: 2, child: Text(permission.name)),
           // TODO:  проверить статус
-          Flexible(child: StatusLabel(status: Status.active)), // Assuming all permissions are active
+          Flexible(child: StatusLabel(status: Status.active)),
           Flexible(
             flex: 2,
             child: Row(
@@ -56,50 +52,6 @@ class PermissionListItem extends StatelessWidget {
           );
         },
       ),
-      // trailing: UsersActionsPopupMenuButton(),
-      expandedAlignment: Alignment.centerLeft,
-      childrenPadding: EdgeInsets.only(left: 50),
-      children: [
-        Table(
-          columnWidths: const {
-            0: FixedColumnWidth(250),
-            1: FlexColumnWidth(),
-          },
-          children: [
-            TableRow(
-              decoration: BoxDecoration(),
-              children: [
-                Text(context.$.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-                Text(permission.description, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-              ],
-            ),
-            TableRow(
-              children: [
-                Text(context.$.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-                SelectableText(permission.uuid, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-              ],
-            ),
-            TableRow(
-              children: [
-                Text(context.$.createdAt, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-                SelectableText(
-                  permission.createdAt.toIso8601String(),
-                  style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white,
-                ),
-              ],
-            ),
-            TableRow(
-              children: [
-                Text(context.$.updatedAt, style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white),
-                SelectableText(
-                  permission.updatedAt.toIso8601String(),
-                  style: textTheme.bodyMedium!.copyWith(height: 1.8) + Colors.white,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
