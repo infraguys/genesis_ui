26d806833a5feeb158a836a6e7765d9480a7f08e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-20 18:46:07 +0300

    feat: add DeleteOrganizationIconButton for organization deletion functionality

---
+++ lib/src/layer_presentation/pages/organization_page/widgets/delete_organization_icon_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/layer_domain/params/organizations/delete_organization_params.dart';
+import 'package:genesis/src/layer_presentation/blocs/organization_bloc/organization_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
+
+class DeleteOrganizationIconButton extends StatelessWidget {
+  const DeleteOrganizationIconButton({required this.uuid, super.key});
+
+  final String uuid;
+
+  @override
+  Widget build(BuildContext context) {
+    return DeleteIconButton(
+      onPressed: () {
+        context.read<OrganizationBloc>().add(
+          OrganizationEvent.delete(DeleteOrganizationParams(uuid)),
+        );
+      },
+    );
+  }
+}
