ec61d49f2e173e5dd2389af0125acc1c3113d3bd
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-21 01:18:22 +0300

    feat: add DeleteUserIconButton for user deletion functionality

---
+++ lib/src/layer_presentation/pages/user_page/widgets/delete_user_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/layer_domain/entities/user.dart';
+import 'package:genesis/src/layer_domain/params/users/delete_user_params.dart';
+import 'package:genesis/src/layer_presentation/blocs/user_bloc/user_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/users_page/widgets/delete_user_dialog.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
+
+class DeleteUserIconButton extends StatelessWidget {
+  const DeleteUserIconButton({required this.user, super.key});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return DeleteIconButton(
+      onPressed: () async {
+        await showDialog<void>(
+          context: context,
+          builder: (context) => DeleteUserDialog(
+            users: List.generate(1, (_) => user),
+            onDelete: () {
+              context.read<UserBloc>().add(
+                UserEvent.deleteUser(
+                  DeleteUserParams(uuid: user.uuid),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
