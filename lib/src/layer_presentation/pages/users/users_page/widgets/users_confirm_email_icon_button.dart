229d3d99b0c77c9f2dc76ea713664e386a3edaf1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-21 17:13:17 +0300

    fix: disable onPressed action in ConfirmEmailIconButton for improved user experience

--- lib/src/layer_presentation/pages/users/users_page/widgets/users_confirm_email_icon_button.dart
+++ lib/src/layer_presentation/pages/users/users_page/widgets/users_confirm_email_icon_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
-import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
-import 'package:genesis/src/layer_presentation/blocs/user_bloc/user_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/confirm_email_icon_button.dart';
 
@@ -17,12 +15,13 @@ class UsersConfirmEmailIconButton extends StatelessWidget {
           return SizedBox.shrink();
         }
         return ConfirmEmailIconButton(
-          onPressed: () {
-            final listOfParams = state.map((it) => ConfirmEmailParams(uuid: it.uuid, verified: it.emailVerified));
-            context.read<UserBloc>().add(
-              UserEvent.confirmEmails(listOfParams.toList()),
-            );
-          },
+          onPressed: null,
+          // onPressed: () {
+          //   final listOfParams = state.map((it) => ConfirmEmailParams(uuid: it.uuid, verified: it.emailVerified));
+          //   context.read<UserBloc>().add(
+          //     UserEvent.confirmEmails(listOfParams.toList()),
+          //   );
+          // },
         );
       },
     );
