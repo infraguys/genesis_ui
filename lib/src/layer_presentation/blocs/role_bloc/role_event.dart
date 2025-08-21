32b833c569eaba0bf69fb8ffab3a7f4ef817b0f1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-21 12:30:52 +0300

    feat: add update role event to RoleEvent class

--- lib/src/layer_presentation/blocs/role_bloc/role_event.dart
+++ lib/src/layer_presentation/blocs/role_bloc/role_event.dart
@@ -2,10 +2,16 @@ part of 'role_bloc.dart';
 
 sealed class RoleEvent {
   factory RoleEvent.create(CreateRoleParams params) = _CreateRole;
+
+  factory RoleEvent.update() = _UpdateRole;
 }
 
 final class _CreateRole implements RoleEvent {
   const _CreateRole(this.params);
 
   final CreateRoleParams params;
+}
+
+final class _UpdateRole implements RoleEvent {
+  const _UpdateRole();
 }
