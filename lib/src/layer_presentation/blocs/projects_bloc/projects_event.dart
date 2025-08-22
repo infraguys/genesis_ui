671e5b42c859cd42e0b7a9641ea99ae941050cbc
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 08:19:54 +0300

    feat: update ProjectsEvent to include deleteProjects event

--- lib/src/layer_presentation/blocs/projects_bloc/projects_event.dart
+++ lib/src/layer_presentation/blocs/projects_bloc/projects_event.dart
@@ -1,11 +1,19 @@
 part of 'projects_bloc.dart';
 
 sealed class ProjectsEvent {
-  factory ProjectsEvent.getProjects([GetProjectsParams? params]) = _GetProjects;
+  factory ProjectsEvent.getProjects([GetProjectsParams? params]) = _Get;
+
+  factory ProjectsEvent.deleteProjects(List<DeleteProjectParams> listOfParams) = _DeleteProjects;
 }
 
-final class _GetProjects implements ProjectsEvent {
-  const _GetProjects([GetProjectsParams? params]) : params = params ?? const GetProjectsParams();
+final class _Get implements ProjectsEvent {
+  const _Get([GetProjectsParams? params]) : params = params ?? const GetProjectsParams();
 
   final GetProjectsParams params;
+}
+
+final class _DeleteProjects implements ProjectsEvent {
+  const _DeleteProjects(this.listOfParams);
+
+  final List<DeleteProjectParams> listOfParams;
 }
