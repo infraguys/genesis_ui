8310d6a6f1a7323468599f6016ad6e7695e9a6ad
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 11:04:53 +0300

    feat: add empty project card with tap callback to create new project

--- lib/src/layer_presentation/pages/users/user_page/widgets/list_of_projects.dart
+++ lib/src/layer_presentation/pages/users/user_page/widgets/list_of_projects.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_domain/params/projects/get_projects_params.dart';
-import 'package:genesis/src/layer_presentation/blocs/project_bloc/project_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/user_projects_bloc/user_projects_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/project_card.dart';
+import 'package:genesis/src/routing/app_router.dart';
+import 'package:go_router/go_router.dart';
 
 class ListOfProjects extends StatelessWidget {
   const ListOfProjects({required this.userUuid, super.key});
@@ -15,43 +16,49 @@ class ListOfProjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = TextTheme.of(context);
-    return BlocListener<ProjectBloc, ProjectState>(
-      listener: (context, state) {
-        if (state is ProjectDeletedState || state is ProjectUpdatedState) {
-          context.read<UserProjectsBloc>().add(
-            UserProjectsEvent.getProjects(
-              GetProjectsParams(userUuid: userUuid),
-            ),
-          );
+    return BlocBuilder<UserProjectsBloc, UserProjectsState>(
+      builder: (context, state) {
+        if (state is! UserProjectsLoadedState) {
+          return AppProgressIndicator();
         }
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(context.$.projects, style: textTheme.headlineSmall),
+            const SizedBox(height: 24),
+            Wrap(
+              spacing: 24,
+              runSpacing: 24,
+              children: [
+                SizedBox(
+                  width: 500,
+                  height: 250,
+                  child: ProjectCard.empty(
+                    onTap: () {
+                      final bloc = context.read<UserProjectsBloc>();
+                      context.pushNamed<bool>(AppRoutes.createProject.name).then(
+                        (value) {
+                          if (value != null && value) {
+                            bloc.add(
+                              UserProjectsEvent.getProjects(GetProjectsParams(userUuid: userUuid)),
+                            );
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+                for (final project in state.projects)
+                  SizedBox(
+                    width: 500,
+                    height: 250,
+                    child: ProjectCard(project: project),
+                  ),
+              ],
+            ),
+          ],
+        );
       },
-      child: BlocBuilder<UserProjectsBloc, UserProjectsState>(
-        builder: (context, state) {
-          if (state is! UserProjectsLoadedState) {
-            return Center(child: CupertinoActivityIndicator());
-          }
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(context.$.projects, style: textTheme.headlineSmall),
-              const SizedBox(height: 24),
-              Wrap(
-                spacing: 24,
-                runSpacing: 24,
-                children: state.projects.map(
-                  (project) {
-                    return SizedBox(
-                      width: 500,
-                      height: 250,
-                      child: ProjectCard(project: project),
-                    );
-                  },
-                ).toList(),
-              ),
-            ],
-          );
-        },
-      ),
     );
   }
 }
