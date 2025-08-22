8d2cf5e85911e18805fe48c29cadd7df6e805905
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 11:28:39 +0300

    feat: add prefixIconColor to input decoration theme

--- lib/src/theming/input_decoration_theme.dart
+++ lib/src/theming/input_decoration_theme.dart
@@ -4,6 +4,7 @@ final _inputDecorationThemeData = InputDecorationThemeData(
   filled: true,
   fillColor: Colors.white.withValues(alpha: 0.05),
   hintStyle: TextStyle(color: Colors.white24),
+  prefixIconColor: Colors.white24,
   contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 14),
   focusedBorder: OutlineInputBorder(
     borderSide: BorderSide.none,
