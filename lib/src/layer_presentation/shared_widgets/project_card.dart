f3bee82ca2afdd5e486c6cb8ea0cc16cc8a0edf1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 11:03:34 +0300

    feat: add support for empty project card with tap callback

--- lib/src/layer_presentation/shared_widgets/project_card.dart
+++ lib/src/layer_presentation/shared_widgets/project_card.dart
@@ -4,16 +4,39 @@ import 'package:genesis/src/layer_presentation/shared_widgets/hexagon_icon_butto
 import 'package:genesis/src/theming/palette.dart';
 
 class ProjectCard extends StatelessWidget {
-  const ProjectCard({required this.project, super.key});
+  const ProjectCard({
+    required this.project,
+    super.key,
+  }) : _isEmpty = false,
+       _onTap = null;
 
-  final Project project;
+  const ProjectCard.empty({
+    super.key,
+    VoidCallback? onTap,
+  }) : _isEmpty = true,
+       project = null,
+       _onTap = onTap;
+
+  final bool _isEmpty;
+  final Project? project;
+  final VoidCallback? _onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Palette.color333333,
+    if (_isEmpty) {
+      return Material(
+        color: Colors.transparent,
+        child: _SimpleProjectCard(
+          child: InkWell(
+            onTap: _onTap,
+            child: Center(child: Icon(Icons.add, color: Palette.color6DCF91, size: 32)),
+          ),
+        ),
+      );
+    }
+    return _SimpleProjectCard(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+        padding: EdgeInsets.all(18.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -22,14 +45,8 @@ class ProjectCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // todo: вынести в типографику
-                  Text(
-                    project.name,
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  Text(
-                    project.description,
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
+                  Text(project!.name, style: TextStyle(color: Colors.white, fontSize: 20)),
+                  Text(project!.description, style: TextStyle(color: Colors.white, fontSize: 14)),
                 ],
               ),
             ),
@@ -40,6 +57,21 @@ class ProjectCard extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class _SimpleProjectCard extends StatelessWidget {
+  const _SimpleProjectCard({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: Palette.color333333,
+      child: child,
     );
   }
 }
