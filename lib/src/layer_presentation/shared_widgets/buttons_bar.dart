b7c62c7523c1621757a241288915bce5675dc90b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 11:28:48 +0300

    feat: add support for optional left spacer in ButtonsBar

--- lib/src/layer_presentation/shared_widgets/buttons_bar.dart
+++ lib/src/layer_presentation/shared_widgets/buttons_bar.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ButtonsBar extends StatelessWidget {
-  const ButtonsBar({required this.children, super.key});
+  const ButtonsBar({required this.children, super.key}) : _isLeftSpacer = true;
+
+  const ButtonsBar.withoutLeftSpacer({required this.children, super.key}) : _isLeftSpacer = false;
 
   final List<Widget> children;
+  final bool _isLeftSpacer;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       spacing: 4.0,
       children: [
-        Spacer(),
+        if (_isLeftSpacer) Spacer(),
         ...children,
       ],
     );
