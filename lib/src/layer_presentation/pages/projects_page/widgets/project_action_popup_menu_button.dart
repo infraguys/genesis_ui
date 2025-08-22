95987bf036abb8225faf18a5bbaa3d48ee06df45
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-22 07:27:15 +0300

    fix: remove delete project option from project action popup menu

--- lib/src/layer_presentation/pages/projects_page/widgets/project_action_popup_menu_button.dart
+++ lib/src/layer_presentation/pages/projects_page/widgets/project_action_popup_menu_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/layer_domain/entities/project.dart';
-import 'package:genesis/src/layer_presentation/pages/projects_page/widgets/delete_project_dialog.dart';
 import 'package:genesis/src/layer_presentation/pages/projects_page/widgets/update_project_dialog.dart';
 import 'package:provider/provider.dart';
 
@@ -15,7 +13,6 @@ class ProjectActionPopupMenuButton extends StatelessWidget {
       icon: Icon(Icons.more_vert),
       onSelected: (value) {
         final child = switch (value) {
-          _PopupBtnValue.deleteProject => DeleteProjectDialog(),
           _PopupBtnValue.updateProject => UpdateProjectDialog(),
         };
         showDialog<void>(
@@ -36,11 +33,6 @@ class ProjectActionPopupMenuButton extends StatelessWidget {
             value: _PopupBtnValue.updateProject,
             child: Text('Update'.hardcoded),
           ),
-          PopupMenuItem(
-            value: _PopupBtnValue.deleteProject,
-            labelTextStyle: WidgetStatePropertyAll(TextStyle(color: Colors.red)),
-            child: Text(context.$.delete),
-          ),
         ];
       },
     );
@@ -48,6 +40,5 @@ class ProjectActionPopupMenuButton extends StatelessWidget {
 }
 
 enum _PopupBtnValue {
-  deleteProject,
   updateProject,
 }
