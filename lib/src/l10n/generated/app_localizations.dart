91b1147316f162ab0ca93af53de227e4822e4d58
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-25 00:03:13 +0300

    feat: add search localization to English and Russian language files

--- lib/src/l10n/generated/app_localizations.dart
+++ lib/src/l10n/generated/app_localizations.dart
@@ -415,6 +415,12 @@ abstract class AppLocalizations {
   /// In en, this message translates to:
   /// **'This field is required'**
   String get requiredField;
+
+  /// No description provided for @search.
+  ///
+  /// In en, this message translates to:
+  /// **'Search'**
+  String get search;
 }
 
 class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
