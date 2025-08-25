a7a30d5eb5776f9b6f1d6942110ecc9387f5e072
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-25 11:16:05 +0300

    feat: add getRole method to IRolesApi for role retrieval

--- lib/src/layer_data/source/remote/interfaces/i_roles_api.dart
+++ lib/src/layer_data/source/remote/interfaces/i_roles_api.dart
@@ -1,10 +1,13 @@
 import 'package:genesis/src/layer_data/dtos/role_dto.dart';
 import 'package:genesis/src/layer_data/requests/roles/create_role_req.dart';
 import 'package:genesis/src/layer_data/requests/roles/delete_role_req.dart';
+import 'package:genesis/src/layer_data/requests/roles/get_role_req.dart';
 import 'package:genesis/src/layer_data/requests/roles/get_roles_req.dart';
 
 abstract interface class IRolesApi {
   Future<List<RoleDto>> getRolesByUserUuid(String userUuid);
+
+  Future<RoleDto> getRole(GetRoleReq req);
 
   Future<List<RoleDto>> getRoles(GetRolesReq req);
 
