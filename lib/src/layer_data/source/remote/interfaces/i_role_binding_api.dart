9f50c45f3f4546a32e9d672d641bf5fe907a5368
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-25 15:35:46 +0300

    fix: remove i_role_binding_api.dart

--- lib/src/layer_data/source/remote/interfaces/i_role_binding_api.dart
+++
@@ -1,6 +0,0 @@
-import 'package:genesis/src/layer_data/dtos/roles_bindings.dart';
-import 'package:genesis/src/layer_data/requests/role_bindings/create_role_binding_req.dart';
-
-abstract interface class IRoleBindingApi {
-  Future<RolesBindingDto> createRoleBinding(CreateRoleBindingReq req);
-}
