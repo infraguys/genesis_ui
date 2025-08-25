c8ccab89a7698366372443c78f2f1b3ef260acc8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-25 15:41:20 +0300

    refactor: rename event handlers for role creation and update

--- lib/src/layer_presentation/blocs/role_bloc/role_bloc.dart
+++ lib/src/layer_presentation/blocs/role_bloc/role_bloc.dart
@@ -8,18 +8,18 @@ part 'role_state.dart';
 
 class RoleBloc extends Bloc<RoleEvent, RoleState> {
   RoleBloc(this._rolesRepository) : super(RoleState.initial()) {
-    on(_onCreateRole);
-    on(_onUpdateRole);
+    on(_onCreate);
+    on(_onUpdate);
   }
 
   final IRolesRepository _rolesRepository;
 
-  Future<void> _onCreateRole(_Create event, Emitter<RoleState> emit) async {
+  Future<void> _onCreate(_Create event, Emitter<RoleState> emit) async {
     final useCase = CreateRoleUseCase(_rolesRepository);
     emit(RoleState.loading());
     await useCase(event.params);
     emit(RoleState.created());
   }
 
-  Future<void> _onUpdateRole(_Update event, Emitter<RoleState> emit) async {}
+  Future<void> _onUpdate(_Update event, Emitter<RoleState> emit) async {}
 }
