ac3403c4968d0c1b0d670c63e61036baa6bd7504
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 07:58:56 +0300

    feat: add createdAt and updatedAt fields to getRoleBindingsParams model

--- lib/src/layer_domain/params/role_bindings/get_role_bindings_params.dart
+++ lib/src/layer_domain/params/role_bindings/get_role_bindings_params.dart
@@ -3,9 +3,13 @@ final class GetRoleBindingsParams {
     this.userUuid,
     this.projectUuid,
     this.roleUuid,
+    this.createdAt,
+    this.updatedAt,
   });
 
   final String? userUuid;
   final String? projectUuid;
   final String? roleUuid;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
 }
