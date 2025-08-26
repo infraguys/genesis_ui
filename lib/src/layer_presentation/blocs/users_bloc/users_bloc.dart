10d00a525881dc77e3d2744a7e00211ad2582e49
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 18:24:47 +0300

    feat: add _onForceConfirmEmails method to UsersBloc for bulk email confirmation

--- lib/src/layer_presentation/blocs/users_bloc/users_bloc.dart
+++ lib/src/layer_presentation/blocs/users_bloc/users_bloc.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
+import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
 import 'package:genesis/src/layer_domain/params/users/get_users_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/users/delete_users_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/users/force_confirm_emails_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/users/get_users_usecase.dart';
 
 part 'users_event.dart';
 part 'users_state.dart';
 
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
-  UsersBloc(this._usersRepository) : super(UsersInitState()) {
+  UsersBloc(this._usersRepository) : super(UsersState.initial()) {
     on(_getUsers);
     on(_onDeleteUsers);
+    on(_onForceConfirmEmails);
     add(UsersEvent.getUsers());
   }
 
@@ -19,15 +22,25 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
 
   Future<void> _getUsers(_GetUsers event, Emitter<UsersState> emit) async {
     final useCase = GetUsersUseCase(_usersRepository);
-    emit(UsersLoadingState());
+    emit(UsersState.loading());
     final users = await useCase(GetUsersParams());
-    emit(UsersLoadedState(users));
+    emit(UsersState.loaded(users));
   }
 
   Future<void> _onDeleteUsers(_DeleteUsers event, Emitter<UsersState> emit) async {
     final useCase = DeleteUsersUseCase(_usersRepository);
-    emit(UsersLoadingState());
+    emit(UsersState.loading());
     await useCase(event.userUuids);
+    add(UsersEvent.getUsers());
+  }
+
+  Future<void> _onForceConfirmEmails(_ForceConfirmEmails event, Emitter<UsersState> emit) async {
+    final useCase = ForceConfirmEmailsUseCase(_usersRepository);
+    final listOfParams = event.users
+        .where((user) => !user.emailVerified)
+        .map((user) => ConfirmEmailParams(uuid: user.uuid));
+    emit(UsersState.loading());
+    await useCase(listOfParams.toList());
     add(UsersEvent.getUsers());
   }
 }
