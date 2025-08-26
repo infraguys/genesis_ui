7140d16828f13586118d50524d6791970d7dc676
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 03:44:53 +0300

    feat: add methods for creating and retrieving role bindings in the role bindings repository

--- lib/src/layer_domain/repositories/i_role_bindings_repository.dart
+++ lib/src/layer_domain/repositories/i_role_bindings_repository.dart
@@ -1,3 +1,11 @@
+import 'package:genesis/src/layer_domain/entities/role_binding.dart';
+import 'package:genesis/src/layer_domain/params/role_bindings/create_role_binding_params.dart';
+import 'package:genesis/src/layer_domain/params/role_bindings/get_role_bindings_params.dart';
+
 abstract interface class IRoleBindingsRepository {
+  Future<void> createRoleBinding(CreateRoleBindingParams params);
+
   Future<void> deleteRoleBinding(String uuid);
+
+  Future<List<RoleBinding>> getRoleBindings(GetRoleBindingsParams params);
 }
