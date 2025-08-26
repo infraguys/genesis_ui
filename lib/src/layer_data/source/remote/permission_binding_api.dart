7400fdc10c3f0c4fe22ba446d01565e22f599ae6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 01:30:47 +0300

    feat: rename PermissionBindingApi to PermissionBindingsApi and update return type to PermissionBindingDto

--- lib/src/layer_data/source/remote/permission_binding_api.dart
+++ lib/src/layer_data/source/remote/permission_binding_api.dart
@@ -2,23 +2,23 @@ import 'package:dio/dio.dart';
 import 'package:genesis/src/core/exceptions/data_not_found_exception.dart';
 import 'package:genesis/src/core/exceptions/network_exception.dart';
 import 'package:genesis/src/core/network/rest_client/rest_client.dart';
-import 'package:genesis/src/layer_data/dtos/permission_dto.dart';
+import 'package:genesis/src/layer_data/dtos/permission_binding_dto.dart';
 import 'package:genesis/src/layer_data/source/remote/interfaces/i_permission_bindings_api.dart';
 
-final class PermissionBindingApi implements IPermissionBindingsApi {
-  PermissionBindingApi(this._client);
+final class PermissionBindingsApi implements IPermissionBindingsApi {
+  PermissionBindingsApi(this._client);
 
   final RestClient _client;
 
   @override
-  Future<PermissionDto> createPermissionBinding(req) async {
+  Future<PermissionBindingDto> createPermissionBinding(req) async {
     try {
       final Response(:data, :requestOptions) = await _client.post<Map<String, dynamic>>(
         req.toPath(),
         data: req.toJson(),
       );
       if (data != null) {
-        return PermissionDto.fromJson(data);
+        return PermissionBindingDto.fromJson(data);
       }
       throw DataNotFoundException(requestOptions.uri.path);
     } on DioException catch (e) {
