f57a438de13eea1a6898c486cc41c46b829373b0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 00:48:49 +0300

    feat: add CreateRoleBindingUseCase for managing role bindings

--- lib/src/layer_domain/use_cases/role_bindings/create_role_bindings_usecase.dart
+++ lib/src/layer_domain/use_cases/role_bindings/create_role_bindings_usecase.dart
@@ -1,6 +1,16 @@
 import 'package:genesis/src/layer_domain/params/role_bindings/create_role_binding_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_role_bindings_repository.dart';
 
+final class CreateRoleBindingUseCase {
+  CreateRoleBindingUseCase(this._repository);
+
+  final IRoleBindingsRepository _repository;
+
+  Future<void> call(CreateRoleBindingParams params) async {
+    _repository.createRoleBinding(params);
+  }
+}
+
 final class CreateRoleBindingsUseCase {
   CreateRoleBindingsUseCase(this._repository);
 
