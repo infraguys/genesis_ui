7ba4a022e07b92c38a9d5fd5fbc8f3af885227c1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 17:38:48 +0300

    feat: add ConfirmEmailUseCase for handling email confirmation logic

--- lib/src/layer_domain/use_cases/users/confirm_emails_usecase.dart
+++ lib/src/layer_domain/use_cases/users/confirm_emails_usecase.dart
@@ -2,6 +2,16 @@ import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 
+final class ConfirmEmailUseCase {
+  ConfirmEmailUseCase(this._repository);
+
+  final IUsersRepository _repository;
+
+  Future<User> call(ConfirmEmailParams params) async {
+    return await _repository.confirmEmail(params);
+  }
+}
+
 final class ConfirmEmailsUseCase {
   ConfirmEmailsUseCase(this._repository);
 
@@ -9,11 +19,7 @@ final class ConfirmEmailsUseCase {
 
   Future<List<User>> call(List<ConfirmEmailParams> params) async {
     return await Future.wait(
-      params
-          .where((it) => !it.verified)
-          .map(
-            (param) => _repository.confirmEmail(param),
-          ),
+      params.where((it) => !it.verified).map(_repository.confirmEmail),
     );
   }
 }
