88e98ae6b783dc9c70a2578c8a8f4d3aba67b73a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 03:42:39 +0300

    feat: remove userUuid and roleUuid from CreateProjectParams for simplification

--- lib/src/layer_domain/params/projects/create_project_params.dart
+++ lib/src/layer_domain/params/projects/create_project_params.dart
@@ -1,15 +1,11 @@
 final class CreateProjectParams {
   const CreateProjectParams({
-    required this.userUuid,
     required this.name,
     required this.description,
     required this.organizationUuid,
-    required this.roleUuid,
   });
 
-  final String userUuid;
   final String name;
   final String? description;
   final String organizationUuid;
-  final String roleUuid;
 }
