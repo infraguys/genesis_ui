a94bf81a5a9a8ed7861b8244aa97b8e2f3f44cf0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 04:15:25 +0300

    feat: integrate RoleBindingsBloc to refresh project list on role deletion

--- lib/src/layer_presentation/pages/users/user_page/widgets/list_of_projects.dart
+++ lib/src/layer_presentation/pages/users/user_page/widgets/list_of_projects.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_domain/params/projects/get_projects_params.dart';
+import 'package:genesis/src/layer_presentation/blocs/role_bindings_bloc/role_bindings_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/user_projects_bloc/user_projects_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/add_project_card.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/project_card.dart';
 import 'package:genesis/src/routing/app_router.dart';
@@ -16,49 +18,51 @@ class ListOfProjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = TextTheme.of(context);
-    return BlocBuilder<UserProjectsBloc, UserProjectsState>(
-      builder: (context, state) {
-        if (state is! UserProjectsLoadedState) {
-          return AppProgressIndicator();
-        }
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(context.$.projects, style: textTheme.headlineSmall),
-            const SizedBox(height: 24),
-            Wrap(
-              spacing: 24,
-              runSpacing: 24,
-              children: [
-                SizedBox(
-                  width: 500,
-                  height: 250,
-                  child: ProjectCard.empty(
-                    onTap: () {
-                      final bloc = context.read<UserProjectsBloc>();
-                      context.pushNamed<bool>(AppRoutes.createProject.name).then(
-                        (value) {
-                          if (value != null && value) {
-                            bloc.add(
-                              UserProjectsEvent.getProjects(GetProjectsParams(userUuid: userUuid)),
-                            );
-                          }
-                        },
-                      );
-                    },
-                  ),
-                ),
-                for (final it in state.projectsWithRoles)
+
+    return BlocListener<RoleBindingsBloc, RoleBindingsState>(
+      listenWhen: (_, current) => current is RoleBindingsDeletedState,
+      listener: (context, state) {
+        UserProjectsEvent.getProjects(GetProjectsParams(userUuid: userUuid));
+      },
+      child: BlocBuilder<UserProjectsBloc, UserProjectsState>(
+        builder: (context, state) {
+          if (state is! UserProjectsLoadedState) {
+            return AppProgressIndicator();
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(context.$.projects, style: textTheme.headlineSmall),
+              const SizedBox(height: 24),
+              Wrap(
+                spacing: 24,
+                runSpacing: 24,
+                children: [
                   SizedBox(
                     width: 500,
                     height: 250,
-                    child: ProjectCard(project: it.project, roles: it.roles),
+                    child: AddProjectCard(
+                      onTap: () async {
+                        final bloc = context.read<UserProjectsBloc>();
+                        final isCreated = await context.pushNamed<bool>(AppRoutes.createProject.name);
+                        if (isCreated != null && isCreated) {
+                          bloc.add(UserProjectsEvent.getProjects(GetProjectsParams(userUuid: userUuid)));
+                        }
+                      },
+                    ),
                   ),
-              ],
-            ),
-          ],
-        );
-      },
+                  for (final it in state.projectsWithRoles)
+                    SizedBox(
+                      width: 500,
+                      height: 250,
+                      child: ProjectCard(project: it.project, roles: it.roles, userUuid: userUuid),
+                    ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
