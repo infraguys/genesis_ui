a2a01002ba4583433ed6b88813774cc4967de2a4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 03:20:35 +0300

    feat: add roleUuid parameter to GetRoleBindingsParams for enhanced filtering

--- lib/src/layer_domain/params/role_bindings/get_role_bindings_params.dart
+++ lib/src/layer_domain/params/role_bindings/get_role_bindings_params.dart
@@ -2,8 +2,10 @@ final class GetRoleBindingsParams {
   GetRoleBindingsParams({
     this.userUuid,
     this.projectUuid,
+    this.roleUuid,
   });
 
   final String? userUuid;
   final String? projectUuid;
+  final String? roleUuid;
 }
