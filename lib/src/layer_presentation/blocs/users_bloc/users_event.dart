9a4553df04021a0033b245e52e295a5e640e3afc
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 18:25:24 +0300

    feat: add forceConfirmEmails event to UsersEvent for bulk email confirmation

--- lib/src/layer_presentation/blocs/users_bloc/users_event.dart
+++ lib/src/layer_presentation/blocs/users_bloc/users_event.dart
@@ -4,6 +4,8 @@ sealed class UsersEvent {
   factory UsersEvent.getUsers() = _GetUsers;
 
   factory UsersEvent.deleteUsers(List<String> userUuids) = _DeleteUsers;
+
+  factory UsersEvent.forceConfirmEmails(List<User> users) = _ForceConfirmEmails;
 }
 
 final class _GetUsers implements UsersEvent {}
@@ -12,4 +14,10 @@ final class _DeleteUsers implements UsersEvent {
   _DeleteUsers(this.userUuids);
 
   final List<String> userUuids;
+}
+
+final class _ForceConfirmEmails implements UsersEvent {
+  _ForceConfirmEmails(this.users);
+
+  final List<User> users;
 }
