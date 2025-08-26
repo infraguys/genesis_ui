bcf6ba27d47516c956effb3e5e5a025e2cc8daec
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 01:29:19 +0300

    feat: add PermissionBindingDto for serializing permission binding data

---
+++ lib/src/layer_data/dtos/permission_binding_dto.dart
@@ -0,0 +1,40 @@
+import 'package:genesis/src/core/interfaces/i_dto.dart';
+import 'package:genesis/src/layer_domain/entities/permission_binding.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'permission_binding_dto.g.dart';
+
+@JsonSerializable(createToJson: false, constructor: '_')
+class PermissionBindingDto implements IDto<PermissionBinding> {
+  PermissionBindingDto._({
+    required this.uuid,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.role,
+    required this.permission,
+    required this.projectId,
+  });
+
+  factory PermissionBindingDto.fromJson(Map<String, dynamic> json) => _$PermissionBindingDtoFromJson(json);
+
+  final String uuid;
+  @JsonKey(fromJson: DateTime.parse)
+  final DateTime createdAt;
+  @JsonKey(fromJson: DateTime.parse)
+  final DateTime updatedAt;
+  final String? projectId;
+  final String role;
+  final String permission;
+
+  @override
+  PermissionBinding toEntity() {
+    return PermissionBinding(
+      uuid: uuid,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      projectId: projectId,
+      role: role,
+      permission: permission,
+    );
+  }
+}
