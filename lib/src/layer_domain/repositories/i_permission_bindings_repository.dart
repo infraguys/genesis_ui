b40a8a333b792fc7a9a08a77b03e11794cf0b6d2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 01:30:27 +0300

    fix: update return type of createPermissionBinding method to use PermissionBinding

--- lib/src/layer_domain/repositories/i_permission_bindings_repository.dart
+++ lib/src/layer_domain/repositories/i_permission_bindings_repository.dart
@@ -1,7 +1,8 @@
+import 'package:genesis/src/layer_domain/entities/permission_binding.dart';
 import 'package:genesis/src/layer_domain/params/permission_bindings_params/create_permission_binding_params.dart';
 
 abstract interface class IPermissionBindingsRepository {
-  Future<void> createPermissionBinding(CreatePermissionBindingParams params);
+  Future<PermissionBinding> createPermissionBinding(CreatePermissionBindingParams params);
 
   Future<void> deletePermissionBinding(String uuid);
 }
