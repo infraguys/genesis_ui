d6be0d5056673fcfc8aaf5bb56320ee068f09016
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 00:23:13 +0300

    feat: update DeleteUsersUseCase to accept List<DeleteUserParams> for improved type safety

--- lib/src/layer_domain/use_cases/users/delete_users_usecase.dart
+++ lib/src/layer_domain/use_cases/users/delete_users_usecase.dart
@@ -1,14 +1,24 @@
 import 'package:genesis/src/layer_domain/params/users/delete_user_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 
+class DeleteUserUseCase {
+  DeleteUserUseCase(this._repository);
+
+  final IUsersRepository _repository;
+
+  Future<void> call(DeleteUserParams params) async {
+    await _repository.deleteUser(params);
+  }
+}
+
 class DeleteUsersUseCase {
   DeleteUsersUseCase(this._repository);
 
   final IUsersRepository _repository;
 
-  Future<List<void>> call(List<String> userUuids) async {
+  Future<List<void>> call(List<DeleteUserParams> listOfParams) async {
     return await Future.wait(
-      userUuids.map((uuid) => _repository.deleteUser(DeleteUserParams(uuid: uuid))),
+      listOfParams.map(_repository.deleteUser),
     );
   }
 }
