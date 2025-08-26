49b18217dcd6240716d386fab3011b4355e82cbb
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 03:45:48 +0300

    feat: implement create and retrieve methods for role bindings in the repository

--- lib/src/layer_data/repositories/role_bindings_repository.dart
+++ lib/src/layer_data/repositories/role_bindings_repository.dart
@@ -1,5 +1,8 @@
+import 'package:genesis/src/layer_data/requests/role_bindings/create_role_binding_req.dart';
 import 'package:genesis/src/layer_data/requests/role_bindings/delete_role_binding_req.dart';
+import 'package:genesis/src/layer_data/requests/role_bindings/get_role_bindings_req.dart';
 import 'package:genesis/src/layer_data/source/remote/interfaces/i_role_bindings_api.dart';
+import 'package:genesis/src/layer_domain/entities/role_binding.dart';
 import 'package:genesis/src/layer_domain/repositories/i_role_bindings_repository.dart';
 
 final class RoleBindingsRepository implements IRoleBindingsRepository {
@@ -8,7 +11,18 @@ final class RoleBindingsRepository implements IRoleBindingsRepository {
   final IRoleBindingsApi _roleBindingsApi;
 
   @override
+  Future<void> createRoleBinding(params) async {
+    await _roleBindingsApi.createRoleBinding(CreateRoleBindingReq(params));
+  }
+
+  @override
   Future<void> deleteRoleBinding(uuid) async {
     await _roleBindingsApi.deleteRoleBinding(DeleteRoleBindingReq(uuid: uuid));
+  }
+
+  @override
+  Future<List<RoleBinding>> getRoleBindings(params) async {
+    final dtos = await _roleBindingsApi.getRoleBindings(GetRoleBindingsReq(params));
+    return dtos.map((it) => it.toEntity()).toList();
   }
 }
