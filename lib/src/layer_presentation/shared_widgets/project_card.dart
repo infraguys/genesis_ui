0351551a94383625a86327c895416a40921fb8cd
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 04:13:31 +0300

    feat: update ProjectCard to handle role deletion with RoleBindingsBloc

--- lib/src/layer_presentation/shared_widgets/project_card.dart
+++ lib/src/layer_presentation/shared_widgets/project_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
+import 'package:genesis/src/layer_presentation/blocs/role_bindings_bloc/role_bindings_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/hexagon_icon_button.dart';
 import 'package:genesis/src/theming/palette.dart';
 
@@ -9,37 +11,19 @@ class ProjectCard extends StatelessWidget {
   const ProjectCard({
     required this.project,
     required this.roles,
+    required this.userUuid,
     super.key,
-  }) : _isEmpty = false,
-       _onTap = null;
+  });
 
-  const ProjectCard.empty({
-    super.key,
-    VoidCallback? onTap,
-  }) : _isEmpty = true,
-       project = null,
-       roles = null,
-       _onTap = onTap;
-
-  final bool _isEmpty;
   final Project? project;
   final List<Role>? roles;
-  final VoidCallback? _onTap;
+  final String userUuid;
 
   @override
   Widget build(BuildContext context) {
-    if (_isEmpty) {
-      return Material(
-        color: Colors.transparent,
-        child: _SimpleProjectCard(
-          child: InkWell(
-            onTap: _onTap,
-            child: Center(child: Icon(Icons.add, color: Palette.color6DCF91, size: 32)),
-          ),
-        ),
-      );
-    }
-    return _SimpleProjectCard(
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: Palette.color333333,
       child: Padding(
         padding: EdgeInsets.all(18.0),
         child: Row(
@@ -54,12 +38,23 @@ class ProjectCard extends StatelessWidget {
                   Text(project!.description, style: TextStyle(color: Colors.white, fontSize: 14)),
                   SizedBox(height: 50),
                   Wrap(
+                    spacing: 6.0,
                     children: roles!
                         .map(
                           (it) => Chip(
+                            color: WidgetStatePropertyAll(Palette.color6DCF91.withValues(alpha: 10)),
+                            surfaceTintColor: Colors.transparent,
                             label: Text(it.name.capitalize, style: TextStyle(fontSize: 14)),
                             deleteIcon: Icon(Icons.close),
-                            onDeleted: () {},
+                            onDeleted: () {
+                              context.read<RoleBindingsBloc>().add(
+                                RoleBindingsEvent.delete(
+                                  userUuid: userUuid,
+                                  roleUuid: it.uuid,
+                                  projectUuid: project!.uuid,
+                                ),
+                              );
+                            },
                           ),
                         )
                         .toList(),
@@ -74,21 +69,6 @@ class ProjectCard extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _SimpleProjectCard extends StatelessWidget {
-  const _SimpleProjectCard({required this.child, super.key});
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.hardEdge,
-      color: Palette.color333333,
-      child: child,
     );
   }
 }
