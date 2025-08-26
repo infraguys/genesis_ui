fa25b4335a25ef1c3e62281321258c5e3ef05db2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-26 11:10:50 +0300

    feat: enhance ProjectCard to handle project roles and navigation to AttachRolesPage

--- lib/src/layer_presentation/shared_widgets/project_card.dart
+++ lib/src/layer_presentation/shared_widgets/project_card.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
+import 'package:genesis/src/layer_domain/params/projects/get_projects_params.dart';
 import 'package:genesis/src/layer_presentation/blocs/role_bindings_bloc/role_bindings_bloc.dart';
+import 'package:genesis/src/layer_presentation/blocs/user_projects_bloc/user_projects_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/hexagon_icon_button.dart';
+import 'package:genesis/src/routing/app_router.dart';
 import 'package:genesis/src/theming/palette.dart';
+import 'package:go_router/go_router.dart';
 
 class ProjectCard extends StatelessWidget {
   const ProjectCard({
@@ -14,7 +18,7 @@ class ProjectCard extends StatelessWidget {
     super.key,
   });
 
-  final Project? project;
+  final Project project;
   final List<Role>? roles;
   final String userUuid;
 
@@ -33,11 +37,12 @@ class ProjectCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // todo: вынести в типографику
-                  Text(project!.name, style: TextStyle(color: Colors.white, fontSize: 20)),
-                  Text(project!.description, style: TextStyle(color: Colors.white, fontSize: 14)),
+                  Text(project.name, style: TextStyle(color: Colors.white, fontSize: 20)),
+                  Text(project.description, style: TextStyle(color: Colors.white, fontSize: 14)),
                   SizedBox(height: 50),
                   Wrap(
                     spacing: 6.0,
+                    runSpacing: 6.0,
                     children: roles!
                         .map(
                           (it) => InputChip(
@@ -47,7 +52,7 @@ class ProjectCard extends StatelessWidget {
                                 RoleBindingsEvent.delete(
                                   userUuid: userUuid,
                                   roleUuid: it.uuid,
-                                  projectUuid: project!.uuid,
+                                  projectUuid: project.uuid,
                                 ),
                               );
                             },
@@ -60,7 +65,21 @@ class ProjectCard extends StatelessWidget {
             ),
             HexagonIconButton(
               iconData: Icons.add,
-              onPressed: () {},
+              onPressed: () async {
+                final bloc = context.read<UserProjectsBloc>();
+                final isCreated = await context.pushNamed<bool>(
+                  AppRoutes.attachRoles.name,
+                  pathParameters: {
+                    'projectUuid': project.uuid,
+                    ...GoRouterState.of(context).pathParameters,
+                  },
+                  extra: GoRouterState.of(context).extra,
+                );
+
+                if (isCreated == true) {
+                  bloc.add(UserProjectsEvent.getProjects(GetProjectsParams(userUuid: userUuid)));
+                }
+              },
             ),
           ],
         ),
