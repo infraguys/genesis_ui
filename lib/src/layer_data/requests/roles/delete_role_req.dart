de6c2e3beab5b411eaf7b4805fb738d4f3d807e1
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 13:11:28 +0300

    refactor: update DeleteRoleReq to use UUID directly instead of DeleteRoleParams

--- lib/src/layer_data/requests/roles/delete_role_req.dart
+++ lib/src/layer_data/requests/roles/delete_role_req.dart
@@ -1,18 +1,13 @@
 import 'package:genesis/src/core/interfaces/path_encodable.dart';
 import 'package:genesis/src/core/network/endpoints/roles_endpoints.dart';
-import 'package:genesis/src/layer_domain/params/roles/delete_role_params.dart';
 
 final class DeleteRoleReq implements PathEncodable {
-  DeleteRoleReq(this._params);
+  DeleteRoleReq(this.uuid);
 
-  final DeleteRoleParams _params;
+  final String uuid;
 
   @override
   String toPath() {
-    return RolesEndpoints.deleteRole(_params.uuid);
+    return RolesEndpoints.deleteRole(uuid);
   }
-}
-
-extension DeleteRoleParamsX on DeleteRoleParams {
-  DeleteRoleReq toReq() => DeleteRoleReq(this);
 }
