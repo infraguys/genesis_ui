278c67736827c12d7022e681e67a8cb59919db57
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 14:29:42 +0300

    feat: add use cases for deleting single and multiple role bindings

---
+++ lib/src/layer_domain/use_cases/role_bindings/delete_role_bindings_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:genesis/src/layer_domain/repositories/i_role_bindings_repository.dart';
+
+final class DeleteRoleBindingUseCase {
+  DeleteRoleBindingUseCase(this._repository);
+
+  final IRoleBindingsRepository _repository;
+
+  Future<void> call(String uuid) async {
+    return await _repository.deleteRoleBinding(uuid);
+  }
+}
+
+final class DeleteRoleBindingsUseCase {
+  DeleteRoleBindingsUseCase(this._repository);
+
+  final IRoleBindingsRepository _repository;
+
+  Future<void> call(List<String> uuids) async {
+    await Future.wait(uuids.map(_repository.deleteRoleBinding));
+  }
+}
