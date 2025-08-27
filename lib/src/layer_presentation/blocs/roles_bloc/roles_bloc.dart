df5e8e6cd6cb66ca458da5ed877d66e1cd183fac
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 19:33:05 +0300

    refactor: update RolesBloc to use separate repositories for roles and permission bindings

--- lib/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart
+++ lib/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
-import 'package:genesis/src/layer_domain/params/roles/delete_role_params.dart';
+import 'package:genesis/src/layer_domain/params/permission_bindings_params/get_permission_bindings_params.dart';
 import 'package:genesis/src/layer_domain/params/roles/get_roles_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_permission_bindings_repository.dart';
 import 'package:genesis/src/layer_domain/repositories/i_roles_repositories.dart';
+import 'package:genesis/src/layer_domain/use_cases/permission_bindings_usecases/delete_permission_bindings_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/permission_bindings_usecases/get_permission_bindings_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/roles/delete_roles_usecase.dart';
 import 'package:genesis/src/layer_domain/use_cases/roles/get_roles.dart';
 
@@ -10,29 +13,41 @@ part 'roles_event.dart';
 part 'roles_state.dart';
 
 class RolesBloc extends Bloc<RolesEvent, RolesState> {
-  RolesBloc(this._repository) : super(RolesState.init()) {
+  RolesBloc({
+    required IRolesRepository rolesRepository,
+    required IPermissionBindingsRepository permissionBindingsRepository,
+  }) : _rolesRepository = rolesRepository,
+       _permissionBindingsRepository = permissionBindingsRepository,
+       super(RolesState.init()) {
     on(_onGetRoles);
     on(_onDeleteRoles);
     add(RolesEvent.getRoles());
   }
 
-  final IRolesRepository _repository;
+  final IRolesRepository _rolesRepository;
+  final IPermissionBindingsRepository _permissionBindingsRepository;
 
   Future<void> _onGetRoles(_GetRoles event, Emitter<RolesState> emit) async {
-    final usesCase = GetRoles(_repository);
-    final params = GetRolesParams(
-      userUuid: event.userUuid,
-      projectUuid: event.projectUuid,
-    );
+    final usesCase = GetRolesUseCase(_rolesRepository);
     emit(RolesState.loading());
-    final roles = await usesCase(params);
+    final roles = await usesCase(event.params);
     emit(RolesState.loaded(roles));
   }
 
   Future<void> _onDeleteRoles(_DeleteRoles event, Emitter<RolesState> emit) async {
-    final useCase = DeleteRolesUseCase(_repository);
+    final getPermissionBindingsUseCase = GetPermissionBindingsUseCase(_permissionBindingsRepository);
+    final deletePermissionBindingsUseCase = DeletePermissionBindingsUseCase(_permissionBindingsRepository);
+    final deleteRolesUseCase = DeleteRolesUseCase(_rolesRepository);
+
     emit(RolesState.loading());
-    await useCase(event.listOfParams);
+
+    final permissionBindings = await getPermissionBindingsUseCase(
+      GetPermissionBindingsParams(role: event.roles.single.uuid),
+    );
+
+    await deletePermissionBindingsUseCase(permissionBindings);
+
+    await deleteRolesUseCase(event.roles);
     add(RolesEvent.getRoles());
   }
 }
