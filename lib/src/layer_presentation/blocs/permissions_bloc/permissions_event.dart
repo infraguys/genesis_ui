3cddbd3fdf2918560b2b74fb7ead99b542c7b0e6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 14:27:11 +0300

    feat: update getPermissions event to accept optional parameters

--- lib/src/layer_presentation/blocs/permissions_bloc/permissions_event.dart
+++ lib/src/layer_presentation/blocs/permissions_bloc/permissions_event.dart
@@ -1,7 +1,11 @@
 part of 'permissions_bloc.dart';
 
 sealed class PermissionsEvent {
-  factory PermissionsEvent.getPermissions() = _GetPermissions;
+  factory PermissionsEvent.getPermissions([GetPermissionsParams params]) = _GetPermissions;
 }
 
-final class _GetPermissions implements PermissionsEvent {}
+final class _GetPermissions implements PermissionsEvent {
+  const _GetPermissions([this.params = const GetPermissionsParams()]);
+
+  final GetPermissionsParams params;
+}
