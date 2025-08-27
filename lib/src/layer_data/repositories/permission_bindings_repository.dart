d8f95af722481702c37090b9aad6ab3553c95bd2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 16:32:09 +0300

    feat: implement getPermissionBindings method in PermissionBindingsRepository for retrieving permission bindings

--- lib/src/layer_data/repositories/permission_bindings_repository.dart
+++ lib/src/layer_data/repositories/permission_bindings_repository.dart
@@ -1,4 +1,6 @@
 import 'package:genesis/src/layer_data/requests/permission_bindings/create_permission_binding_req.dart';
+import 'package:genesis/src/layer_data/requests/permission_bindings/delete_permission_binding_req.dart';
+import 'package:genesis/src/layer_data/requests/permission_bindings/get_permission_bindings_req.dart';
 import 'package:genesis/src/layer_data/source/remote/interfaces/i_permission_bindings_api.dart';
 import 'package:genesis/src/layer_domain/entities/permission_binding.dart';
 import 'package:genesis/src/layer_domain/repositories/i_permission_bindings_repository.dart';
@@ -9,6 +11,12 @@ final class PermissionBindingsRepository implements IPermissionBindingsRepositor
   final IPermissionBindingsApi _permissionBindingsApi;
 
   @override
+  Future<List<PermissionBinding>> getPermissionBindings(params) async {
+    final dtos = await _permissionBindingsApi.getPermissionBindings(GetPermissionBindingsReq(params));
+    return dtos.map((it) => it.toEntity()).toList();
+  }
+
+  @override
   Future<PermissionBinding> createPermissionBinding(params) async {
     final dto = await _permissionBindingsApi.createPermissionBinding(CreatePermissionBindingReq(params));
     return dto.toEntity();
@@ -16,6 +24,6 @@ final class PermissionBindingsRepository implements IPermissionBindingsRepositor
 
   @override
   Future<void> deletePermissionBinding(uuid) async {
-    throw UnimplementedError();
+    await _permissionBindingsApi.deletePermissionBinding(DeletePermissionBindingReq(uuid));
   }
 }
