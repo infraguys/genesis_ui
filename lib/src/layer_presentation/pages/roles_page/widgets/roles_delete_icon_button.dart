878116d67839be232e892e5c49af1ce2b9ba3f16
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 13:12:02 +0300

    refactor: update deleteRoles event to accept state directly instead of mapping to DeleteRoleParams

--- lib/src/layer_presentation/pages/roles_page/widgets/roles_delete_icon_button.dart
+++ lib/src/layer_presentation/pages/roles_page/widgets/roles_delete_icon_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
-import 'package:genesis/src/layer_domain/params/roles/delete_role_params.dart';
 import 'package:genesis/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/roles_selection_bloc/roles_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
@@ -18,8 +17,7 @@ class RolesDeleteIconButton extends StatelessWidget {
         }
         return DeleteIconButton(
           onPressed: () {
-            final listOfParams = state.map((it) => DeleteRoleParams(uuid: it.uuid));
-            context.read<RolesBloc>().add(RolesEvent.deleteRoles(listOfParams.toList()));
+            context.read<RolesBloc>().add(RolesEvent.deleteRoles(state));
           },
         );
       },
