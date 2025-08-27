49db531a2de9567be9075d2eb3483db56000f8c6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-28 01:52:37 +0300

    refactor: update DeleteUsersIconButton to use DeleteUsersDialog

--- lib/src/layer_presentation/pages/users/users_page/widgets/delete_users_icon_button.dart
+++ lib/src/layer_presentation/pages/users/users_page/widgets/delete_users_icon_button.dart
@@ -4,7 +4,7 @@ import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_user_dialog.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_users_dialog.dart';
 
 class DeleteUsersIconButton extends StatelessWidget {
   const DeleteUsersIconButton({super.key});
@@ -20,7 +20,7 @@ class DeleteUsersIconButton extends StatelessWidget {
           onPressed: () async {
             await showDialog<void>(
               context: context,
-              builder: (context) => DeleteUserDialog(
+              builder: (context) => DeleteUsersDialog(
                 users: state,
                 onDelete: () {
                   context.read<UsersBloc>().add(UsersEvent.deleteUsers(state));
