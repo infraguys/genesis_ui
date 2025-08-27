5291b267402d2ca1fbf60800ca95fe8541330b13
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-27 16:28:23 +0300

    feat: add getPermissionBindings method to IPermissionBindingsRepository for retrieving permission bindings

--- lib/src/layer_domain/repositories/i_permission_bindings_repository.dart
+++ lib/src/layer_domain/repositories/i_permission_bindings_repository.dart
@@ -1,8 +1,11 @@
 import 'package:genesis/src/layer_domain/entities/permission_binding.dart';
 import 'package:genesis/src/layer_domain/params/permission_bindings_params/create_permission_binding_params.dart';
+import 'package:genesis/src/layer_domain/params/permission_bindings_params/get_permission_bindings_params.dart';
 
 abstract interface class IPermissionBindingsRepository {
   Future<PermissionBinding> createPermissionBinding(CreatePermissionBindingParams params);
+
+  Future<List<PermissionBinding>> getPermissionBindings(GetPermissionBindingsParams params);
 
   Future<void> deletePermissionBinding(String uuid);
 }
