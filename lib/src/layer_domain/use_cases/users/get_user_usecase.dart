478811bef420d6f87a8815bb1700e2afa2140d6f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-28 14:17:00 +0300

    refactor: update GetUserUseCase to accept UUID directly instead of GetUserParams

--- lib/src/layer_domain/use_cases/users/get_user_usecase.dart
+++ lib/src/layer_domain/use_cases/users/get_user_usecase.dart
@@ -1,5 +1,4 @@
 import 'package:genesis/src/layer_domain/entities/user.dart';
-import 'package:genesis/src/layer_domain/params/users/get_user_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 
 class GetUserUseCase {
@@ -7,7 +6,7 @@ class GetUserUseCase {
 
   final IUsersRepository _repository;
 
-  Future<User> call(GetUserParams params) async {
-    return await _repository.getUser(params);
+  Future<User> call(String uuid) async {
+    return await _repository.getUser(uuid);
   }
 }
