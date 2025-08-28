76174b5d398c5099b708a3ee4ee124e2155db9b2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-28 15:53:51 +0300

    refactor: rename CreateUserPage to _CreateUserView and introduce a new CreateUserPage widget for improved structure

--- lib/src/layer_presentation/pages/users/create_user_page/create_user_page.dart
+++ lib/src/layer_presentation/pages/users/create_user_page/create_user_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/layer_domain/params/users/create_user_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/layer_presentation/blocs/user_bloc/user_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_snackbar.dart';
@@ -12,14 +13,14 @@ import 'package:genesis/src/layer_presentation/shared_widgets/buttons_bar.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/save_icon_button.dart';
 import 'package:go_router/go_router.dart';
 
-class CreateUserPage extends StatefulWidget {
-  const CreateUserPage({super.key});
+class _CreateUserView extends StatefulWidget {
+  const _CreateUserView();
 
   @override
-  State<CreateUserPage> createState() => _CreateUserPageState();
+  State<_CreateUserView> createState() => _CreateUserViewState();
 }
 
-class _CreateUserPageState extends State<CreateUserPage> {
+class _CreateUserViewState extends State<_CreateUserView> {
   final _formKey = GlobalKey<FormState>();
   final _formController = _FormController();
   late final UserBloc _userBloc;
@@ -154,4 +155,22 @@ class _FormController extends FormControllersManager {
 
   @override
   List<TextEditingController> get all => [firstName, lastName, email, username, password];
+}
+
+class CreateUserPage extends StatelessWidget {
+  const CreateUserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return UserBloc(context.read<IUsersRepository>());
+          },
+        ),
+      ],
+      child: _CreateUserView(),
+    );
+  }
 }
