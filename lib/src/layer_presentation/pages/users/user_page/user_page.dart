a7574bcc96b505dcbc6035651e5f2c90612dfa15
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-28 15:54:05 +0300

    refactor: rename UserPage to _UserView and update user handling to use UUID directly

--- lib/src/layer_presentation/pages/users/user_page/user_page.dart
+++ lib/src/layer_presentation/pages/users/user_page/user_page.dart
@@ -5,11 +5,16 @@ import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/params/users/update_user_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_projects_repository.dart';
+import 'package:genesis/src/layer_domain/repositories/i_role_bindings_repository.dart';
+import 'package:genesis/src/layer_domain/repositories/i_roles_repositories.dart';
+import 'package:genesis/src/layer_domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/layer_presentation/blocs/user_bloc/user_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/user_projects_bloc/user_projects_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/users/user_page/widgets/delete_user_icon_button.dart';
 import 'package:genesis/src/layer_presentation/pages/users/user_page/widgets/list_of_projects.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_snackbar.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_text_input.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
@@ -19,29 +24,29 @@ import 'package:genesis/src/layer_presentation/shared_widgets/status_label.dart'
 import 'package:genesis/src/layer_presentation/shared_widgets/verified_label.dart';
 import 'package:go_router/go_router.dart';
 
-class UserPage extends StatefulWidget {
-  const UserPage({required this.user, super.key});
+class _UserView extends StatefulWidget {
+  const _UserView({required this.userUUID});
 
-  final User user;
+  final String userUUID;
 
   @override
-  State<UserPage> createState() => _UserPageState();
+  State<_UserView> createState() => _UserViewState();
 }
 
-class _UserPageState extends State<UserPage> {
+class _UserViewState extends State<_UserView> {
   final _formKey = GlobalKey<FormState>();
   late _ControllersManager _controllersManager;
   late final UserBloc _userBloc;
+  late final User user;
 
   @override
   void initState() {
-    _userBloc = context.read<UserBloc>();
-    context.read<UserProjectsBloc>().add(
-      UserProjectsEvent.getProjects(widget.user.uuid),
-    );
-    // context.read<UserRolesBloc>().add(UserRolesEvent.getRolesByUser(widget.user.uuid));
-    _controllersManager = _ControllersManager(widget.user);
     super.initState();
+    _userBloc = context.read<UserBloc>();
+    _userBloc.add(UserEvent.getUser(widget.userUUID));
+    context.read<UserProjectsBloc>().add(UserProjectsEvent.getProjects(widget.userUUID));
+    // context.read<UserRolesBloc>().add(UserRolesEvent.getRolesByUser(widget.user.uuid));
+    // _controllersManager = _ControllersManager(user);
   }
 
   @override
@@ -55,11 +60,12 @@ class _UserPageState extends State<UserPage> {
     final textTheme = TextTheme.of(context);
 
     return Scaffold(
-      body: BlocListener<UserBloc, UserState>(
+      body: BlocConsumer<UserBloc, UserState>(
         listenWhen: (previous, current) => switch (current) {
           UserUpdatedState() => true,
           UserDeletedState() => true,
           UserFailureState() => true,
+          UserLoadedState() => true,
           _ => false,
         },
         listener: (context, state) {
@@ -71,153 +77,161 @@ class _UserPageState extends State<UserPage> {
             case UserUpdatedState():
             case UserDeletedState():
               context.read<UsersBloc>().add(UsersEvent.getUsers());
+            case UserLoadedState(:final user):
+              this.user = user;
+              _controllersManager = _ControllersManager(this.user);
             case UserFailureState(:final message):
               snack = AppSnackBar.failure(message);
             default:
           }
 
-          scaffoldMessenger.showSnackBar(snack).closed.then(navigator.pop);
+          // scaffoldMessenger.showSnackBar(snack).closed.then(navigator.pop);
         },
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 24.0,
-            children: [
-              Breadcrumbs(
-                items: [
-                  BreadcrumbItem(text: context.$.users),
-                  BreadcrumbItem(text: widget.user.username),
-                ],
-              ),
-              ButtonsBar(
-                children: [
-                  DeleteUserIconButton(user: widget.user),
-                  SaveIconButton(onPressed: save),
-                ],
-              ),
-              LayoutBuilder(
-                builder: (context, constraints) {
-                  return SizedBox(
-                    height: 410,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: constraints.maxWidth * 0.5,
-                          child: Form(
-                            key: _formKey,
+        builder: (context, state) {
+          if (state is! UserLoadedState) {
+            return AppProgressIndicator();
+          }
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 24.0,
+              children: [
+                Breadcrumbs(
+                  items: [
+                    BreadcrumbItem(text: context.$.users),
+                    BreadcrumbItem(text: user.username),
+                  ],
+                ),
+                ButtonsBar(
+                  children: [
+                    DeleteUserIconButton(user: user),
+                    SaveIconButton(onPressed: save),
+                  ],
+                ),
+                LayoutBuilder(
+                  builder: (context, constraints) {
+                    return SizedBox(
+                      height: 410,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: constraints.maxWidth * 0.5,
+                            child: Form(
+                              key: _formKey,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                spacing: 24,
+                                children: [
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.usernameController,
+                                      hintText: context.$.username,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.firstNameController,
+                                      hintText: context.$.firstName,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.lastNameController,
+                                      hintText: context.$.lastName,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.surnameController,
+                                      hintText: context.$.surName,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.phoneController,
+                                      hintText: 'Phone'.hardcoded,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: AppTextInput(
+                                      controller: _controllersManager.emailController,
+                                      hintText: context.$.email,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: constraints.maxWidth * 0.5,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              spacing: 24,
+                              // spacing: 48,
                               children: [
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.usernameController,
-                                    hintText: context.$.username,
-                                  ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  spacing: 8.0,
+                                  children: [
+                                    Text(context.$.status),
+                                    StatusLabel(status: user.status),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  spacing: 8.0,
+                                  children: [
+                                    Text(context.$.verification),
+                                    VerifiedLabel(isVerified: user.emailVerified),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  spacing: 8.0,
+                                  children: [
+                                    Text(context.$.createdAt),
+                                    Text(user.createdAt.toString()),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  spacing: 8.0,
+                                  children: [
+                                    Text(context.$.updatedAt),
+                                    Text(user.updatedAt.toString()),
+                                  ],
                                 ),
                                 SizedBox(
                                   width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.firstNameController,
-                                    hintText: context.$.firstName,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.lastNameController,
-                                    hintText: context.$.lastName,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.surnameController,
-                                    hintText: context.$.surName,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.phoneController,
-                                    hintText: 'Phone'.hardcoded,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  child: AppTextInput(
-                                    controller: _controllersManager.emailController,
-                                    hintText: context.$.email,
+                                  child: AppTextInput.multiLine(
+                                    controller: _controllersManager.descriptionController,
+                                    hintText: context.$.description,
+                                    maxLines: 4,
+                                    minLines: 4,
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: constraints.maxWidth * 0.5,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            // spacing: 48,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.status),
-                                  StatusLabel(status: widget.user.status),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.verification),
-                                  VerifiedLabel(isVerified: widget.user.emailVerified),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.createdAt),
-                                  Text(widget.user.createdAt.toString()),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.updatedAt),
-                                  Text(widget.user.updatedAt.toString()),
-                                ],
-                              ),
-                              SizedBox(
-                                width: constraints.maxWidth * 0.4,
-                                child: AppTextInput.multiLine(
-                                  controller: _controllersManager.descriptionController,
-                                  hintText: context.$.description,
-                                  maxLines: 4,
-                                  minLines: 4,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              Text(context.$.projects, style: textTheme.headlineLarge),
-              ListOfProjects(userUuid: widget.user.uuid),
-            ],
-          ),
-        ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                Text(context.$.projects, style: textTheme.headlineLarge),
+                ListOfProjects(userUuid: user.uuid),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
@@ -227,7 +241,7 @@ class _UserPageState extends State<UserPage> {
       _userBloc.add(
         UserEvent.updateUser(
           UpdateUserParams(
-            uuid: widget.user.uuid,
+            uuid: user.uuid,
             username: _controllersManager.usernameController.text,
             description: _controllersManager.descriptionController.text,
             firstName: _controllersManager.firstNameController.text,
@@ -270,4 +284,33 @@ class _ControllersManager extends FormControllersManager {
     phoneController,
     emailController,
   ];
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({required this.userUUID, super.key});
+
+  final String userUUID;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return UserBloc(context.read<IUsersRepository>());
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return UserProjectsBloc(
+              projectsRepository: context.read<IProjectsRepository>(),
+              roleBindingsRepository: context.read<IRoleBindingsRepository>(),
+              rolesRepository: context.read<IRolesRepository>(),
+            );
+          },
+        ),
+      ],
+      child: _UserView(userUUID: userUUID),
+    );
+  }
 }
