706d9a6f17a97cf329a21e97f15156d384ee8121
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-28 14:16:49 +0300

    refactor: update GetUserReq to use UUID directly instead of GetUserParams

--- lib/src/layer_data/requests/users/get_user_req.dart
+++ lib/src/layer_data/requests/users/get_user_req.dart
@@ -1,14 +1,13 @@
 import 'package:genesis/src/core/interfaces/path_encodable.dart';
 import 'package:genesis/src/core/network/endpoints/users_endpoints.dart';
-import 'package:genesis/src/layer_domain/params/users/get_user_params.dart';
 
 final class GetUserReq implements PathEncodable {
-  const GetUserReq(this._params);
+  const GetUserReq(this._uuid);
 
-  final GetUserParams _params;
+  final String _uuid;
 
   @override
   String toPath() {
-    return UsersEndpoints.getUser(_params.uuid);
+    return UsersEndpoints.getUser(_uuid);
   }
 }
