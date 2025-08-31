31cd8f86c7b6fcb225cbfb9f3d76f0cd08346992
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-31 19:24:06 +0300

    refactor: update DeleteUserReq to use UUID directly instead of params

--- lib/src/layer_data/requests/users/delete_user_req.dart
+++ lib/src/layer_data/requests/users/delete_user_req.dart
@@ -1,14 +1,13 @@
 import 'package:genesis/src/core/interfaces/path_encodable.dart';
 import 'package:genesis/src/core/network/endpoints/users_endpoints.dart';
-import 'package:genesis/src/layer_domain/params/users/delete_user_params.dart';
 
 final class DeleteUserReq implements PathEncodable {
-  DeleteUserReq(this._params);
+  DeleteUserReq(this._uuid);
 
-  final DeleteUserParams _params;
+  final String _uuid;
 
   @override
   String toPath() {
-    return UsersEndpoints.deleteUser(_params.uuid);
+    return UsersEndpoints.deleteUser(_uuid);
   }
 }
