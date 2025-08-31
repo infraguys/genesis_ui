6782374b2337631cb452600a70a4136aae230fcf
Eugene Koretsky <E.Koretsky@outlook.com>
2025-08-31 19:25:19 +0300

    refactor: update deleteUser method to accept UUID instead of DeleteUserParams

--- lib/src/layer_domain/repositories/i_users_repository.dart
+++ lib/src/layer_domain/repositories/i_users_repository.dart
@@ -2,7 +2,6 @@ import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/params/users/change_user_password_params.dart';
 import 'package:genesis/src/layer_domain/params/users/confirm_email_params.dart';
 import 'package:genesis/src/layer_domain/params/users/create_user_params.dart';
-import 'package:genesis/src/layer_domain/params/users/delete_user_params.dart';
 import 'package:genesis/src/layer_domain/params/users/get_users_params.dart';
 import 'package:genesis/src/layer_domain/params/users/update_user_params.dart';
 
@@ -13,7 +12,7 @@ abstract interface class IUsersRepository {
 
   Future<User> updateUser(UpdateUserParams params);
 
-  Future<void> deleteUser(DeleteUserParams params);
+  Future<void> deleteUser(String uuid);
 
   Future<User> changeUserPassword(ChangeUserPasswordParams params);
 
