d453c923fd3305a256c3a1c40806b38632c49b45
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:00:53 +0300

    feat: rename DeleteProjectsIconButton to DeleteProjectsElevatedButton and update usage

--- lib/src/layer_presentation/pages/projects_page/widgets/delete_projects_elevated_button.dart
+++ lib/src/layer_presentation/pages/projects_page/widgets/delete_projects_elevated_button.dart
@@ -4,10 +4,10 @@ import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_presentation/blocs/projects_bloc/projects_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/projects_selection_bloc/projects_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/projects_page/widgets/delete_projects_dialog.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
 
-class DeleteProjectsIconButton extends StatelessWidget {
-  const DeleteProjectsIconButton({super.key});
+class DeleteProjectsElevatedButton extends StatelessWidget {
+  const DeleteProjectsElevatedButton({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class DeleteProjectsIconButton extends StatelessWidget {
         if (state.isEmpty) {
           return SizedBox.shrink();
         }
-        return DeleteIconButton(
+        return DeleteElevatedButton(
           onPressed: () async {
             await showDialog<void>(
               context: context,
