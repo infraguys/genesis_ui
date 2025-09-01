65c5eac9614b6a6f13ea0843ec2ee084cbafa76f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:48:29 +0300

    feat: add DeleteRoleElevatedButton for role deletion functionality

---
+++ lib/src/layer_presentation/pages/role_page/widgets/delete_role_elevated_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/layer_domain/entities/role.dart';
+import 'package:genesis/src/layer_presentation/blocs/role_bloc/role_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_roles_dialog.dart';
+
+class DeleteRoleElevatedButton extends StatelessWidget {
+  const DeleteRoleElevatedButton({required this.role, super.key});
+
+  final Role role;
+
+  @override
+  Widget build(BuildContext context) {
+    return DeleteElevatedButton(
+      onPressed: () async {
+        final roleBloc = context.read<RoleBloc>();
+        await showDialog<void>(
+          context: context,
+          builder: (context) {
+            return DeleteRolesDialog.single(
+              role: role,
+              onDelete: () => roleBloc.add(RoleEvent.delete(role)),
+            );
+          },
+        );
+      },
+    );
+    ;
+  }
+}
