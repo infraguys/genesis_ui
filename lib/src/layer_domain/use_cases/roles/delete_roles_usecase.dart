df957230a244b5c5a84a36210f456ae6e5b5cc98
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:46:43 +0300

    feat: implement DeleteRoleUseCase for single role deletion

--- lib/src/layer_domain/use_cases/roles/delete_roles_usecase.dart
+++ lib/src/layer_domain/use_cases/roles/delete_roles_usecase.dart
@@ -1,6 +1,16 @@
 import 'package:genesis/src/layer_domain/entities/role.dart';
 import 'package:genesis/src/layer_domain/repositories/i_roles_repositories.dart';
 
+final class DeleteRoleUseCase {
+  DeleteRoleUseCase(this._repository);
+
+  final IRolesRepository _repository;
+
+  Future<void> call(RoleUUID uuid) async {
+    await _repository.deleteRole(uuid);
+  }
+}
+
 final class DeleteRolesUseCase {
   DeleteRolesUseCase(this._repository);
 
