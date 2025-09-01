afc399a33d5af0e59ec87539bb548de37a58aeae
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-01 03:51:21 +0300

    refactor: update UsersActionsPopupMenuButton to integrate UsersBloc and simplify delete functionality

--- lib/src/layer_presentation/pages/users/users_page/widgets/users_actions_popup_menu_button.dart
+++ lib/src/layer_presentation/pages/users/users_page/widgets/users_actions_popup_menu_button.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
-import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/change_user_password_dialog.dart';
-import 'package:provider/provider.dart';
+import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_users_dialog.dart';
+import 'package:genesis/src/theming/palette.dart';
 
 class UsersActionsPopupMenuButton extends StatelessWidget {
   const UsersActionsPopupMenuButton({required this.user, super.key});
@@ -11,39 +13,50 @@ class UsersActionsPopupMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<_PopupBtnValue>(
-      icon: Icon(Icons.more_vert),
-      onSelected: (value) {
-        final child = switch (value) {
-          _PopupBtnValue.changePassword => ChangeUserPasswordDialog(),
-        };
-        showDialog<void>(
-          context: context,
-          builder: (_) {
-            return Provider.value(
-              value: context.read<User>(),
-              child: child,
-            );
-          },
-        );
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      useRootNavigator: true,
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            value: _PopupBtnValue.changePassword,
-            child: Text(context.$.changePassword),
-          ),
-          PopupMenuItem(
-            child: Text(context.$.changeEmail),
-          ),
-        ];
-      },
+    return TooltipVisibility(
+      visible: false,
+      child: PopupMenuButton<_PopupBtnValue>(
+        clipBehavior: Clip.antiAlias,
+        padding: EdgeInsets.zero,
+        icon: Icon(Icons.more_vert),
+        useRootNavigator: true,
+        onSelected: (value) {
+          final users = List.generate(1, (_) => user);
+          final child = switch (value) {
+            // _PopupBtnValue.changePassword => ChangeUserPasswordDialog(),
+            _PopupBtnValue.delete => DeleteUsersDialog(
+              users: users,
+              onDelete: () => context.read<UsersBloc>().add(UsersEvent.deleteUsers(users)),
+            ),
+          };
+          showDialog<void>(
+            context: context,
+            builder: (_) {
+              return child;
+            },
+          );
+        },
+        itemBuilder: (context) {
+          return [
+            // PopupMenuItem(
+            //   value: _PopupBtnValue.changePassword,
+            //   child: Text(context.$.changePassword),
+            // ),
+            PopupMenuItem(
+              value: _PopupBtnValue.delete,
+              child: Text(
+                context.$.delete,
+                style: TextStyle(color: Palette.colorF04C4C),
+              ),
+            ),
+          ];
+        },
+      ),
     );
   }
 }
 
 enum _PopupBtnValue {
-  changePassword,
+  // changePassword,
+  delete,
 }
