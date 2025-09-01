ed10ea3d6cedcd77ea7b892144668a76ef5b0d4d
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-01 03:50:13 +0300

    refactor: update onDelete callback type in DeleteProjectsDialog to VoidCallback

--- lib/src/layer_presentation/pages/projects_page/widgets/delete_projects_dialog.dart
+++ lib/src/layer_presentation/pages/projects_page/widgets/delete_projects_dialog.dart
@@ -8,7 +8,7 @@ class DeleteProjectsDialog extends StatelessWidget {
   const DeleteProjectsDialog({required this.projects, super.key, this.onDelete});
 
   final List<Project> projects;
-  final void Function(List<Project> projects)? onDelete;
+  final VoidCallback? onDelete;
 
   String getContent(List<Project> projects) {
     if (projects.length == 1) {
@@ -30,7 +30,7 @@ class DeleteProjectsDialog extends StatelessWidget {
           style: TextButton.styleFrom(foregroundColor: Palette.colorF04C4C),
           onPressed: () {
             context.pop();
-            onDelete?.call(projects);
+            onDelete?.call();
           },
           child: Text(context.$.ok),
         ),
