29b469add40cd95231467f9defb91cb7a09d3c71
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-01 15:20:34 +0300

    feat: update notNull method to enforce String return type for callbacks

--- lib/src/core/extensions/nullable_extension.dart
+++ lib/src/core/extensions/nullable_extension.dart
@@ -1,3 +1,3 @@
 extension Nullable<T> on T? {
-  T? notNull(T Function(T it) cb) => this != null ? cb(this as T) : null;
+  String? notNull(String Function(T it) cb) => this != null ? cb(this as T) : null;
 }
