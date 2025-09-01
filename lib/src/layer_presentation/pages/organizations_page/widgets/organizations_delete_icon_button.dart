ffd831dcbe4748387877b6afd0eec9e982f56a0f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:05:37 +0300

    feat: rename OrganizationsDeleteIconButton to use DeleteElevatedButton

--- lib/src/layer_presentation/pages/organizations_page/widgets/organizations_delete_icon_button.dart
+++ lib/src/layer_presentation/pages/organizations_page/widgets/organizations_delete_icon_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/organization.dart';
 import 'package:genesis/src/layer_presentation/blocs/organizations_bloc/organizations_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/organizations_selection_bloc/organizations_selection_bloc.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
 
 class OrganizationsDeleteIconButton extends StatelessWidget {
   const OrganizationsDeleteIconButton({super.key});
@@ -15,9 +15,8 @@ class OrganizationsDeleteIconButton extends StatelessWidget {
         if (state.isEmpty) {
           return SizedBox.shrink();
         }
-        return DeleteIconButton(
+        return DeleteElevatedButton(
           onPressed: () {
-            // final listOfParams = state.map((it) => DeleteOrganizationParams(it.uuid));
             context.read<OrganizationsBloc>().add(OrganizationsEvent.deleteOrganizations(state));
           },
         );
