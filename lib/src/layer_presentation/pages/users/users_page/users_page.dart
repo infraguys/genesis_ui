98d2e0a9521d9f9a6b8ee6bec78b46a33bcdadbd
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:08:15 +0300

    feat: rename DeleteUsersIconButton to DeleteUsersElevatedButton in users_page.dart

--- lib/src/layer_presentation/pages/users/users_page/users_page.dart
+++ lib/src/layer_presentation/pages/users/users_page/users_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
-import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/delete_users_icon_button.dart';
+import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/delete_users_elevated_button.dart';
 import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/users_confirm_email_icon_button.dart';
 import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/users_create_icon_button.dart';
 import 'package:genesis/src/layer_presentation/pages/users/users_page/widgets/users_table.dart';
@@ -30,7 +30,7 @@ class _UsersView extends StatelessWidget {
           children: [
             SearchInput(),
             Spacer(),
-            DeleteUsersIconButton(),
+            DeleteUsersElevatedButton(),
             UsersConfirmEmailIconButton(),
             UsersCreateIconButton(),
           ],
