f051351a4d8391781c41a862ac0126d91a4770fd
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:06:00 +0300

    fix: update getBindings event to use RoleUUID instead of Role

--- lib/src/layer_presentation/blocs/permission_bindings_bloc/permission_bindings_event.dart
+++ lib/src/layer_presentation/blocs/permission_bindings_bloc/permission_bindings_event.dart
@@ -1,11 +1,11 @@
 part of 'permission_bindings_bloc.dart';
 
 sealed class PermissionBindingsEvent {
-  factory PermissionBindingsEvent.getBindings(Role role) = _GetBindings;
+  factory PermissionBindingsEvent.getBindings(RoleUUID uuid) = _GetBindings;
 }
 
 final class _GetBindings implements PermissionBindingsEvent {
-  _GetBindings(this.role);
+  _GetBindings(this.uuid);
 
-  final Role role;
+  final RoleUUID uuid;
 }
