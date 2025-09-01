42cd8c272de835ce92d1363985e599717de5cb97
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-01 14:07:55 +0300

    refactor: simplify permissions_table by removing unused PermissionBindingsBloc listener

--- lib/src/layer_presentation/shared_widgets/permissions_table.dart
+++ lib/src/layer_presentation/shared_widgets/permissions_table.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/layer_domain/entities/permission.dart';
-import 'package:genesis/src/layer_presentation/blocs/permission_bindings_bloc/permission_bindings_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/permissions_selection_bloc/permissions_selection_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_snackbar.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_table.dart';
@@ -19,94 +18,84 @@ class PermissionsTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<PermissionBindingsBloc, PermissionBindingsState>(
-      listenWhen: (_, current) => current is PermissionBindingsLoaded,
-      listener: (context, state) {
-        if (state is PermissionBindingsLoaded) {
-          context.read<PermissionsSelectionBloc>().add(
-            PermissionsSelectionEvent.setCheckedFromResponse(bindings: state.bindings, allPermissions: permissions),
-          );
-        }
-      },
-      child: AppTable(
-        columnSpans: [
-          TableSpan(extent: FixedSpanExtent(40.0)),
-          TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
-          TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
-          TableSpan(extent: FractionalTableSpanExtent(4 / 10)),
-          TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
-          TableSpan(extent: FixedSpanExtent(56.0)),
-        ],
-        headerCells: [
+    return AppTable(
+      columnSpans: [
+        TableSpan(extent: FixedSpanExtent(40.0)),
+        TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
+        TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
+        TableSpan(extent: FractionalTableSpanExtent(4 / 10)),
+        TableSpan(extent: FractionalTableSpanExtent(2 / 10)),
+        TableSpan(extent: FixedSpanExtent(56.0)),
+      ],
+      headerCells: [
+        BlocBuilder<PermissionsSelectionBloc, List<Permission>>(
+          builder: (context, state) {
+            return Checkbox(
+              tristate: true,
+              onChanged: (_) => context.read<PermissionsSelectionBloc>().add(
+                PermissionsSelectionEvent.toggleAll(permissions),
+              ),
+              value: switch (state.length) {
+                0 => false,
+                final len when len == permissions.length => true,
+                _ => null,
+              },
+            );
+          },
+        ),
+        Text(context.$.role, style: TextStyle(color: Colors.white)),
+        Text(context.$.status, style: TextStyle(color: Colors.white)),
+        Text(context.$.uuid, style: TextStyle(color: Colors.white)),
+        Text(context.$.createdAt, style: TextStyle(color: Colors.white)),
+      ],
+      length: permissions.length,
+      cellsBuilder: (index) {
+        final permission = permissions[index];
+        return [
           BlocBuilder<PermissionsSelectionBloc, List<Permission>>(
             builder: (context, state) {
               return Checkbox(
-                tristate: true,
+                value: state.contains(permission),
                 onChanged: (_) => context.read<PermissionsSelectionBloc>().add(
-                  PermissionsSelectionEvent.toggleAll(permissions),
+                  PermissionsSelectionEvent.toggle(permission),
                 ),
-                value: switch (state.length) {
-                  0 => false,
-                  final len when len == permissions.length => true,
-                  _ => null,
-                },
               );
             },
           ),
-          Text(context.$.role, style: TextStyle(color: Colors.white)),
-          Text(context.$.status, style: TextStyle(color: Colors.white)),
-          Text(context.$.uuid, style: TextStyle(color: Colors.white)),
-          Text(context.$.createdAt, style: TextStyle(color: Colors.white)),
-        ],
-        length: permissions.length,
-        cellsBuilder: (index) {
-          final role = permissions[index];
-          return [
-            BlocBuilder<PermissionsSelectionBloc, List<Permission>>(
-              builder: (context, state) {
-                return Checkbox(
-                  value: state.contains(role),
-                  onChanged: (_) => context.read<PermissionsSelectionBloc>().add(
-                    PermissionsSelectionEvent.toggle(role),
+          Text(permission.name, style: TextStyle(color: Colors.white)),
+          StatusLabel(status: permission.status),
+          RichText(
+            text: TextSpan(
+              children: [
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: SelectableText(
+                    permission.uuid.value,
+                    style: TextStyle(color: Colors.white, fontFamily: GoogleFonts.robotoMono().fontFamily),
                   ),
-                );
-              },
-            ),
-            Text(role.name, style: TextStyle(color: Colors.white)),
-            StatusLabel(status: role.status),
-            RichText(
-              text: TextSpan(
-                children: [
-                  WidgetSpan(
-                    alignment: PlaceholderAlignment.middle,
-                    child: SelectableText(
-                      role.uuid,
-                      style: TextStyle(color: Colors.white, fontFamily: GoogleFonts.robotoMono().fontFamily),
-                    ),
+                ),
+                WidgetSpan(child: const SizedBox(width: 8)),
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: IconButton(
+                    icon: Icon(Icons.copy, color: Colors.white, size: 18),
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: permission.uuid.value));
+                      final snack = AppSnackBar.success('Скопировано в буфер обмена: ${permission.uuid.value}');
+                      ScaffoldMessenger.of(context).showSnackBar(snack);
+                    },
                   ),
-                  WidgetSpan(child: const SizedBox(width: 8)),
-                  WidgetSpan(
-                    alignment: PlaceholderAlignment.middle,
-                    child: IconButton(
-                      icon: Icon(Icons.copy, color: Colors.white, size: 18),
-                      onPressed: () {
-                        Clipboard.setData(ClipboardData(text: role.uuid));
-                        final snack = AppSnackBar.success('Скопировано в буфер обмена: ${role.uuid}');
-                        ScaffoldMessenger.of(context).showSnackBar(snack);
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Text(
-              DateFormat('dd.MM.yyyy HH:mm').format(role.createdAt),
-              style: TextStyle(color: Colors.white, fontFamily: GoogleFonts.robotoMono().fontFamily),
-            ),
-            SizedBox.shrink(),
-          ];
-        },
-      ),
+          ),
+          Text(
+            DateFormat('dd.MM.yyyy HH:mm').format(permission.createdAt),
+            style: TextStyle(color: Colors.white, fontFamily: GoogleFonts.robotoMono().fontFamily),
+          ),
+          SizedBox.shrink(),
+        ];
+      },
     );
   }
 }
