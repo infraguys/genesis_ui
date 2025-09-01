d78b6c260667627817502f681f3b2f36c2d589a2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:03:52 +0300

    feat: rename DeleteUsersIconButton to DeleteUsersElevatedButton and update usage

--- lib/src/layer_presentation/pages/users/users_page/widgets/delete_users_elevated_button.dart
+++ lib/src/layer_presentation/pages/users/users_page/widgets/delete_users_elevated_button.dart
@@ -3,11 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_bloc/users_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/users_selection_bloc/users_selection_bloc.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_icon_button.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/delete_users_dialog.dart';
 
-class DeleteUsersIconButton extends StatelessWidget {
-  const DeleteUsersIconButton({super.key});
+class DeleteUsersElevatedButton extends StatelessWidget {
+  const DeleteUsersElevatedButton({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +16,11 @@ class DeleteUsersIconButton extends StatelessWidget {
         if (state.isEmpty) {
           return const SizedBox.shrink();
         }
-        return DeleteIconButton(
+        return DeleteElevatedButton(
           onPressed: () async {
             await showDialog<void>(
               context: context,
-              builder: (context) => DeleteUsersDialog(
+              builder: (context) => DeleteUsersDialog.multiple(
                 users: state,
                 onDelete: () => context.read<UsersBloc>().add(UsersEvent.deleteUsers(state)),
               ),
