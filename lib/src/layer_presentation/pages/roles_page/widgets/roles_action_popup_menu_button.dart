2f688e88579f809da387775e6d1b66c7ef74ff29
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-01 03:49:42 +0300

    refactor: update RolesActionPopupMenuButton to use RolesBloc and simplify dialog handling

--- lib/src/layer_presentation/pages/roles_page/widgets/roles_action_popup_menu_button.dart
+++ lib/src/layer_presentation/pages/roles_page/widgets/roles_action_popup_menu_button.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
-import 'package:genesis/src/layer_domain/entities/project.dart';
 import 'package:genesis/src/layer_domain/entities/role.dart';
-import 'package:provider/provider.dart';
+import 'package:genesis/src/layer_presentation/blocs/roles_bloc/roles_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/roles_page/widgets/delete_roles_dialog.dart';
+import 'package:genesis/src/theming/palette.dart';
 
 class RolesActionPopupMenuButton extends StatelessWidget {
   const RolesActionPopupMenuButton({
@@ -14,43 +16,41 @@ class RolesActionPopupMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<_PopupBtnValue>(
-      icon: Icon(Icons.more_vert),
-      onSelected: (value) {
-        final child = switch (value) {
-          _PopupBtnValue.editRole => Placeholder(),
-          _PopupBtnValue.deleteRole => Placeholder(),
-        };
-        showDialog<void>(
-          context: context,
-          builder: (_) {
-            return Provider.value(
-              value: context.read<Project>(),
-              child: child,
-            );
-          },
-        );
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      useRootNavigator: true,
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            value: _PopupBtnValue.editRole,
-            child: Text(context.$.edit),
-          ),
-          PopupMenuItem(
-            value: _PopupBtnValue.deleteRole,
-            labelTextStyle: WidgetStatePropertyAll(TextStyle(color: Colors.red)),
-            child: Text(context.$.delete),
-          ),
-        ];
-      },
+    return TooltipVisibility(
+      visible: false,
+      child: PopupMenuButton<_PopupBtnValue>(
+        clipBehavior: Clip.antiAlias,
+        icon: Icon(Icons.more_vert),
+        onSelected: (value) {
+          final roles = List.generate(1, (_) => role);
+          final child = switch (value) {
+            _PopupBtnValue.deleteRole => DeleteRolesDialog(
+              roles: roles,
+              onDelete: () => context.read<RolesBloc>().add(RolesEvent.deleteRoles(roles)),
+            ),
+          };
+          showDialog<void>(
+            context: context,
+            builder: (_) {
+              return child;
+            },
+          );
+        },
+        useRootNavigator: true,
+        itemBuilder: (context) {
+          return [
+            PopupMenuItem(
+              value: _PopupBtnValue.deleteRole,
+              labelTextStyle: WidgetStatePropertyAll(TextStyle(color: Palette.colorF04C4C)),
+              child: Text(context.$.delete),
+            ),
+          ];
+        },
+      ),
     );
   }
 }
 
 enum _PopupBtnValue {
-  editRole,
   deleteRole,
 }
