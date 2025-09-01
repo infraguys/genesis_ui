e925a24290dabbcf7e790954b289f1e44846ff3a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-02 00:47:44 +0300

    feat: refactor RoleEvent to unify get role event and add delete role event

--- lib/src/layer_presentation/blocs/role_bloc/role_event.dart
+++ lib/src/layer_presentation/blocs/role_bloc/role_event.dart
@@ -1,15 +1,17 @@
 part of 'role_bloc.dart';
 
 sealed class RoleEvent {
-  factory RoleEvent.getRole(RoleUUID uuid) = _GetRole;
+  factory RoleEvent.get(RoleUUID uuid) = _Get;
 
   factory RoleEvent.create(CreateRoleParams params) = _Create;
 
   factory RoleEvent.update(UpdateRoleParams params) = _Update;
+
+  factory RoleEvent.delete(Role role) = _Delete;
 }
 
-final class _GetRole implements RoleEvent {
-  const _GetRole(this.uuid);
+final class _Get implements RoleEvent {
+  const _Get(this.uuid);
 
   final RoleUUID uuid;
 }
@@ -24,4 +26,10 @@ final class _Update implements RoleEvent {
   const _Update(this.params);
 
   final UpdateRoleParams params;
+}
+
+final class _Delete implements RoleEvent {
+  const _Delete(this.role);
+
+  final Role role;
 }
