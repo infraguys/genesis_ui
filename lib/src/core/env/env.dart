4fc83eb60bbb97c6b2408d5133b39819c16f765b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-05 01:20:50 +0300

    add web splash

--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -1,40 +1,39 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
+
 
 abstract class Env {
-  static Future<void> loadConfig() async {
-    if (kIsWeb) {
-      final uri = Uri.base.resolve('config.json?ts=${DateTime.now().millisecondsSinceEpoch}');
-      final Response(:data, :statusCode) = await Dio().getUri<Map<String, dynamic>>(
-        uri,
-        options: Options(
-          headers: {'Cache-Control': 'no-cache'},
-        ),
-      );
-      if (data == null) {
-        throw Exception('Failed to load config: HTTP $statusCode');
-      }
-    }
-    // Натив: сначала Documents, потом assets
-    //   try {
-    //     // dart:io путь к файлу
-    //     final dir = await getApplicationDocumentsDirectory(); // path_provider
-    //     final file = File('${dir.path}/config.json');
-    //     if (await file.exists()) {
-    //       return jsonDecode(await file.readAsString()) as Map<String, dynamic>;
-    //     }
-    //   } catch (_) {/* пропускаем и идём к assets */}
-    //   final asset = await rootBundle.loadString('assets/config.json');
-    //   return jsonDecode(asset) as Map<String, dynamic>;
-    // }
-  }
+  // static Future<void> loadConfig() async {
+  //   if (kIsWeb) {
+  //     final uri = Uri.base.resolve('config.json?ts=${DateTime.now().millisecondsSinceEpoch}');
+  //     final Response(:data, :statusCode) = await Dio().getUri<Map<String, dynamic>>(
+  //       uri,
+  //       options: Options(
+  //         headers: {'Cache-Control': 'no-cache'},
+  //       ),
+  //     );
+  //     if (data == null) {
+  //       throw Exception('Failed to load config: HTTP $statusCode');
+  //     }
+  //   }
+  //   // Натив: сначала Documents, потом assets
+  //   //   try {
+  //   //     // dart:io путь к файлу
+  //   //     final dir = await getApplicationDocumentsDirectory(); // path_provider
+  //   //     final file = File('${dir.path}/config.json');
+  //   //     if (await file.exists()) {
+  //   //       return jsonDecode(await file.readAsString()) as Map<String, dynamic>;
+  //   //     }
+  //   //   } catch (_) {/* пропускаем и идём к assets */}
+  //   //   final asset = await rootBundle.loadString('assets/config.json');
+  //   //   return jsonDecode(asset) as Map<String, dynamic>;
+  //   // }
+  // }
 
   // static void _setConfig(Map<String, dynamic> config) {
   //   _apiUrl = config['api_url'] as String? ?? String.fromEnvironment('api_url');
   //
   // }
 
-  static final _envString = String.fromEnvironment('env', defaultValue: EnvMode.unknown.name);
+  static const _envString = String.fromEnvironment('env');
 
   static const apiUrl = String.fromEnvironment('api_url');
 
