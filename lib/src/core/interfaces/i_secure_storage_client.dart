e3ee9224ddc2bd629219a10ea09f3d91c25a4140
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-08 00:55:06 +0300

    delete app_loc

--- lib/src/core/interfaces/i_secure_storage_client.dart
+++ lib/src/core/interfaces/i_secure_storage_client.dart
@@ -1,3 +1,3 @@
 import 'package:genesis/src/core/interfaces/i_simple_storage_client.dart';
 
-abstract interface class SecureStorageClient implements ISimpleStorageClient {}
+abstract interface class ISecureStorageClient implements ISimpleStorageClient {}
--- lib/src/layer_presentation/blocs/app_bloc/app_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'app_bloc.dart';
-
-sealed class AppEvent {
-  factory AppEvent.loading() = _Loading;
-}
-
-final class _Loading implements AppEvent {}
