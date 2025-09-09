d057373d893c21e3c541da69bdb4b309ada0ea9e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-09 11:15:32 +0300

    feat: add sign out

--- lib/src/layer_domain/repositories/i_auth_repository.dart
+++ lib/src/layer_domain/repositories/i_auth_repository.dart
@@ -4,5 +4,7 @@ import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
 abstract interface class IAuthRepository {
   Future<User> signIn(SignInParams params);
 
+  Future<void> signOut();
+
   Future<User> restoreSession();
 }
