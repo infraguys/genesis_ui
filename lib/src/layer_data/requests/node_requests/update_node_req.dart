becb31bce2be427c4d96114c8cee6c807750cab0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-18 11:14:24 +0300

    fix: implement update_node_req

--- lib/src/layer_data/requests/node_requests/update_node_req.dart
+++ lib/src/layer_data/requests/node_requests/update_node_req.dart
@@ -1,1 +1,29 @@
-final class UpdateNodeReq {}
+import 'package:genesis/src/core/interfaces/json_encodable.dart';
+import 'package:genesis/src/core/interfaces/path_encodable.dart';
+import 'package:genesis/src/core/network/endpoints/nodes_endpoints.dart';
+import 'package:genesis/src/layer_data/dtos/node_type_dto.dart';
+import 'package:genesis/src/layer_domain/params/nodes_params/update_node_params.dart';
+
+final class UpdateNodeReq implements JsonEncodable, PathEncodable {
+  UpdateNodeReq(this._params);
+
+  final UpdateNodeParams _params;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _params.name,
+      'cores': _params.cores,
+      'ram': _params.cores,
+      'root_disk_size': _params.rootDiskSize,
+      'image': _params.image,
+      'node_type': NodeTypeDto.of(_params.nodeType).toJson(),
+      'description': _params.description,
+    };
+  }
+
+  @override
+  String toPath() {
+    return NodesEndpoints.updateNode(_params.uuid);
+  }
+}
