0dad7b856ac39ee375de7decee2dd034797da0be
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-18 11:13:42 +0300

    feat: add update_node_params

---
+++ lib/src/layer_domain/params/nodes_params/update_node_params.dart
@@ -0,0 +1,23 @@
+import 'package:genesis/src/layer_domain/entities/node.dart';
+
+final class UpdateNodeParams {
+  const UpdateNodeParams({
+    required this.uuid,
+    required this.name,
+    required this.description,
+    required this.cores,
+    required this.ram,
+    required this.rootDiskSize,
+    required this.image,
+    required this.nodeType,
+  });
+
+  final NodeUUID uuid;
+  final String name;
+  final String description;
+  final int cores;
+  final int ram;
+  final int rootDiskSize;
+  final String image;
+  final NodeType nodeType;
+}
