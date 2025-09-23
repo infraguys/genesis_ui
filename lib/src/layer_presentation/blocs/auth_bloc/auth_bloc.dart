51c83d261de3e3f038f89739f25be44a8cfb059e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-23 11:05:33 +0300

    refactor: update method names in AuthBloc for consistency

--- lib/src/layer_presentation/blocs/auth_bloc/auth_bloc.dart
+++ lib/src/layer_presentation/blocs/auth_bloc/auth_bloc.dart
@@ -6,22 +6,22 @@ import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_auth_repository.dart';
 import 'package:genesis/src/layer_domain/use_cases/auth_usecases/restore_session_usecase.dart';
-import 'package:genesis/src/layer_domain/use_cases/users/sign_in_use_case.dart';
-import 'package:genesis/src/layer_domain/use_cases/users/sign_out_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/auth_usecases/sign_in_use_case.dart';
+import 'package:genesis/src/layer_domain/use_cases/auth_usecases/sign_out_usecase.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(this._authRepository) : super(AuthState.unauthenticated()) {
-    on(_signIn);
-    on(_signOut);
-    on(_restoreSession);
+    on(_onSignIn);
+    on(_onSignOut);
+    on(_onRestoreSession);
   }
 
   final IAuthRepository _authRepository;
 
-  Future<void> _signIn(_SingIn event, Emitter<AuthState> emit) async {
+  Future<void> _onSignIn(_SingIn event, Emitter<AuthState> emit) async {
     final useCase = SignInUseCase(_authRepository);
 
     try {
@@ -36,13 +36,13 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }
   }
 
-  Future<void> _signOut(_SingOut _, Emitter<AuthState> emit) async {
+  Future<void> _onSignOut(_SingOut _, Emitter<AuthState> emit) async {
     final useCase = SignOutUseCase(_authRepository);
     await useCase();
     emit(AuthState.unauthenticated());
   }
 
-  Future<void> _restoreSession(_RestoreSession event, Emitter<AuthState> emit) async {
+  Future<void> _onRestoreSession(_RestoreSession event, Emitter<AuthState> emit) async {
     final useCase = RestoreSessionUseCase(_authRepository);
     emit(AuthState.loading());
     try {
