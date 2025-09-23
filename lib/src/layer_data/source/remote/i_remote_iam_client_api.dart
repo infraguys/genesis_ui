ff832cfc039bbbbd31174ac17f0933b789287178
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-24 01:36:36 +0300

    feat: add introspectClient method to IRemoteIamClientApi

--- lib/src/layer_data/source/remote/i_remote_iam_client_api.dart
+++ lib/src/layer_data/source/remote/i_remote_iam_client_api.dart
@@ -1,5 +1,7 @@
+import 'package:genesis/src/layer_data/dtos/client_introspection_dto.dart';
 import 'package:genesis/src/layer_data/dtos/token_dto.dart';
 import 'package:genesis/src/layer_data/dtos/user_dto.dart';
+import 'package:genesis/src/layer_data/requests/iam_client_requests/get_introspection_req.dart';
 import 'package:genesis/src/layer_data/requests/users/get_current_user_req.dart';
 import 'package:genesis/src/layer_data/requests/users/sign_in_req.dart';
 
@@ -7,4 +9,6 @@ abstract interface class IRemoteIamClientApi {
   Future<TokenDto> createTokenByPassword(SignInReq req);
 
   Future<UserDto> getCurrentUser(GetCurrentUserReq req);
+
+  Future<ClientIntrospectionDto> introspectClient(GetIntrospectionReq req);
 }
