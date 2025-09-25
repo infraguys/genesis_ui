92b3a55f03b683d1cbafc7cf858abafc5efd0f2f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-25 09:44:17 +0300

    feat: add permission management extensions for users, organizations, and projects

---
+++ lib/src/layer_domain/iam/permission_names.dart
@@ -0,0 +1,55 @@
+extension type PermissionNames(Set<String> _set) {
+  /// Namespace: iam.users
+  UserPermissions get users => UserPermissions(_set);
+
+  /// Namespace: iam.organization
+  OrganizationPermissions get organizations => OrganizationPermissions(_set);
+
+  /// Namespace: iam.nodes
+  NodePermissions get nodes => NodePermissions(_set);
+
+  /// Namespace: iam.roles
+  RolePermissions get roles => RolePermissions(_set);
+
+  /// Namespace: iam.projects
+  ProjectPermissions get projects => ProjectPermissions(_set);
+
+  bool get isAdmin => _set.contains('*.*.*');
+}
+
+extension type OrganizationPermissions(Set<String> _set) implements PermissionNames {
+  bool get canReadAll => _set.contains('iam.organization.read_all') || isAdmin;
+
+  bool get canWriteAll => _set.contains('iam.organization.write_all') || isAdmin;
+
+  bool get canDeleteAll => _set.contains('iam.organization.delete_all') || isAdmin;
+
+  bool get canCreate => _set.contains('iam.organization.create') || isAdmin;
+
+  bool get canDelete => _set.contains('iam.organization.delete') || isAdmin;
+
+  bool get canDeleteAny => canDelete || canDeleteAll;
+}
+extension type UserPermissions(Set<String> _set) implements PermissionNames {
+  bool get canWriteAll => _set.contains('iam.user.write_all') || isAdmin;
+
+  bool get canReadAll => _set.contains('iam.user.read_all') || isAdmin;
+
+  bool get canDeleteAll => _set.contains('iam.user.delete_all') || isAdmin;
+
+  bool get canDelete => _set.contains('iam.user.delete') || isAdmin;
+
+  bool get canListAll => _set.contains('iam.user.list') || isAdmin;
+}
+extension type NodePermissions(Set<String> _set) {}
+extension type RolePermissions(Set<String> _set) {}
+
+extension type ProjectPermissions(Set<String> _set) implements PermissionNames {
+  bool get canReadAll => _set.contains('iam.organization.read_all') || isAdmin;
+
+  bool get canDeleteAll => _set.contains('iam.project.delete_all') || isAdmin;
+
+  bool get canWriteAll => _set.contains('iam.project.write_all') || isAdmin;
+
+  bool get canListAll => _set.contains('iam.project.list_all') || isAdmin;
+}
