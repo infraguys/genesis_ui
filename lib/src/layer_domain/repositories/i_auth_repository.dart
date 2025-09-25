ae79db15b729f4e6d518ff61d871676c0caac2e6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-25 05:31:17 +0300

    feat: update signIn and restoreSession methods to return user and permission names as a tuple

--- lib/src/layer_domain/repositories/i_auth_repository.dart
+++ lib/src/layer_domain/repositories/i_auth_repository.dart
@@ -1,10 +1,11 @@
 import 'package:genesis/src/layer_domain/entities/user.dart';
+import 'package:genesis/src/layer_domain/iam/permission_names.dart';
 import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
 
 abstract interface class IAuthRepository {
-  Future<User> signIn(SignInParams params);
+  Future<({User user, PermissionNames permissionNames})> signIn(SignInParams params);
 
   Future<void> signOut();
 
-  Future<User> restoreSession();
+  Future<({User user, PermissionNames permissionNames})> restoreSession();
 }
