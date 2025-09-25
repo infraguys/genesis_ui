60f1a51f0d13f4a03997db1602376e1a7cfde4eb
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-25 16:14:50 +0300

    feat: conditionally render delete user button based on permissions

--- lib/src/layer_presentation/pages/user_pages/user_details_page/widgets/delete_user_btn.dart
+++ lib/src/layer_presentation/pages/user_pages/user_details_page/widgets/delete_user_btn.dart
@@ -7,19 +7,22 @@ class _DeleteUserButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DeleteElevatedButton(
-      onPressed: () async {
-        final userBloc = context.read<UserBloc>();
-        await showDialog<void>(
-          context: context,
-          builder: (context) {
-            return ConfirmationDialog(
-              message: context.$.deleteUser(user.username),
-              onDelete: () => userBloc.add(UserEvent.deleteUser(user)),
-            );
-          },
-        );
-      },
+    return Visibility(
+      visible: context.permissionNames.users.canDeleteAll,
+      child: DeleteElevatedButton(
+        onPressed: () async {
+          final userBloc = context.read<UserBloc>();
+          await showDialog<void>(
+            context: context,
+            builder: (context) {
+              return ConfirmationDialog(
+                message: context.$.deleteUser(user.username),
+                onDelete: () => userBloc.add(UserEvent.deleteUser(user)),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
