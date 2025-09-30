6fb0ec8169d86624cb846a163e0a0574f7cc2f00
Eugene Koretsky <E.Koretsky@outlook.com>
2025-09-30 15:11:39 +0300

    refactor: simplify form handling in CreateOrganizationPage by removing controllers manager

--- lib/src/layer_presentation/pages/organization_pages/create_organization_page/create_organization_page.dart
+++ lib/src/layer_presentation/pages/organization_pages/create_organization_page/create_organization_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
-import 'package:genesis/src/core/interfaces/form_controllers.dart';
 import 'package:genesis/src/layer_domain/params/organizations/create_organization_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_organizations_repository.dart';
 import 'package:genesis/src/layer_presentation/blocs/organization_bloc/organization_bloc.dart';
 import 'package:genesis/src/layer_presentation/blocs/organizations_bloc/organizations_bloc.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_snackbar.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/app_text_input.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/buttons_bar.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/save_icon_button.dart';
@@ -23,27 +21,22 @@ class _CreateOrganizationView extends StatefulWidget {
 class _CreateOrganizationViewState extends State<_CreateOrganizationView> {
   final _formKey = GlobalKey<FormState>();
 
-  late _ControllersManager _controllersManager;
   late final OrganizationBloc _organizationBloc;
+
+  var _name = '';
+  var _description = '';
 
   @override
   void initState() {
     _organizationBloc = context.read<OrganizationBloc>();
-    _controllersManager = _ControllersManager();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controllersManager.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocListener<OrganizationBloc, OrganizationState>(
       listenWhen: (_, current) => switch (current) {
-        OrganizationCreatedState() || OrganizationFailureState() => true,
+        _ when current is! OrganizationLoadingState => true,
         _ => false,
       },
       listener: (context, state) {
@@ -55,6 +48,8 @@ class _CreateOrganizationViewState extends State<_CreateOrganizationView> {
             context.read<OrganizationsBloc>().add(OrganizationsEvent.getOrganizations());
             messenger.showSnackBar(AppSnackBar.success(context.$.success));
             navigator.pop();
+
+          case OrganizationPermissionFailureState(:final message):
           case OrganizationFailureState(:final message):
             messenger.showSnackBar(AppSnackBar.failure(message));
           default:
@@ -86,17 +81,24 @@ class _CreateOrganizationViewState extends State<_CreateOrganizationView> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       spacing: 24,
                       children: [
-                        AppTextInput(
-                          controller: _controllersManager.nameController,
-                          hintText: context.$.name,
+                        TextFormField(
+                          initialValue: _name,
+                          decoration: InputDecoration(
+                            hintText: context.$.name,
+                          ),
+                          onSaved: (newValue) => _name = newValue!,
                           validator: (value) => switch (value) {
                             _ when value!.isEmpty => context.$.requiredField,
                             _ => null,
                           },
                         ),
-                        AppTextInput.multiLine(
-                          controller: _controllersManager.descriptionController,
-                          hintText: context.$.description,
+                        TextFormField(
+                          initialValue: _description,
+                          decoration: InputDecoration(
+                            hintText: context.$.description,
+                          ),
+                          onSaved: (newValue) => _description = newValue!,
+                          minLines: 3,
                           maxLines: 3,
                         ),
                       ],
@@ -113,24 +115,12 @@ class _CreateOrganizationViewState extends State<_CreateOrganizationView> {
 
   void save() {
     if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
       _organizationBloc.add(
-        OrganizationEvent.create(
-          CreateOrganizationParams(
-            name: _controllersManager.nameController.text,
-            description: _controllersManager.descriptionController.text,
-          ),
-        ),
+        OrganizationEvent.create(CreateOrganizationParams(name: _name, description: _description,)),
       );
     }
   }
-}
-
-class _ControllersManager extends FormControllersManager {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
-
-  @override
-  List<TextEditingController> get all => [nameController, descriptionController];
 }
 
 class CreateOrganizationPage extends StatelessWidget {
