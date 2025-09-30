9b324354764a6c660564200919f3a2e69da668c6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-01 02:12:25 +0300

    feat: extend OrganizationsState with new states for deletion and permission failure

--- lib/src/layer_presentation/blocs/organizations_bloc/organizations_state.dart
+++ lib/src/layer_presentation/blocs/organizations_bloc/organizations_state.dart
@@ -1,13 +1,27 @@
 part of 'organizations_bloc.dart';
 
-sealed class OrganizationsState {}
+sealed class OrganizationsState {
+  bool get shouldRebuildList => this is OrganizationsLoadedState || this is OrganizationsLoadingState;
+}
 
-final class OrganizationsInitState implements OrganizationsState {}
+final class OrganizationsInitialState extends OrganizationsState {}
 
-final class OrganizationsLoadingState implements OrganizationsState {}
+final class OrganizationsLoadingState extends OrganizationsState {}
 
-final class OrganizationsLoadedState implements OrganizationsState {
+final class OrganizationsLoadedState extends OrganizationsState {
   OrganizationsLoadedState(this.organizations);
 
   final List<Organization> organizations;
+}
+
+final class OrganizationsDeletedState extends OrganizationsState {
+  OrganizationsDeletedState(this.organizations);
+
+  final List<Organization> organizations;
+}
+
+final class OrganizationsPermissionFailureState extends OrganizationsState {
+  OrganizationsPermissionFailureState(this.message);
+
+  final String message;
 }
