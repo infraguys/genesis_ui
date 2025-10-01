55affabcd0c04655aa499379e57ed834274b4ce6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-01 11:11:53 +0300

    feat: enhance user state management with failure and permission failure states

--- lib/src/layer_presentation/blocs/users_bloc/users_state.dart
+++ lib/src/layer_presentation/blocs/users_bloc/users_state.dart
@@ -8,20 +8,42 @@ sealed class UsersState {
   factory UsersState.loaded(List<User> users) = UsersLoadedState;
 
   factory UsersState.deleted(List<User> users) = UsersDeletedState;
+
+  factory UsersState.failure(String message) = UsersFailureState;
+
+  factory UsersState.permissionFailure(String message) = UsersPermissionFailureState;
 }
 
 final class UsersInitialState implements UsersState {}
 
 final class UsersLoadingState implements UsersState {}
 
-final class UsersDeletedState implements UsersState {
-  UsersDeletedState(this.users);
+final class UsersDeletedState extends _UsersDataState {
+  UsersDeletedState(super.users);
+}
+
+final class UsersLoadedState extends _UsersDataState {
+  UsersLoadedState(super.users);
+}
+
+// Base classes to reduce code duplication
+
+base class _UsersDataState implements UsersState {
+  _UsersDataState(this.users);
 
   final List<User> users;
 }
 
-final class UsersLoadedState implements UsersState {
-  UsersLoadedState(this.users);
+final class UsersFailureState extends _FailureState {
+  UsersFailureState(super.message);
+}
 
-  final List<User> users;
+final class UsersPermissionFailureState extends _FailureState {
+  UsersPermissionFailureState(super.message);
+}
+
+base class _FailureState implements UsersState {
+  _FailureState(this.message);
+
+  final String message;
 }
