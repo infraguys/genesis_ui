75a5c9cc0a39bbaa3337c4ec3beab0da8f9bfb5b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-07 03:12:29 +0300

    refactor: simplify drawer theme shape by removing border radius

--- lib/src/theming/drawer_theme_data.dart
+++ lib/src/theming/drawer_theme_data.dart
@@ -3,5 +3,5 @@ part of './app_theme.dart';
 final _drawerThemeData = DrawerThemeData(
   width: 240,
   backgroundColor: Colors.white,
-  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+  shape: RoundedRectangleBorder(),
 );
