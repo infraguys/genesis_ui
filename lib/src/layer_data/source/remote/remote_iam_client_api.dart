7067b8677f6460e4eb72fa9ce8306b039af3550a
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:05:06 +0300

    refactor: update getToken and getCurrentUser methods to use renamed request classes

--- lib/src/layer_data/source/remote/remote_iam_client_api.dart
+++ lib/src/layer_data/source/remote/remote_iam_client_api.dart
@@ -5,15 +5,15 @@ import 'package:genesis/src/layer_data/dtos/client_introspection_dto.dart';
 import 'package:genesis/src/layer_data/dtos/token_dto.dart';
 import 'package:genesis/src/layer_data/dtos/user_dto.dart';
 import 'package:genesis/src/layer_data/requests/iam_client_requests/get_introspection_req.dart';
+import 'package:genesis/src/layer_data/requests/iam_client_requests/token_req.dart';
 import 'package:genesis/src/layer_data/requests/users/get_current_user_req.dart';
-import 'package:genesis/src/layer_data/requests/users/get_token_req.dart';
 
 final class RemoteIamClientApi {
   RemoteIamClientApi(this._client);
 
   final RestClient _client;
 
-  Future<TokenDto> getToken(GetTokenReq req) async {
+  Future<TokenDto> getToken(TokenReq req) async {
     try {
       final Response(:data) = await _client.post<Map<String, dynamic>>(
         req.toPath(),
@@ -30,7 +30,7 @@ final class RemoteIamClientApi {
     }
   }
 
-  Future<UserDto> getCurrentUser(GetCurrentUserReq req) async {
+  Future<UserDto> getCurrentUser([GetCurrentUserReq req = const GetCurrentUserReq()]) async {
     try {
       final Response(:data, :requestOptions) = await _client.get<Map<String, dynamic>>(
         req.toPath(),
