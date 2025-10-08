952846a2eda9458cedba5a9633187c11e3035fcf
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:05:20 +0300

    feat: update IAuthRepository to return AuthSession for token management and session restoration

--- lib/src/layer_domain/repositories/i_auth_repository.dart
+++ lib/src/layer_domain/repositories/i_auth_repository.dart
@@ -1,11 +1,13 @@
-import 'package:genesis/src/layer_domain/entities/user.dart';
-import 'package:genesis/src/layer_domain/iam/permission_names.dart';
-import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
+import 'package:genesis/src/layer_domain/entities/auth_session.dart';
+import 'package:genesis/src/layer_domain/params/get_token_params.dart';
+import 'package:genesis/src/layer_domain/params/users/refresh_token_params.dart';
 
 abstract interface class IAuthRepository {
-  Future<({User user, PermissionNames permissionNames})> signIn(SignInParams params);
+  Future<AuthSession> getToken(GetTokenParams params);
+
+  Future<AuthSession> forceRefreshToken(RefreshTokenParams params);
 
   Future<void> signOut();
 
-  Future<({User user, PermissionNames permissionNames})> restoreSession();
+  Future<AuthSession> restoreSession();
 }
