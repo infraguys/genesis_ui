dcd6ee3d4d45292002047481ec50547084af003c
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:08:09 +0300

    feat: add refreshToken event for handling token refresh operations

--- lib/src/layer_presentation/blocs/auth_bloc/auth_event.dart
+++ lib/src/layer_presentation/blocs/auth_bloc/auth_event.dart
@@ -6,6 +6,8 @@ sealed class AuthEvent {
   factory AuthEvent.signOut() = _SingOut;
 
   factory AuthEvent.restoreSession() = _RestoreSession;
+
+  factory AuthEvent.refreshToken(RefreshTokenParams params) = _RefreshToken;
 }
 
 final class _SingIn implements AuthEvent {
@@ -18,3 +20,9 @@ final class _SingIn implements AuthEvent {
 final class _SingOut implements AuthEvent {}
 
 final class _RestoreSession implements AuthEvent {}
+
+final class _RefreshToken implements AuthEvent {
+  _RefreshToken(this._params);
+
+  final RefreshTokenParams _params;
+}
