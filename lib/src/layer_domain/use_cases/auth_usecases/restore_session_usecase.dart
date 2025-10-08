e05a9951264a7aa41e48f9fe1ec47b30f1f6f42f
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:05:13 +0300

    feat: update restoreSession use case to return AuthSession instead of user and permission names

--- lib/src/layer_domain/use_cases/auth_usecases/restore_session_usecase.dart
+++ lib/src/layer_domain/use_cases/auth_usecases/restore_session_usecase.dart
@@ -1,5 +1,4 @@
-import 'package:genesis/src/layer_domain/entities/user.dart';
-import 'package:genesis/src/layer_domain/iam/permission_names.dart';
+import 'package:genesis/src/layer_domain/entities/auth_session.dart';
 import 'package:genesis/src/layer_domain/repositories/i_auth_repository.dart';
 
 final class RestoreSessionUseCase {
@@ -7,7 +6,7 @@ final class RestoreSessionUseCase {
 
   final IAuthRepository _repository;
 
-  Future<({User user, PermissionNames permissionNames})> call() async {
+  Future<AuthSession> call() async {
     return await _repository.restoreSession();
   }
 }
