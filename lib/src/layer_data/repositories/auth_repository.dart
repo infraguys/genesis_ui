cb9a0a69ca4918f6738a6fd3702d36fd499012bb
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:08:38 +0300

    feat: update AuthRepository to use new token handling methods and return AuthSession

--- lib/src/layer_data/repositories/auth_repository.dart
+++ lib/src/layer_data/repositories/auth_repository.dart
@@ -1,36 +1,36 @@
 import 'package:genesis/src/core/exceptions/no_token_exception.dart';
 import 'package:genesis/src/layer_data/dtos/user_dto.dart';
 import 'package:genesis/src/layer_data/requests/iam_client_requests/get_introspection_req.dart';
+import 'package:genesis/src/layer_data/requests/iam_client_requests/token_req.dart';
 import 'package:genesis/src/layer_data/requests/projects/get_projects_req.dart';
 import 'package:genesis/src/layer_data/requests/users/get_current_user_req.dart';
-import 'package:genesis/src/layer_data/requests/users/sign_in_req.dart';
 import 'package:genesis/src/layer_data/source/local/token_dao.dart';
-import 'package:genesis/src/layer_data/source/remote/i_remote_iam_client_api.dart';
-import 'package:genesis/src/layer_data/source/remote/projects_api/i_projects_api.dart';
-import 'package:genesis/src/layer_domain/entities/user.dart';
+import 'package:genesis/src/layer_data/source/remote/projects_api/projects_api.dart';
+import 'package:genesis/src/layer_data/source/remote/remote_iam_client_api.dart';
+import 'package:genesis/src/layer_domain/entities/auth_session.dart';
 import 'package:genesis/src/layer_domain/iam/permission_names.dart';
 import 'package:genesis/src/layer_domain/repositories/i_auth_repository.dart';
 
 class AuthRepository implements IAuthRepository {
   AuthRepository({
-    required IRemoteIamClientApi iamApi,
+    required RemoteIamClientApi iamApi,
     required TokenDao tokenDao,
-    required IProjectsApi projectApi,
+    required ProjectsApi projectApi,
   }) : _iamApi = iamApi,
        _tokenDao = tokenDao,
        _projectsApi = projectApi;
 
-  final IRemoteIamClientApi _iamApi;
+  final RemoteIamClientApi _iamApi;
   final TokenDao _tokenDao;
-  final IProjectsApi _projectsApi;
+  final ProjectsApi _projectsApi;
 
   @override
-  Future<({User user, PermissionNames permissionNames})> signIn(params) async {
+  Future<AuthSession> getToken(params) async {
     late UserDto userDto;
 
-    final tokenDto = await _iamApi.createTokenByPassword(SignInReq(params));
-    await _tokenDao.writeToken(tokenDto.accessToken);
-    await _tokenDao.writeRefreshToken(tokenDto.refreshToken);
+    final tokenDto = await _iamApi.getToken(GetTokenReq(params));
+    await _tokenDao.writeAllTokens(accessToken: tokenDto.accessToken, refreshToken: tokenDto.refreshToken);
+
     userDto = await _iamApi.getCurrentUser(GetCurrentUserReq());
 
     if (userDto.username.toLowerCase() != 'admin') {
@@ -38,30 +38,51 @@ class AuthRepository implements IAuthRepository {
       final projectUuid = projectDtos.first.uuid;
 
       final paramsWithNewScope = params.copyWith(scope: 'project:$projectUuid');
-      final tokenDto = await _iamApi.createTokenByPassword(SignInReq(paramsWithNewScope));
-      await _tokenDao.writeToken(tokenDto.accessToken);
-      await _tokenDao.writeRefreshToken(tokenDto.refreshToken);
+      final tokenDto = await _iamApi.getToken(GetTokenReq(paramsWithNewScope));
+      await _tokenDao.writeAllTokens(accessToken: tokenDto.accessToken, refreshToken: tokenDto.refreshToken);
+
       userDto = await _iamApi.getCurrentUser(GetCurrentUserReq());
     }
     final clientIntrospectionDto = await _iamApi.introspectClient(GetIntrospectionReq());
 
-    return (
+    return AuthSession(
       user: userDto.toEntity(),
       permissionNames: PermissionNames(clientIntrospectionDto.permissions),
+      refreshToken: tokenDto.refreshToken,
     );
   }
 
   @override
-  Future<({User user, PermissionNames permissionNames})> restoreSession() async {
+  Future<AuthSession> forceRefreshToken(params) async {
+    late UserDto userDto;
+
+    final tokenDto = await _iamApi.getToken(RefreshTokenReq(params));
+    await _tokenDao.writeAllTokens(accessToken: tokenDto.accessToken, refreshToken: tokenDto.refreshToken);
+
+    userDto = await _iamApi.getCurrentUser();
+    final clientIntrospectionDto = await _iamApi.introspectClient(GetIntrospectionReq());
+
+    return AuthSession(
+      user: userDto.toEntity(),
+      permissionNames: PermissionNames(clientIntrospectionDto.permissions),
+      refreshToken: tokenDto.refreshToken,
+      scope: clientIntrospectionDto.projectId != null ? clientIntrospectionDto.projectId! : '',
+    );
+  }
+
+  @override
+  Future<AuthSession> restoreSession() async {
     final token = await _tokenDao.readToken();
+    final refreshToken = await _tokenDao.readRefreshToken();
     if (token == null || token.isEmpty) {
       throw NoTokenException();
     }
     final userDto = await _iamApi.getCurrentUser(GetCurrentUserReq());
     final clientIntrospectionDto = await _iamApi.introspectClient(GetIntrospectionReq());
-    return (
+    return AuthSession(
       user: userDto.toEntity(),
       permissionNames: PermissionNames(clientIntrospectionDto.permissions),
+      refreshToken: refreshToken!,
     );
   }
 
