63eb0e6b141496362439923d9b41a13d883b2dab
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 10:14:35 +0300

    refactor: rename sign_in_req.dart to get_token_req.dart and update class and parameter names

--- lib/src/layer_data/requests/iam_client_requests/get_token_req.dart
+++ lib/src/layer_data/requests/iam_client_requests/get_token_req.dart
@@ -2,20 +2,20 @@ import 'package:genesis/src/core/env/env.dart';
 import 'package:genesis/src/core/interfaces/json_encodable.dart';
 import 'package:genesis/src/core/interfaces/path_encodable.dart';
 import 'package:genesis/src/core/network/endpoints/clients_endpoints.dart';
-import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
+import 'package:genesis/src/layer_domain/params/get_token_params.dart';
 
-final class SignInReq implements JsonEncodable, PathEncodable {
-  const SignInReq(this._params);
+final class GetTokenReq implements JsonEncodable, PathEncodable {
+  const GetTokenReq(this._params);
 
-  final SignInParams _params;
+  final GetTokenParams _params;
 
   @override
   Map<String, dynamic> toJson() {
     return {
-      'grant_type': Env.grantType,
+      'grant_type': 'password',
       'client_id': Env.clientId,
       'client_secret': Env.clientSecret,
-      'scope': _params.scope,
+      'scope': ?_params.scope,
       'ttl': Env.ttl,
       'refresh_ttl': Env.refreshTtl,
       'username': _params.username,
