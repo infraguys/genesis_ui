9292a9e27d39817266da6485352fb892dfe409d0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-08 11:08:47 +0300

    feat: implement refresh token handling in AuthBloc and update sign-in use case

--- lib/src/layer_presentation/blocs/auth_bloc/auth_bloc.dart
+++ lib/src/layer_presentation/blocs/auth_bloc/auth_bloc.dart
@@ -2,12 +2,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/exceptions/api_exception.dart';
 import 'package:genesis/src/core/exceptions/network_exception.dart';
 import 'package:genesis/src/core/exceptions/no_token_exception.dart';
+import 'package:genesis/src/layer_domain/entities/auth_session.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_domain/iam/permission_names.dart';
-import 'package:genesis/src/layer_domain/params/sign_in_params.dart';
+import 'package:genesis/src/layer_domain/params/get_token_params.dart';
+import 'package:genesis/src/layer_domain/params/users/refresh_token_params.dart';
 import 'package:genesis/src/layer_domain/repositories/i_auth_repository.dart';
+import 'package:genesis/src/layer_domain/use_cases/auth_usecases/force_refresh_token_usecase.dart';
+import 'package:genesis/src/layer_domain/use_cases/auth_usecases/get_token_use_case.dart';
 import 'package:genesis/src/layer_domain/use_cases/auth_usecases/restore_session_usecase.dart';
-import 'package:genesis/src/layer_domain/use_cases/auth_usecases/sign_in_use_case.dart';
 import 'package:genesis/src/layer_domain/use_cases/auth_usecases/sign_out_usecase.dart';
 
 part 'auth_event.dart';
@@ -18,15 +21,16 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on(_onSignIn);
     on(_onSignOut);
     on(_onRestoreSession);
+    on(_onRefreshToken);
   }
 
   final IAuthRepository _authRepository;
 
   Future<void> _onSignIn(_SingIn event, Emitter<AuthState> emit) async {
-    final useCase = SignInUseCase(_authRepository);
+    final useCase = GetTokenUseCase(_authRepository);
 
     try {
-      final authSession = await useCase(SignInParams(username: event.username, password: event.password));
+      final authSession = await useCase(GetTokenParams(username: event.username, password: event.password));
       emit(AuthState.authenticated(authSession));
     } on ApiException catch (e) {
       emit(AuthState.failure(e.message));
@@ -51,6 +55,21 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       emit(AuthState.authenticated(authSession));
     } on NoTokenException catch (_) {
       emit(AuthState.unauthenticated());
+    } on ApiException catch (e) {
+      emit(AuthState.failure(e.message));
+    } on NetworkException catch (e) {
+      emit(AuthState.failure(e.message));
+    } on Exception catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> _onRefreshToken(_RefreshToken event, Emitter<AuthState> emit) async {
+    final useCase = ForceRefreshTokenUseCase(_authRepository);
+
+    try {
+      final authSession = await useCase(event._params);
+      emit(AuthState.authenticated(authSession));
     } on ApiException catch (e) {
       emit(AuthState.failure(e.message));
     } on NetworkException catch (e) {
