83a7e6f8fb09d24ac40bd295d78bc7c15c97ae4b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-10 14:25:48 +0300

    feat: define IPGInstancesRepository interface for PG instance operations

---
+++ lib/src/layer_domain/repositories/i_pg_instances.dart
@@ -0,0 +1,14 @@
+import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
+
+abstract interface class IPGInstancesRepository {
+  Future<PgInstance> getPgInstance(PgInstance uuid);
+
+  Future<List<PgInstance>> getPgInstances(GetPgInstancesParams params);
+
+  // Future<PGInstance> createNode(CreateNodeParams params);
+
+  // Future<PGInstance> updateNode(UpdateNodeParams params);
+
+  Future<void> deletePgInstance(PGInstanceUUID uuid);
+}
