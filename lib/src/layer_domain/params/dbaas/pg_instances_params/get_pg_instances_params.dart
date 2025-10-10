05285a8b35d3f3df9e0c2dbe251ccfd68339db75
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-10 10:18:16 +0300

    feat: add GetPgInstancesParams class for managing parameters in PG instance retrieval

---
+++ lib/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart
@@ -0,0 +1,33 @@
+import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+
+final class GetPgInstancesParams {
+  GetPgInstancesParams({
+    this.uuid,
+    this.name,
+    this.description,
+    this.projectId,
+    this.createdAt,
+    this.updatedAt,
+    this.status,
+    this.cpu,
+    this.ram,
+    this.diskSize,
+    this.nodesNumber,
+    this.syncReplicaNumber,
+    this.version,
+  });
+
+  final PGInstanceUUID? uuid;
+  final String? name;
+  final String? description;
+  final String? projectId;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final PGInstanceStatus? status;
+  final int? cpu;
+  final int? ram;
+  final int? diskSize;
+  final int? nodesNumber;
+  final int? syncReplicaNumber;
+  final String? version;
+}
