92eff06137dd3bbb9a24971747cba572af9533c0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-12 23:31:08 -0700

    feat: implement PgInstancesBloc for managing PG instances state

---
+++ lib/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/blocs/pg_instances_bloc/pg_instances_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/features/dbaas/domain/use_cases/get_pg_instances_usecase.dart';
+import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
+import 'package:genesis/src/layer_domain/repositories/i_pg_instances_repository.dart';
+
+part 'pg_instances_event.dart';
+part 'pg_instances_state.dart';
+
+class PgInstancesBloc extends Bloc<PgInstancesEvent, PgInstancesState> {
+  PgInstancesBloc(this._repository) : super(PgInstancesInitialState()) {
+    on(_onGetInstances);
+  }
+
+  final IPgInstancesRepository _repository;
+
+  Future<void> _onGetInstances(_GetInstances event, Emitter<PgInstancesState> emit) async {
+    final useCase = GetPgInstancesUseCase(_repository);
+    emit(PgInstancesLoadingState());
+    final instances = await useCase(event.params);
+    emit(PgInstancesLoadedState(instances));
+  }
+}
