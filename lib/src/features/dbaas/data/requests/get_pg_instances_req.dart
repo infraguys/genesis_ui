84bf7d5dd7abea61b39e764de302cc35852972b6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 07:46:53 -0700

    feat: rename get_pg_instances_req.dart and refactor to use extension methods for query encoding

---
+++ lib/src/features/dbaas/data/requests/get_pg_instances_req.dart
@@ -0,0 +1,33 @@
+import 'package:genesis/src/core/network/endpoints/pg_instances_endpoints.dart';
+import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
+
+extension GetPgInstancesReqExt on GetPgInstancesParams {
+  Map<String, dynamic> toQuery() {
+    return {
+      'uuid': id,
+      'name': ?name,
+      'description': ?description,
+      'project_id': ?projectId,
+      'created_at': ?createdAt?.toIso8601String(),
+      'updated_at': ?updatedAt?.toIso8601String(),
+      'status': ?_fromStatus(status),
+      'cpu': ?cores,
+      'ram': ?ram,
+      'disk_size': ?diskSize,
+      'nodes_number': ?nodesNumber,
+      'sync_replica_number': ?syncReplicaNumber,
+      'version': ?version,
+    };
+  }
+
+  String? _fromStatus(PgInstanceStatus? status) => switch (status) {
+    PgInstanceStatus.active => 'ACTIVE',
+    PgInstanceStatus.error => 'ERROR',
+    PgInstanceStatus.inProgress => 'IN_PROGRESS',
+    PgInstanceStatus.newStatus => 'NEW',
+    _ => null,
+  };
+
+  String toPath() => PgInstancesEndpoints.createInstance();
+}
