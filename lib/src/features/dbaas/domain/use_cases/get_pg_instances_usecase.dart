2808f6d3f82522974946336f2b76def2b8d833b2
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:43:47 -0700

    refactor: reorder import statements in get_pg_instances_usecase.dart

--- lib/src/features/dbaas/domain/use_cases/get_pg_instances_usecase.dart
+++ lib/src/features/dbaas/domain/use_cases/get_pg_instances_usecase.dart
@@ -1,6 +1,6 @@
+import 'package:genesis/src/features/dbaas/domain/repositories/i_pg_instances_repository.dart';
 import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
 import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
-import 'package:genesis/src/layer_domain/repositories/i_pg_instances_repository.dart';
 
 final class GetPgInstancesUseCase {
   GetPgInstancesUseCase(this._repository);
