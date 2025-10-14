8fb8488208eb72991b00b2f188fe775237161e44
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:51:08 -0700

    refactor: update PgInstance methods to use consistent naming and add createPgInstance method

--- lib/src/features/dbaas/data/source/remote/pg_instances_api.dart
+++ lib/src/features/dbaas/data/source/remote/pg_instances_api.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:genesis/src/core/exceptions/base_network_exception.dart';
 import 'package:genesis/src/core/network/rest_client/rest_client.dart';
+import 'package:genesis/src/features/dbaas/data/requests/create_pg_instance_req.dart';
+import 'package:genesis/src/features/dbaas/domain/params/create_pg_instance_params.dart';
 import 'package:genesis/src/layer_data/dtos/pg_instance_dto.dart';
 import 'package:genesis/src/layer_data/requests/pg_instances_requests/get_pg_instances_req.dart';
 import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
@@ -10,12 +12,12 @@ final class PgInstancesApi {
 
   final RestClient _client;
 
-  Future<PGInstanceDto> getPgInstance(PgInstanceUUID uuid) async {
+  Future<PgInstanceDto> getPgInstance(PgInstanceID uuid) async {
     // TODO: implement getPGInstance
     throw UnimplementedError();
   }
 
-  Future<List<PGInstanceDto>> getPgInstances(GetPgInstancesReq req) async {
+  Future<List<PgInstanceDto>> getPgInstances(GetPgInstancesReq req) async {
     try {
       final Response(:data) = await _client.get<List<dynamic>>(
         req.toPath(),
@@ -25,14 +27,26 @@ final class PgInstancesApi {
         return List.empty();
       }
       final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
-      return castedData.map((it) => PGInstanceDto.fromJson(it)).toList();
+      return castedData.map((it) => PgInstanceDto.fromJson(it)).toList();
     } on DioException catch (e) {
       throw BaseNetworkException.from(e);
     }
   }
 
-  Future<void> deletePgInstance(PgInstanceUUID uuid) async {
+  Future<void> deletePgInstance(PgInstanceID uuid) async {
     // TODO: implement deletePGInstance
     throw UnimplementedError();
+  }
+
+  Future<PgInstanceDto> createPgInstance(CreatePgInstanceParams params) async {
+    try {
+      final Response(:data) = await _client.post<Map<String, dynamic>>(
+        params.toPath(),
+        data: params.toJson(),
+      );
+      return PgInstanceDto.fromJson(data!);
+    } on DioException catch (e) {
+      throw BaseNetworkException.from(e);
+    }
   }
 }
