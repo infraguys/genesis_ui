e2e9f591b171b0bb3168488e83b8ef6624a42c51
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:54:02 -0700

    feat: add extension methods for CreatePgInstanceParams to handle JSON serialization and endpoint path

---
+++ lib/src/features/dbaas/data/requests/create_pg_instance_req.dart
@@ -0,0 +1,20 @@
+import 'package:genesis/src/core/network/endpoints/pg_instances_endpoints.dart';
+import 'package:genesis/src/features/dbaas/domain/params/create_pg_instance_params.dart';
+
+extension CreatePgInstanceReqExt on CreatePgInstanceParams {
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'ipsv4': ipv4,
+      'cpu': cores,
+      'ram': ram,
+      'disk_size': diskSize,
+      'nodes_number': nodesNumber,
+      'sync_replica_number': syncReplicaNumber,
+      'version': '/v1/types/postgres/versions/26786c09-d175-44e5-9013-ac14c88acd1c',
+    };
+  }
+
+  String toPath() => PgInstancesEndpoints.createInstance();
+}
