ffb3360a36ebf85b78feece3a153fc7f85de970e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 07:50:04 -0700

    feat: refactor PgInstancesApi to update method signatures and improve request handling

--- lib/src/features/dbaas/data/source/remote/pg_instances_api.dart
+++ lib/src/features/dbaas/data/source/remote/pg_instances_api.dart
@@ -2,35 +2,26 @@ import 'package:dio/dio.dart';
 import 'package:genesis/src/core/exceptions/base_network_exception.dart';
 import 'package:genesis/src/core/network/rest_client/rest_client.dart';
 import 'package:genesis/src/features/dbaas/data/requests/create_pg_instance_req.dart';
+import 'package:genesis/src/features/dbaas/data/requests/get_pg_instances_req.dart';
+import 'package:genesis/src/features/dbaas/data/requests/update_pg_instance_req.dart';
 import 'package:genesis/src/features/dbaas/domain/params/create_pg_instance_params.dart';
+import 'package:genesis/src/features/dbaas/domain/params/update_pg_instance_params.dart';
 import 'package:genesis/src/layer_data/dtos/pg_instance_dto.dart';
-import 'package:genesis/src/layer_data/requests/pg_instances_requests/get_pg_instances_req.dart';
 import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
 
 final class PgInstancesApi {
   PgInstancesApi(this._client);
 
   final RestClient _client;
 
+  /// Single instance methods
+  ///
+  /// Методы для работы с одним экземпляром
+
   Future<PgInstanceDto> getPgInstance(PgInstanceID uuid) async {
     // TODO: implement getPGInstance
     throw UnimplementedError();
-  }
-
-  Future<List<PgInstanceDto>> getPgInstances(GetPgInstancesReq req) async {
-    try {
-      final Response(:data) = await _client.get<List<dynamic>>(
-        req.toPath(),
-        queryParameters: req.toQuery(),
-      );
-      if (data == null) {
-        return List.empty();
-      }
-      final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
-      return castedData.map((it) => PgInstanceDto.fromJson(it)).toList();
-    } on DioException catch (e) {
-      throw BaseNetworkException.from(e);
-    }
   }
 
   Future<void> deletePgInstance(PgInstanceID uuid) async {
@@ -45,6 +36,38 @@ final class PgInstancesApi {
         data: params.toJson(),
       );
       return PgInstanceDto.fromJson(data!);
+    } on DioException catch (e) {
+      throw BaseNetworkException.from(e);
+    }
+  }
+
+  Future<PgInstanceDto> updatePgInstance(UpdatePgInstanceParams params) async {
+    try {
+      final Response(:data) = await _client.post<Map<String, dynamic>>(
+        params.toPath(),
+        data: params.toJson(),
+      );
+      return PgInstanceDto.fromJson(data!);
+    } on DioException catch (e) {
+      throw BaseNetworkException.from(e);
+    }
+  }
+
+  /// Multiple instances methods
+  ///
+  /// Методы для работы с несколькими экземплярами
+
+  Future<List<PgInstanceDto>> getPgInstances(GetPgInstancesParams params) async {
+    try {
+      final Response(:data) = await _client.get<List<dynamic>>(
+        params.toPath(),
+        queryParameters: params.toQuery(),
+      );
+      if (data == null) {
+        return List.empty();
+      }
+      final castedData = List.castFrom<dynamic, Map<String, dynamic>>(data);
+      return castedData.map((it) => PgInstanceDto.fromJson(it)).toList();
     } on DioException catch (e) {
       throw BaseNetworkException.from(e);
     }
