1c7566082a9398c2f5588ed0f8ff0f38d1374076
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:55:25 -0700

    feat: update imports and navigation for PgInstanceListView

--- lib/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/pg_instance_list_page.dart
+++ lib/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/pg_instance_list_page.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/string_extension.dart';
-import 'package:genesis/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/blocs/pg_instances_bloc/pg_instances_bloc.dart';
-import 'package:genesis/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/blocs/widgets/pg_instances_table.dart';
-import 'package:genesis/src/layer_domain/repositories/i_pg_instances_repository.dart';
+import 'package:genesis/src/features/dbaas/domain/repositories/i_pg_instances_repository.dart';
+import 'package:genesis/src/features/dbaas/presentation/pg_instances_pages/blocs/pg_instances_bloc/pg_instances_bloc.dart';
+import 'package:genesis/src/features/dbaas/presentation/pg_instances_pages/pg_instance_list_page/widgets/pg_instances_table.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/app_progress_indicator.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/breadcrumbs.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/buttons_bar.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/create_icon_button.dart';
+import 'package:genesis/src/routing/app_router.dart';
+import 'package:go_router/go_router.dart';
 
 class _PgInstanceListView extends StatelessWidget {
-  const _PgInstanceListView({super.key});  // ignore: unused_element_parameter
+  const _PgInstanceListView({super.key}); // ignore: unused_element_parameter
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,7 @@ class _PgInstanceListView extends StatelessWidget {
         ButtonsBar(
           children: [
             // SearchInput(),
-            CreateIconButton(onPressed: () {})
+            CreateIconButton(onPressed: () => context.goNamed(AppRoutes.createInstance.name)),
           ],
         ),
         const SizedBox(height: 24),
