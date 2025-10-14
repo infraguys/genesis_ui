50678843b22fdf7d15b1e9ed89f5f022569311b8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:51:34 -0700

    refactor: rename PGInstancesEndpoints class and update instance identifiers from uuid to id

--- lib/src/core/network/endpoints/pg_instances_endpoints.dart
+++ lib/src/core/network/endpoints/pg_instances_endpoints.dart
@@ -1,22 +1,22 @@
 import 'package:genesis/src/core/env/env.dart';
 import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
 
-abstract class PGInstancesEndpoints {
+abstract class PgInstancesEndpoints {
   static const _instances = '${Env.dbaasApiPrefix}/${Env.versionApi}/types/postgres/instances/';
-  static const _instance = '$_instances:uuid';
+  static const _instance = '$_instances:id';
 
   static String getInstances() => _instances;
 
   static String createInstance() => _instances;
 
-  static String getInstance(PgInstanceUUID uuid) => _instance.fillUuid(uuid);
+  static String getInstance(PgInstanceID id) => _instance.fillUuid(id);
 
-  static String updateInstance(PgInstanceUUID uuid) => _instance.fillUuid(uuid);
+  static String updateInstance(PgInstanceID id) => _instance.fillUuid(id);
 
-  static String deleteInstance(PgInstanceUUID uuid) => _instance.fillUuid(uuid);
+  static String deleteInstance(PgInstanceID id) => _instance.fillUuid(id);
 }
 
 // ignore: camel_case_extensions
 extension _ on String {
-  String fillUuid(PgInstanceUUID uuid) => replaceFirst(':uuid', uuid.raw);
+  String fillUuid(PgInstanceID uuid) => replaceFirst('id', uuid.raw);
 }
