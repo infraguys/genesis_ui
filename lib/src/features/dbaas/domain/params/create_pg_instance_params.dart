7bd7346d51921051b18716944e8cdb40a41fdbe4
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-14 04:53:45 -0700

    feat: add CreatePgInstanceParams class for PostgreSQL instance creation

---
+++ lib/src/features/dbaas/domain/params/create_pg_instance_params.dart
@@ -0,0 +1,21 @@
+final class CreatePgInstanceParams {
+  CreatePgInstanceParams({
+    required this.name,
+    required this.cores,
+    required this.ram,
+    required this.diskSize,
+    required this.nodesNumber,
+    required this.syncReplicaNumber,
+    required this.ipv4,
+    this.description,
+  });
+
+  final String name;
+  final String? description;
+  final List<String> ipv4;
+  final int cores;
+  final int ram;
+  final int diskSize;
+  final int nodesNumber;
+  final int syncReplicaNumber;
+}
