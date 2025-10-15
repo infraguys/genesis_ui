8eca02fd5cb8aabf3d8cc081bfc1ad6254cfa4eb
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-15 01:37:11 -0700

    feat: implement GetPgInstanceUseCase for retrieving PostgreSQL instance details

---
+++ lib/src/features/dbaas/domain/use_cases/get_pg_instance_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:genesis/src/features/dbaas/domain/params/get_pg_intstance_params.dart';
+import 'package:genesis/src/features/dbaas/domain/repositories/i_pg_instances_repository.dart';
+import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
+import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
+
+final class GetPgInstanceUseCase {
+  GetPgInstanceUseCase(this._repository);
+
+  final IPgInstancesRepository _repository;
+
+  Future<PgInstance> call(PgInstanceID id) {
+    return _repository.getPgInstance(GetPgInstanceParams(id));
+  }
+}
