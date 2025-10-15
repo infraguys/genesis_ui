92154a9f893828501b39064f0a73dee3ccb750c5
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-15 01:48:04 -0700

    fix: correct import path for GetPgInstanceParams in get_pg_instance_usecase.dart

--- lib/src/features/dbaas/domain/use_cases/get_pg_instance_usecase.dart
+++ lib/src/features/dbaas/domain/use_cases/get_pg_instance_usecase.dart
@@ -1,4 +1,4 @@
-import 'package:genesis/src/features/dbaas/domain/params/get_pg_intstance_params.dart';
+import 'package:genesis/src/features/dbaas/domain/params/get_pg_instance_params.dart';
 import 'package:genesis/src/features/dbaas/domain/repositories/i_pg_instances_repository.dart';
 import 'package:genesis/src/layer_domain/entities/pg_instance.dart';
 import 'package:genesis/src/layer_domain/params/dbaas/pg_instances_params/get_pg_instances_params.dart';
