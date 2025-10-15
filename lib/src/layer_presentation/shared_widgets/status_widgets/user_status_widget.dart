c0e7e0c2817420b47ee5488710bf17c2fc9af59b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-15 10:10:55 -0700

    refactor: simplify status parameter handling in UserStatusWidget

--- lib/src/layer_presentation/shared_widgets/status_widgets/user_status_widget.dart
+++ lib/src/layer_presentation/shared_widgets/status_widgets/user_status_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:genesis/src/core/typedefs/status_view_params.dart';
 import 'package:genesis/src/layer_domain/entities/user.dart';
 import 'package:genesis/src/layer_presentation/shared_widgets/base_status_widget.dart';
 import 'package:genesis/src/theming/palette.dart';
@@ -9,33 +10,30 @@ class UserStatusWidget extends StatelessWidget {
 
   final UserStatus _status;
 
-  ({String text, Color color, Color labelColor}) _getStatusParams(
-    BuildContext context,
-    UserStatus status,
-  ) {
-    switch (status) {
+  @override
+  Widget build(BuildContext context) {
+    final params = _getStatusParams(context);
+    return BaseStatusWidget(
+      text: params.text,
+      color: params.color,
+      labelColor: params.labelColor,
+    );
+  }
+
+  StatusViewParams _getStatusParams(BuildContext context) {
+    switch (_status) {
       case UserStatus.active:
         return (
           text: context.$.active,
           color: Palette.color6DCF91,
           labelColor: Palette.color6DCF91.withValues(alpha: 0.10),
         );
-      default:
+      case UserStatus.unknown:
         return (
           text: context.$.unknown,
           color: Palette.colorF04C4C,
           labelColor: Palette.colorF04C4C.withValues(alpha: 0.10),
         );
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final params = _getStatusParams(context, _status);
-    return BaseStatusWidget(
-      text: params.text,
-      color: params.color,
-      labelColor: params.labelColor,
-    );
   }
 }
