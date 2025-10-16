57b5dcbbf01a47aa33e71cc652d48003b2a805df
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-16 16:06:51 +0300

    refactor: add delete event for PgInstance in PgInstanceEvent

--- lib/src/features/dbaas/presentation/blocs/pg_instance_bloc/pg_instance_event.dart
+++ lib/src/features/dbaas/presentation/blocs/pg_instance_bloc/pg_instance_event.dart
@@ -6,6 +6,8 @@ sealed class PgInstanceEvent {
   factory PgInstanceEvent.createInstance(CreatePgInstanceParams params) = _CreateInstance;
 
   factory PgInstanceEvent.updateInstance(UpdatePgInstanceParams params) = _UpdateInstance;
+
+  factory PgInstanceEvent.delete(PgInstance instance) = _DeleteInstance;
 }
 
 final class _GetInstance implements PgInstanceEvent {
@@ -18,6 +20,12 @@ final class _CreateInstance implements PgInstanceEvent {
   _CreateInstance(this.params);
 
   final CreatePgInstanceParams params;
+}
+
+final class _DeleteInstance implements PgInstanceEvent {
+  _DeleteInstance(this.instance);
+
+  final PgInstance instance;
 }
 
 final class _UpdateInstance implements PgInstanceEvent {
