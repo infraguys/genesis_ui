9ee642fd4f67ba5c87ab4b3f364d1729a194bc2c
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-19 01:36:06 +0300

    refactor: simplify GetNodesReq class by removing unnecessary interfaces and streamlining toPath method

--- lib/src/features/nodes/data/requests/get_nodes_req.dart
+++ lib/src/features/nodes/data/requests/get_nodes_req.dart
@@ -1,14 +1,11 @@
-import 'package:genesis/src/core/interfaces/path_encodable.dart';
-import 'package:genesis/src/core/interfaces/query_encodable.dart';
 import 'package:genesis/src/core/network/endpoints/nodes_endpoints.dart';
 import 'package:genesis/src/features/nodes/domain/params/get_nodes_params.dart';
 
-final class GetNodesReq implements PathEncodable, QueryEncodable {
+final class GetNodesReq {
   GetNodesReq(this._params);
 
   final GetNodesParams _params;
 
-  @override
   Map<String, dynamic> toQuery() {
     // TODO(Koretsky): Добавить параметров
     return {
@@ -19,8 +16,5 @@ final class GetNodesReq implements PathEncodable, QueryEncodable {
     };
   }
 
-  @override
-  String toPath() {
-    return NodesEndpoints.getNodes();
-  }
+  String toPath() => NodesEndpoints.getNodes();
 }
