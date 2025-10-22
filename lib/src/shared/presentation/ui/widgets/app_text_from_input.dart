b956b10a90ddbcb8a930f44a65bb96c489f8ec3b
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-23 02:18:49 +0300

    feat: add localization strings for user-related fields in multiple languages

--- lib/src/shared/presentation/ui/widgets/app_text_from_input.dart
+++ lib/src/shared/presentation/ui/widgets/app_text_from_input.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 
 class AppTextFormInput extends StatelessWidget {
   const AppTextFormInput({
-    required this.labelText,
-    required this.hintText,
+    this.labelText,
+    this.hintText,
     super.key,
     this.onSaved,
     this.initialValue,
@@ -13,45 +13,67 @@ class AppTextFormInput extends StatelessWidget {
     this.readOnly = false,
     this.maxLines,
     this.minLines,
+    this.helperText,
+    this.obscureText = false,
   });
 
   final void Function(String?)? onSaved;
   final String? initialValue;
-  final String labelText;
-  final String hintText;
+  final String? labelText;
+  final String? hintText;
+  final String? helperText;
   final List<TextInputFormatter>? inputFormatters;
   final String? Function(String?)? validator;
   final bool readOnly;
   final int? maxLines;
   final int? minLines;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Text(
-            labelText,
-            style: TextStyle(fontSize: 12, color: Colors.white24),
+    if (labelText != null) {
+      return Column(
+        spacing: 4.0,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text(
+              labelText!,
+              style: TextStyle(fontSize: 16, height: 16 / 16, color: Colors.white24),
+            ),
           ),
-        ),
-        TextFormField(
-          initialValue: initialValue,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          readOnly: readOnly,
-          decoration: InputDecoration(
-            hintText: hintText,
-            helperText: '',
+          TextFormField(
+            initialValue: initialValue,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            readOnly: readOnly,
+            decoration: InputDecoration(
+              hintText: hintText,
+              helperText: helperText,
+            ),
+            onSaved: onSaved,
+            inputFormatters: inputFormatters,
+            validator: validator,
+            maxLines: maxLines,
+            minLines: minLines,
           ),
-          onSaved: onSaved,
-          inputFormatters: inputFormatters,
-          validator: validator,
-          maxLines: maxLines,
-          minLines: minLines,
-        ),
-      ],
+        ],
+      );
+    }
+    return TextFormField(
+      initialValue: initialValue,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        hintText: hintText,
+        helperText: helperText,
+      ),
+      onSaved: onSaved,
+      inputFormatters: inputFormatters,
+      validator: validator,
+      maxLines: maxLines,
+      minLines: minLines,
+      obscureText: obscureText,
     );
   }
 }
