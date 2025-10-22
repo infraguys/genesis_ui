94b65419a7ea67fa7cacf4dd3626cc50c53021e6
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-22 17:19:59 +0300

    feat: refactor UsersBloc and UsersState for improved state management

--- lib/src/layer_presentation/blocs/users_bloc/users_bloc.dart
+++ lib/src/layer_presentation/blocs/users_bloc/users_bloc.dart
@@ -2,9 +2,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/exceptions/api_exception.dart';
 import 'package:genesis/src/features/users/domain/entities/user.dart';
 import 'package:genesis/src/features/users/domain/params/get_users_params.dart';
+import 'package:genesis/src/features/users/domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/features/users/domain/usecases/delete_users_usecase.dart';
 import 'package:genesis/src/features/users/domain/usecases/force_confirm_emails_usecase.dart';
-import 'package:genesis/src/features/users/domain/repositories/i_users_repository.dart';
 import 'package:genesis/src/features/users/domain/usecases/get_users_usecase.dart';
 
 part 'users_event.dart';
@@ -12,7 +12,7 @@ part 'users_event.dart';
 part 'users_state.dart';
 
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
-  UsersBloc(this._usersRepository) : super(UsersState.initial()) {
+  UsersBloc(this._usersRepository) : super(_InitialState()) {
     on(_getUsers);
     on(_onDeleteUsers);
     on(_onForceConfirmEmails);
@@ -23,9 +23,9 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
 
   Future<void> _getUsers(_GetUsers event, Emitter<UsersState> emit) async {
     final useCase = GetUsersUseCase(_usersRepository);
-    emit(UsersState.loading());
+    emit(UsersLoadingState());
     final users = await useCase(GetUsersParams());
-    emit(UsersState.loaded(users));
+    emit(UsersLoadedState(users));
   }
 
   Future<void> _onDeleteUsers(_DeleteUsers event, Emitter<UsersState> emit) async {
@@ -34,13 +34,13 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
     final previousState = state;
 
     // todo: Подумать что сделать с запросом
-    emit(UsersState.loading());
+    emit(UsersLoadingState());
     try {
       await useCase(event.users.map((it) => it.uuid).toList());
-      emit(UsersState.deleted(event.users));
+      emit(UsersDeletedState(event.users));
       add(UsersEvent.getUsers());
     } on PermissionException catch (e) {
-      emit(UsersState.permissionFailure(e.message));
+      emit(UsersPermissionFailureState(e.message));
       emit(previousState);
     }
   }
@@ -48,7 +48,7 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
   Future<void> _onForceConfirmEmails(_ForceConfirmEmails event, Emitter<UsersState> emit) async {
     final useCase = ForceConfirmEmailsUseCase(_usersRepository);
     final uuids = event.users.where((user) => !user.emailVerified).map((user) => user.uuid);
-    emit(UsersState.loading());
+    emit(UsersLoadingState());
     await useCase(uuids.toList());
     add(UsersEvent.getUsers());
   }
--- lib/src/layer_presentation/blocs/users_bloc/users_state.dart
+++ lib/src/layer_presentation/blocs/users_bloc/users_state.dart
@@ -1,20 +1,8 @@
 part of 'users_bloc.dart';
 
-sealed class UsersState {
-  factory UsersState.initial() = UsersInitialState;
+sealed class UsersState {}
 
-  factory UsersState.loading() = UsersLoadingState;
-
-  factory UsersState.loaded(List<User> users) = UsersLoadedState;
-
-  factory UsersState.deleted(List<User> users) = UsersDeletedState;
-
-  factory UsersState.failure(String message) = UsersFailureState;
-
-  factory UsersState.permissionFailure(String message) = UsersPermissionFailureState;
-}
-
-final class UsersInitialState implements UsersState {}
+final class _InitialState implements UsersState {}
 
 final class UsersLoadingState implements UsersState {}
 
@@ -26,20 +14,20 @@ final class UsersLoadedState extends _UsersDataState {
   UsersLoadedState(super.users);
 }
 
-// Base classes to reduce code duplication
-
-base class _UsersDataState implements UsersState {
-  _UsersDataState(this.users);
-
-  final List<User> users;
+final class UsersPermissionFailureState extends _FailureState {
+  UsersPermissionFailureState(super.message);
 }
 
 final class UsersFailureState extends _FailureState {
   UsersFailureState(super.message);
 }
 
-final class UsersPermissionFailureState extends _FailureState {
-  UsersPermissionFailureState(super.message);
+// Base classes to reduce code duplication
+
+base class _UsersDataState implements UsersState {
+  _UsersDataState(this.users);
+
+  final List<User> users;
 }
 
 base class _FailureState implements UsersState {
