666afd95511d4bd419c10eb97e5ae367d24d0dd0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-22 03:53:29 +0300

    feat: simplify polling mechanism by removing _run method and ensuring immediate tick execution

--- lib/src/shared/presentation/polling_controller.dart
+++ lib/src/shared/presentation/polling_controller.dart
@@ -1,11 +1,9 @@
-// lib/src/core/polling/polling_controller.dart
 import 'dart:async';
 
 class PollingController {
   static const Duration _period = Duration(seconds: 5);
 
   Timer? _timer;
-  bool _isTickInProgress = false;
 
   bool get isActive => _timer != null;
 
@@ -13,25 +11,12 @@ class PollingController {
   void start(void Function() onTick) {
     stop();
     // первый тик сразу
-    _run(onTick);
-    _timer = Timer.periodic(_period, (_) => _run(onTick));
+    onTick();
+    _timer = Timer.periodic(_period, (_) => onTick());
   }
 
   void stop() {
     _timer?.cancel();
     _timer = null;
-  }
-
-  Future<void> _run(void Function() onTick) async {
-    if (_isTickInProgress) {
-      return;
-    }
-    // защита от наложения
-    _isTickInProgress = true;
-    try {
-      onTick();
-    } finally {
-      _isTickInProgress = false;
-    }
   }
 }
