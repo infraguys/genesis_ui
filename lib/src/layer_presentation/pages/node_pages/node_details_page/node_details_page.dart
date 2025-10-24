0caf29f9fe4e55a9f8f98664f287a751bfafc810
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-24 11:05:44 +0300

    feat: refactor node_details_page to use PageLayout for improved structure and readability

--- lib/src/layer_presentation/pages/node_pages/node_details_page/node_details_page.dart
+++ lib/src/layer_presentation/pages/node_pages/node_details_page/node_details_page.dart
@@ -6,18 +6,21 @@ import 'package:genesis/src/core/extensions/string_extension.dart';
 import 'package:genesis/src/features/nodes/domain/entities/node.dart';
 import 'package:genesis/src/features/nodes/domain/params/update_node_params.dart';
 import 'package:genesis/src/features/nodes/domain/repositories/i_nodes_repository.dart';
+import 'package:genesis/src/features/nodes/presentation/widgets/node_status_widget.dart';
 import 'package:genesis/src/layer_presentation/blocs/nodes_bloc/nodes_bloc.dart';
 import 'package:genesis/src/layer_presentation/pages/node_pages/create_node_page/blocs/node_bloc/node_bloc.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/app_progress_indicator.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/app_snackbar.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/app_text_from_input.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/breadcrumbs.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/buttons_bar.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/confirmation_dialog.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
+import 'package:genesis/src/shared/presentation/ui/widgets/page_layout.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/save_icon_button.dart';
-import 'package:genesis/src/features/nodes/presentation/widgets/node_status_widget.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 part './widgets/delete_node_btn.dart';
 
@@ -96,38 +99,67 @@ class _NodeDetailsViewState extends State<_NodeDetailsView> {
           if (state is! NodeLoadedState) {
             return AppProgressIndicator();
           }
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 24,
+          final NodeLoadedState(:node) = state;
+          return PageLayout(
+            breadcrumbs: Breadcrumbs(
+              items: [
+                BreadcrumbItem(text: context.$.nodes),
+                BreadcrumbItem(text: node.name),
+              ],
+            ),
+            buttonsBar: ButtonsBar(
               children: [
-                Breadcrumbs(
-                  items: [
-                    BreadcrumbItem(text: context.$.nodes),
-                    BreadcrumbItem(text: state.node.name),
-                  ],
-                ),
-                ButtonsBar(
-                  children: [
-                    _DeleteNodeButton(node: state.node),
-                    SaveIconButton(onPressed: save),
-                  ],
-                ),
-                LayoutBuilder(
-                  builder: (context, constraints) {
-                    return Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 128,
-                      children: [
-                        SizedBox(
-                          width: constraints.maxWidth * 0.4,
-                          child: Form(
-                            key: _formKey,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              spacing: 16,
-                              children: [
-                                AppTextFormInput(
+                _DeleteNodeButton(node: node),
+                SaveIconButton(onPressed: save),
+              ],
+            ),
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(Icons.hub_rounded, size: 100),
+                          SizedBox(width: 32),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            spacing: 16.0,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  text: 'ID: ',
+                                  children: [
+                                    WidgetSpan(
+                                      alignment: PlaceholderAlignment.middle,
+                                      child: SelectableText(
+                                        node.id.value,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: GoogleFonts.robotoMono().fontFamily,
+                                        ),
+                                      ),
+                                    ),
+                                    WidgetSpan(child: const SizedBox(width: 8)),
+                                    WidgetSpan(
+                                      alignment: PlaceholderAlignment.middle,
+                                      child: IconButton(
+                                        icon: Icon(Icons.copy, color: Colors.white, size: 18),
+                                        onPressed: () {
+                                          Clipboard.setData(ClipboardData(text: node.id.value));
+                                          final msg = context.$.msgCopiedToClipboard(node.id.value);
+                                          ScaffoldMessenger.of(context).showSnackBar(AppSnackBar.success(msg));
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: 500,
+                                child: AppTextFormInput(
                                   initialValue: _name,
                                   helperText: context.$.name,
                                   onSaved: (newValue) => _name = newValue!,
@@ -136,25 +168,96 @@ class _NodeDetailsViewState extends State<_NodeDetailsView> {
                                     _ => null,
                                   },
                                 ),
-                                DropdownMenuFormField<NodeType>(
-                                  menuStyle: MenuStyle(
-                                    fixedSize: WidgetStatePropertyAll(Size.fromWidth(constraints.maxWidth * 0.4)),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Table(
+                            defaultColumnWidth: FixedColumnWidth(200),
+                            children: [
+                              TableRow(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(context.$.status),
                                   ),
-                                  width: double.infinity,
-                                  initialSelection: _nodeType,
-                                  requestFocusOnTap: false,
-                                  helperText: context.$.nodeType,
-                                  onSaved: (newValue) => _nodeType = newValue!,
-                                  dropdownMenuEntries: [
-                                    DropdownMenuEntry(
-                                      value: NodeType.vm,
-                                      label: context.$.virtualMachine,
-                                    ),
-                                    DropdownMenuEntry(
-                                      value: NodeType.hw,
-                                      label: context.$.hardware,
-                                    ),
-                                  ],
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: NodeStatusWidget(status: node.status),
+                                  ),
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(context.$.createdAt),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(DateFormat('dd.MM.yyyy HH:mm').format(node.createdAt)),
+                                  ),
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(context.$.updatedAt),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(DateFormat('dd.MM.yyyy HH:mm').format(node.updatedAt)),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    spacing: 16.0,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 16.0,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              spacing: 16.0,
+                              children: [
+                                LayoutBuilder(
+                                  builder: (context, constraints) {
+                                    return DropdownMenuFormField<NodeType>(
+                                      menuStyle: MenuStyle(
+                                        alignment: Alignment(-1, 0.5),
+                                        fixedSize: WidgetStatePropertyAll(Size.fromWidth(constraints.maxWidth)),
+                                      ),
+                                      width: double.infinity,
+                                      initialSelection: _nodeType,
+                                      requestFocusOnTap: false,
+                                      helperText: context.$.nodeType,
+                                      onSaved: (newValue) => _nodeType = newValue!,
+                                      dropdownMenuEntries: [
+                                        DropdownMenuEntry(
+                                          value: NodeType.vm,
+                                          label: context.$.virtualMachine,
+                                        ),
+                                        DropdownMenuEntry(
+                                          value: NodeType.hw,
+                                          label: context.$.hardware,
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 ),
                                 AppTextFormInput(
                                   initialValue: _image,
@@ -175,6 +278,13 @@ class _NodeDetailsViewState extends State<_NodeDetailsView> {
                                     _ => null,
                                   },
                                 ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              spacing: 16.0,
+                              children: [
                                 AppTextFormInput(
                                   initialValue: _rootDiskSize.toString(),
                                   helperText: context.$.rootDiskSize,
@@ -198,55 +308,23 @@ class _NodeDetailsViewState extends State<_NodeDetailsView> {
                                   initialValue: _ipv4,
                                   helperText: 'ipv4'.hardcoded,
                                 ),
-                                AppTextFormInput(
-                                  initialValue: _description,
-                                  helperText: context.$.description,
-                                  onSaved: (newValue) => _description = newValue!,
-                                ),
                               ],
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: constraints.maxWidth * 0.4,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            spacing: 32,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.status),
-                                  NodeStatusWidget(status: state.node.status),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.createdAt),
-                                  Text(state.node.createdAt.toString()),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                spacing: 8.0,
-                                children: [
-                                  Text(context.$.updatedAt),
-                                  Text(state.node.updatedAt.toString()),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    );
-                  },
+                        ],
+                      ),
+                      AppTextFormInput(
+                        initialValue: _description,
+                        helperText: context.$.description,
+                        onSaved: (newValue) => _description = newValue!,
+                        maxLines: 2,
+                        minLines: 2,
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           );
         },
       ),
