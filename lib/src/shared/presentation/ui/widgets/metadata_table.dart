8ce5f7d6495ad91ee5958b4eb39a36b7cac630ca
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-26 02:30:39 +0300

    feat: add MetadataTable widget for displaying metadata information

---
+++ lib/src/shared/presentation/ui/widgets/metadata_table.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:genesis/src/core/extensions/localized_build_context.dart';
+import 'package:intl/intl.dart';
+
+class MetadataTable extends StatelessWidget {
+  const MetadataTable({
+    required this.statusWidget,
+    required this.createdAt,
+    required this.updatedAt,
+    super.key,
+  });
+
+  final Widget statusWidget;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      defaultColumnWidth: FixedColumnWidth(140),
+      children: [
+        TableRow(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(context.$.status),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: statusWidget,
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(context.$.createdAt),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(DateFormat('dd.MM.yyyy HH:mm').format(createdAt)),
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(context.$.updatedAt),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(DateFormat('dd.MM.yyyy HH:mm').format(updatedAt)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
