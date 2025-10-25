f402e81947248ea1952caf79b264824a0c4860c0
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-25 03:29:33 +0300

    feat: update node_list_page to open CreateNodePage in a dialog and simplify button actions

--- lib/src/layer_presentation/pages/node_pages/node_list_page/node_list_page.dart
+++ lib/src/layer_presentation/pages/node_pages/node_list_page/node_list_page.dart
@@ -3,16 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genesis/src/core/extensions/localized_build_context.dart';
 import 'package:genesis/src/features/nodes/domain/entities/node.dart';
 import 'package:genesis/src/layer_presentation/blocs/nodes_bloc/nodes_bloc.dart';
+import 'package:genesis/src/layer_presentation/pages/node_pages/create_node_page/create_node_page.dart';
 import 'package:genesis/src/layer_presentation/pages/node_pages/node_list_page/blocs/nodes_selection_cubit/nodes_selection_cubit.dart';
 import 'package:genesis/src/layer_presentation/pages/node_pages/node_list_page/widgets/nodes_table.dart';
+import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/app_progress_indicator.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/breadcrumbs.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/buttons_bar.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/confirmation_dialog.dart';
 import 'package:genesis/src/shared/presentation/ui/widgets/create_icon_button.dart';
-import 'package:genesis/src/layer_presentation/shared_widgets/delete_elevated_button.dart';
-import 'package:genesis/src/routing/app_router.dart';
-import 'package:go_router/go_router.dart';
 
 part './widgets/delete_nodes_btn.dart';
 
@@ -33,15 +32,20 @@ class _NodeListView extends StatelessWidget {
         ButtonsBar(
           children: [
             _DeleteNodesButton(),
-            CreateIconButton(onPressed: () => context.goNamed(AppRoutes.createNode.name)),
+            CreateIconButton(
+              onPressed: () async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (context) => Dialog(child: CreateNodePage()),
+                );
+              },
+            ),
           ],
         ),
         Expanded(
           child: BlocConsumer<NodesBloc, NodesState>(
             listenWhen: (_, current) => current is NodesLoadedState,
-            listener: (context, _) {
-              context.read<NodesSelectionCubit>().onClear();
-            },
+            listener: (context, _) => context.read<NodesSelectionCubit>().onClear(),
             builder: (_, state) => switch (state) {
               NodesLoadedState(:final nodes) => NodesTable(nodes: nodes),
               _ => AppProgressIndicator(),
