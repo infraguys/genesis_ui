ca0ec39b0997c9bb07c3677bee5cfd5bb48a35e8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-10-27 01:33:28 +0300

    feat: add DatabasesEndpoints class for managing database endpoints

---
+++ lib/src/core/network/endpoints/databases_endpoints.dart
@@ -0,0 +1,13 @@
+import 'package:genesis/src/core/network/endpoints/endpoint.dart';
+import 'package:genesis/src/features/dbaas/domain/entities/database.dart';
+import 'package:genesis/src/features/dbaas/domain/entities/pg_instance.dart';
+
+abstract class DatabasesEndpoints {
+  static Endpoint items() {
+    return Endpoint.withDbaasPrefix('/types/postgres/instances/databases/');
+  }
+
+  static Endpoint item(PgInstanceID pgInstanceId, DatabaseID dbId) {
+    return Endpoint.withDbaasPrefix('/types/postgres/instances/$pgInstanceId/databases/$dbId');
+  }
+}
