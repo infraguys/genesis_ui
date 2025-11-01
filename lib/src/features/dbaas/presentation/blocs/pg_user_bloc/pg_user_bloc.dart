6e2197d6e75395f8863333c8cc0891cbd94e14b8
Eugene Koretsky <E.Koretsky@outlook.com>
2025-11-01 15:59:10 +0300

    feat: add delete user functionality to PgUserBloc

--- lib/src/features/dbaas/presentation/blocs/pg_user_bloc/pg_user_bloc.dart
+++ lib/src/features/dbaas/presentation/blocs/pg_user_bloc/pg_user_bloc.dart
@@ -1,15 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genesis/src/core/exceptions/api_exception.dart';
 import 'package:genesis/src/features/dbaas/domain/entities/pg_user.dart';
 import 'package:genesis/src/features/dbaas/domain/params/pg_users/create_pg_user_params.dart';
 import 'package:genesis/src/features/dbaas/domain/params/pg_users/pg_user_params.dart';
 import 'package:genesis/src/features/dbaas/domain/params/pg_users/update_pg_user_params.dart';
 import 'package:genesis/src/features/dbaas/domain/repositories/i_pg_user_repository.dart';
 import 'package:genesis/src/features/dbaas/domain/use_cases/pg_user_usecases/create_pg_user_usecase.dart';
+import 'package:genesis/src/features/dbaas/domain/use_cases/pg_user_usecases/delete_pg_user_usecase.dart';
 import 'package:genesis/src/features/dbaas/domain/use_cases/pg_user_usecases/get_pg_user_usecase.dart';
 import 'package:genesis/src/features/dbaas/domain/use_cases/pg_user_usecases/update_pg_user_usecase.dart';
 
 part 'pg_user_event.dart';
-
 part 'pg_user_state.dart';
 
 class PgUserBloc extends Bloc<PgUserEvent, PgUserState> {
@@ -17,6 +18,7 @@ class PgUserBloc extends Bloc<PgUserEvent, PgUserState> {
     on(_onCreatePgUser);
     on(_onGetPgUser);
     on(_onUpdatePgUser);
+    on(_onDeletePgUser);
   }
 
   final IPgUsersRepository _repository;
@@ -38,5 +40,15 @@ class PgUserBloc extends Bloc<PgUserEvent, PgUserState> {
     final useCase = UpdatePgUserUseCase(_repository);
     final pgUser = await useCase(event.params);
     emit(PgUserUpdatedState(pgUser));
+  }
+
+  Future<void> _onDeletePgUser(_DeletePgUser event, Emitter<PgUserState> emit) async {
+    final useCase = DeletePgUserUseCase(_repository);
+    try {
+      await useCase(event.params);
+      emit(PgUserDeletedState(event.pgUser));
+    } on PermissionException catch (e) {
+      // emit(NodePermissionFailureState(e.message));
+    }
   }
 }
