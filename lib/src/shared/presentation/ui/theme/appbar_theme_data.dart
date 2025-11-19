70719a4c7389ef4add03f3a0def52d61bff8d83e
Eugene Koretsky <E.Koretsky@outlook.com>
2025-11-20 01:40:34 +0300

    fix: correct property name for AppBarTheme background color

--- lib/src/shared/presentation/ui/theme/appbar_theme_data.dart
+++ lib/src/shared/presentation/ui/theme/appbar_theme_data.dart
@@ -2,6 +2,6 @@ part of 'app_theme.dart';
 
 final _appbarThemeData = AppBarTheme(
   toolbarHeight: 48,
-  color: Palette.color333333,
+  backgroundColor: Palette.color333333,
   actionsPadding: EdgeInsets.only(right: 16),
 );
