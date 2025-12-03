6a09187a8d7a2ce5fefa08d863b9ed789b433dc5
Eugene Koretsky <E.Koretsky@outlook.com>
2025-12-04 01:09:38 +0300

    refactor: update UserState to define shouldListen behavior for all states

--- lib/src/features/users/presentation/blocs/user_bloc/user_state.dart
+++ lib/src/features/users/presentation/blocs/user_bloc/user_state.dart
@@ -1,23 +1,38 @@
 part of 'user_bloc.dart';
 
 sealed class UserState {
-  bool get shouldListen => this is! UserLoadingState && this is! _InitialState;
+  bool get shouldListen;
 }
 
-final class _InitialState extends UserState {}
+final class _InitialState extends UserState {
+  @override
+  bool get shouldListen => false;
+}
 
-final class UserLoadingState extends UserState {}
+final class UserLoadingState extends UserState {
+  @override
+  bool get shouldListen => false;
+}
 
-final class UserLoadedState extends UserState{
+final class UserLoadedState extends UserState {
   UserLoadedState(this.user);
+
+  @override
+  bool get shouldListen => true;
 
   final User user;
 }
 
-final class UserConfirmedState extends UserState {}
+final class UserConfirmedState extends UserState {
+  @override
+  bool get shouldListen => true;
+}
 
 final class UserUpdatedState extends UserState {
   UserUpdatedState(this.user);
+
+  @override
+  bool get shouldListen => true;
 
   final User user;
 }
@@ -25,11 +40,17 @@ final class UserUpdatedState extends UserState {
 final class UserCreatedState extends UserState {
   UserCreatedState(this.user);
 
+  @override
+  bool get shouldListen => true;
+
   final User user;
 }
 
 final class UserDeletedState extends UserState {
   UserDeletedState(this.user);
+
+  @override
+  bool get shouldListen => true;
 
   final User user;
 }
@@ -39,11 +60,17 @@ final class UserDeletedState extends UserState {
 final class UserFailureState extends UserState {
   UserFailureState(this.message);
 
+  @override
+  bool get shouldListen => false;
+
   final String message;
 }
 
 final class UserPermissionFailureState extends UserState {
   UserPermissionFailureState(this.message);
+
+  @override
+  bool get shouldListen => false;
 
   final String message;
 }
