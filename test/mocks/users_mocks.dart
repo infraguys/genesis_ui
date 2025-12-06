ee6cf9962e810ba07f9f98e8c96bbaac09121e56
Eugene Koretsky <E.Koretsky@outlook.com>
2025-12-07 01:36:11 +0300

    test: add GetUsersUseCase tests and mock repository setup

---
+++ test/mocks/users_mocks.dart
@@ -0,0 +1,5 @@
+import 'package:genesis/src/features/users/domain/repositories/i_users_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([IUsersRepository])
+void main() {}
